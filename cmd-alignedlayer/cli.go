c14d865021df2efde1639785a9a1addcc8af65c6
Franco Cuppari <fcuppari@fi.uba,ar>
2024-04-17 14:26:47 -0300

    Complete first implementation of CLI for verifying

--- cmd-alignedlayer/cli.go
+++ cmd-alignedlayer/cli.go
@@ -5,41 +5,10 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/urfave/cli/v2"
 )
-
-func runCLI() {
-	app := &cli.App{
-		Name:      "Aligned Layer Verifier",
-		UsageText: "\n" + "verify plonk proof.txt public_input_file.txt " + "verification_key.txt" + "\n" + "verify groth16 proof.txt public_input_file.txt " + "verification_key.txt",
-		Commands: []*cli.Command{
-			{
-				Name:  "verify",
-				Usage: "verify a proof using a specified proof system",
-				Action: func(c *cli.Context) error {
-					if c.NArg() < 4 {
-						fmt.Println("Insufficient arguments")
-						cli.ShowCommandHelp(c, "verify")
-						return nil
-					}
-
-					system := c.Args().Get(0)
-					proofPath := c.Args().Get(1)
-					publicInputPath := c.Args().Get(2)
-					verificationKeyPath := c.Args().Get(3)
-
-					return verify(system, proofPath, publicInputPath, verificationKeyPath)
-				},
-			},
-		},
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func verify(system, proofPath, publicInputPath, verificationKeyPath string) error {
 	proof, err := readFile(proofPath)
@@ -69,4 +38,45 @@ func readFile(filePath string) ([]byte, error) {
 		return nil, err
 	}
 	return data, nil
+}
+
+func runCLI() {
+	app := &cli.App{
+		Name:      "Aligned Layer Verifier",
+		UsageText: "\n" + "send-task plonk proof.txt public_input_file.txt " + "verification_key.txt" + "\n" + "send-task groth16 proof.txt public_input_file.txt " + "verification_key.txt",
+		Commands: []*cli.Command{
+			{
+				Name:  "send-task",
+				Usage: "verify a proof using a specified proof system",
+				Action: func(c *cli.Context) error {
+					if c.NArg() < 4 {
+						fmt.Println("Error: Insufficient arguments")
+						err := cli.ShowCommandHelp(c, "send-task")
+						if err != nil {
+							return err
+						}
+						return nil
+					}
+
+					system := c.Args().Get(0)
+					executableDir, err := os.Executable()
+					if err != nil {
+						return fmt.Errorf("error getting executable path: %v", err)
+					}
+					proofPath := filepath.Join(filepath.Dir(executableDir), c.Args().Get(1))
+					publicInputPath := filepath.Join(filepath.Dir(executableDir), c.Args().Get(2))
+					verificationKeyPath := filepath.Join(filepath.Dir(executableDir), c.Args().Get(3))
+
+					return verify(system, proofPath, publicInputPath, verificationKeyPath)
+				},
+				UsageText:   "send-task [SYSTEM] [PROOF_FILE] [PUBLIC_INPUT_FILE] [VERIFICATION_KEY_FILE]",
+				Description: "Verifies a proof using a specified proof system. Provide the name of the proof system (e.g., plonk or groth16) along with the paths to the proof file, public input file, and verification key file.",
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
