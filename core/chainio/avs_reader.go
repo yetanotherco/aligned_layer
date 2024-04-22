38a3d479d20ee051b23015e4c4e053edc05e9c88
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-04-22 14:49:02 -0300

    Add avs reader skeleton

---
+++ core/chainio/avs_reader.go
@@ -0,0 +1,70 @@
+package chainio
+
+import (
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+
+	sdkavsregistry "github.com/Layr-Labs/eigensdk-go/chainio/clients/avsregistry"
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients/eth"
+	logging "github.com/Layr-Labs/eigensdk-go/logging"
+
+	"github.com/yetanotherco/aligned_layer/core/config"
+)
+
+type AvsReader struct {
+	sdkavsregistry.AvsRegistryReader
+	AvsServiceBindings *AvsServiceBindings
+	logger             logging.Logger
+}
+
+func NewAvsReaderFromConfig(c *config.Config) (*AvsReader, error) {
+
+	avsContractBindings, err := NewAvsServiceBindings(c.AlignedLayerServiceManagerAddr, c.BlsOperatorStateRetrieverAddr, c.EthHttpClient, c.Logger)
+	if err != nil {
+		return nil, err
+	}
+	blsRegistryCoordinatorAddr, err := avsContractBindings.ServiceManager.RegistryCoordinator(&bind.CallOpts{})
+	if err != nil {
+		return nil, err
+	}
+	// stakeRegistryAddr, err := avsContractBindings.ServiceManager.StakeRegistry(&bind.CallOpts{})
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// blsPubkeyRegistryAddr, err := avsContractBindings.ServiceManager.BlsPubkeyRegistry(&bind.CallOpts{})
+	// if err != nil {
+	// 	return nil, err
+	// }
+	ethClient, err := eth.NewClient(c.EthRpcUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	avsRegistryReader, err := sdkavsregistry.BuildAvsRegistryChainReader(blsRegistryCoordinatorAddr, c.BlsOperatorStateRetrieverAddr, ethClient, c.Logger)
+	if err != nil {
+		return nil, err
+	}
+
+	// avsRegistryContractClient, err := sdkclients.NewAvsRegistryContractsChainClient(blsRegistryCoordinatorAddr, c.BlsOperatorStateRetrieverAddr, stakeRegistryAddr, blsPubkeyRegistryAddr, ethClient, c.Logger)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// avsRegistryReader, err := sdkavsregistry.NewAvsRegistryReader(avsRegistryContractClient, c.Logger, ethClient)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	avsServiceBindings, err := NewAvsServiceBindings(c.AlignedLayerServiceManagerAddr, c.BlsOperatorStateRetrieverAddr, c.EthHttpClient, c.Logger)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewAvsReader(avsRegistryReader, avsServiceBindings, c.Logger)
+}
+
+func NewAvsReader(avsRegistryReader sdkavsregistry.AvsRegistryReader, avsServiceBindings *AvsServiceBindings, logger logging.Logger) (*AvsReader, error) {
+	return &AvsReader{
+		AvsRegistryReader:  avsRegistryReader,
+		AvsServiceBindings: avsServiceBindings,
+		logger:             logger,
+	}, nil
+}
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/consensys/bavard v0.1.13 // indirect
 	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/crate-crypto/go-kzg-4844 v0.7.0 // indirect
 	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
@@ -19,6 +20,7 @@ require (
 	github.com/ethereum/go-ethereum v1.13.15 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/holiman/uint256 v1.2.4 // indirect
@@ -27,10 +29,12 @@ require (
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/supranational/blst v0.3.11 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/urfave/cli v1.22.14 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
