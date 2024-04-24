69fd2dba3def634b9521d3381c197613de39bbe3
Santos Rosati <rosatisantos@gmail.com>
2024-04-24 13:36:39 -0300

    feat (operator): register with aligned layer (WIP)

---
+++ operator/scripts/register_with_aligned_layer/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"encoding/json"
+	clitypes "github.com/Layr-Labs/eigenlayer-cli/pkg/types"
+	"github.com/Layr-Labs/eigensdk-go/crypto/bls"
+	ecdsa2 "github.com/Layr-Labs/eigensdk-go/crypto/ecdsa"
+	"github.com/Layr-Labs/eigensdk-go/types"
+	sdkutils "github.com/Layr-Labs/eigensdk-go/utils"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/urfave/cli"
+	"github.com/yetanotherco/aligned_layer/core/chainio"
+	"github.com/yetanotherco/aligned_layer/core/config"
+	"log"
+	"math/big"
+	"os"
+	"time"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "aligned_layer"
+	app.Usage = "Tool for registering operator to AlignedLayer AVS"
+	app.Flags = []cli.Flag{
+		config.ConfigFileFlag,
+	}
+
+	app.Action = registerWithAlignedLayer
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func registerWithAlignedLayer(ctx *cli.Context) error {
+	configPath := ctx.GlobalString(config.ConfigFileFlag.Name)
+	nodeConfig := clitypes.OperatorConfig{}
+	err := sdkutils.ReadYamlConfig(configPath, &nodeConfig)
+	if err != nil {
+		return err
+	}
+
+	configJson, err := json.MarshalIndent(nodeConfig, "", "  ")
+	if err != nil {
+		return err
+	}
+	log.Println("Config:", string(configJson))
+
+	ecdsaPrivateKeyPassword := os.Getenv("ECDSA_PRIVATE_KEY_PASSWORD")
+	if ecdsaPrivateKeyPassword == "" {
+		log.Println("ECDSA_PRIVATE_KEY_PASSWORD environment variable not set, using empty string")
+	}
+
+	privateKey, err := ecdsa2.ReadKey(nodeConfig.PrivateKeyStorePath, ecdsaPrivateKeyPassword)
+	if err != nil {
+		return err
+	}
+
+	blsPrivateKeyPassword := os.Getenv("BLS_PRIVATE_KEY_PASSWORD")
+	if blsPrivateKeyPassword == "" {
+		log.Println("BLS_PRIVATE_KEY_PASSWORD environment variable not set, using empty string")
+	}
+
+	blsKeyPair, err := bls.ReadPrivateKeyFromFile(nodeConfig.BlsPrivateKeyStorePath, blsPrivateKeyPassword)
+	if err != nil {
+		return err
+	}
+
+	quorumNumbers := []byte{0}
+
+	// Generate salt and expiry
+
+	privateKeyBytes := []byte(blsKeyPair.PrivKey.String())
+	salt := [32]byte{}
+
+	copy(salt[:], crypto.Keccak256([]byte("churn"), []byte(time.Now().String()), quorumNumbers, privateKeyBytes))
+
+	//ethClient, err := ethclient.Dial("http://localhost:8545")
+	//if err != nil {
+	//	return err
+	//}
+	//curBlockNum, err := ethClient.BlockNumber(context.Background())
+	//if err != nil {
+	//	return err
+	//}
+	//curBlock, err := ethClient.BlockByNumber(context.Background(), big.NewInt(int64(curBlockNum)))
+	//if err != nil {
+	//	return err
+	//}
+
+	//salt := [32]byte{123}
+
+	expiry := big.NewInt(time.Now().Add(10 * time.Minute).Unix())
+	quorumNumbersArr := types.QuorumNums{0}
+	socket := "Not Needed"
+
+	//expiry := big.NewInt((time.Now().Add(10 * time.Minute)).Unix())
+	err = RegisterOperator(context.Background(), blsKeyPair, socket, quorumNumbersArr, privateKey, salt, expiry)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RegisterOperator operator registers the operator with the given public key for the given quorum IDs.
+// RegisterOperator registers a new operator with the given public key and socket with the provided quorum ids.
+// If the operator is already registered with a given quorum id, the transaction will fail (noop) and an error
+// will be returned.
+func RegisterOperator(
+	ctx context.Context,
+	keypair *bls.KeyPair,
+	socket string,
+	quorumNumbers types.QuorumNums,
+	operatorEcdsaPrivateKey *ecdsa.PrivateKey,
+	operatorToAvsRegistrationSigSalt [32]byte,
+	operatorToAvsRegistrationSigExpiry *big.Int,
+) error {
+	writer, err := chainio.NewAvsWriterFromConfig()
+	if err != nil {
+		return err
+	}
+
+	_, err = writer.RegisterOperatorInQuorumWithAVSRegistryCoordinator(ctx, operatorEcdsaPrivateKey,
+		operatorToAvsRegistrationSigSalt, operatorToAvsRegistrationSigExpiry, keypair,
+		quorumNumbers, socket)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
