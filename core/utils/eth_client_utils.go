4d8c497eddcde8b88e7519eef71ed759c9c5d979
NicolasRampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-04-25 18:13:24 -0300

    feat: add util to wait for transaction receipt

---
+++ core/utils/eth_client_utils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"context"
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients/eth"
+	common2 "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"time"
+)
+
+func WaitForTransactionReceipt(client eth.Client, ctx context.Context, txHash common2.Hash) *types.Receipt {
+	for {
+		// verifying transaction receipt
+		receipt, err := client.TransactionReceipt(ctx, txHash)
+		if err != nil {
+			time.Sleep(2 * time.Second)
+		} else {
+			return receipt
+		}
+	}
+}
