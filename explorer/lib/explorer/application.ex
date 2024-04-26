ce43f59cb3ac58077c99ff497624863118b563b6
Urix <43704209+uri-99@users.noreply.github.com>
2024-04-26 15:33:31 -0300

    Feat: phoenix initial setup
    Co-authored-by: glpecile <gpecile@itba.edu.ar>

---
+++ explorer/lib/explorer/application.ex
@@ -0,0 +1,35 @@
+defmodule Explorer.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      ExplorerWeb.Telemetry,
+      {DNSCluster, query: Application.get_env(:explorer, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: Explorer.PubSub},
+      # Start the Finch HTTP client for sending emails
+      {Finch, name: Explorer.Finch},
+      # Start a worker by calling: Explorer.Worker.start_link(arg)
+      # {Explorer.Worker, arg},
+      # Start to serve requests, typically the last entry
+      ExplorerWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Explorer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    ExplorerWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
