9d63b958810d8ed19744ed644a7f1e80f49d6f41
NicolasRampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-04-29 16:48:04 -0300

    chore: add retry when listening for tasks

--- aggregator/internal/pkg/subscriber.go
+++ aggregator/internal/pkg/subscriber.go
@@ -1,12 +1,36 @@
 package pkg
 
-import "github.com/yetanotherco/aligned_layer/core/types"
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/yetanotherco/aligned_layer/core/types"
+	"time"
+)
+
+const (
+	MaxRetries    = 5
+	RetryInterval = 10 * time.Second
+)
 
 func (agg *Aggregator) SubscribeToNewTasks() error {
+
+	var err error
+
+	for attempt := 0; attempt < MaxRetries; attempt++ {
+		if err != nil {
+			err = tryCreateTaskSubscriber(agg)
+		} else {
+			err = agg.subscribeToNewTasks()
+		}
+	}
+
+	return err
+}
+
+func (agg *Aggregator) subscribeToNewTasks() error {
 	for {
 		select {
 		case err := <-agg.taskSubscriber.Err():
-			// TODO: Retry subscription
 			agg.AggregatorConfig.BaseConfig.Logger.Error("Error in subscription", "err", err)
 			return err
 		case task := <-agg.NewTaskCreatedChan:
@@ -25,4 +49,24 @@ func (agg *Aggregator) SubscribeToNewTasks() error {
 			agg.taskResponsesMutex.Unlock()
 		}
 	}
+}
+
+func tryCreateTaskSubscriber(agg *Aggregator) error {
+	var err error
+
+	for attempt := 0; attempt < MaxRetries; attempt++ {
+		agg.AggregatorConfig.BaseConfig.Logger.Info("Subscribing to Ethereum serviceManager task events")
+		agg.taskSubscriber, err = agg.avsSubscriber.AvsContractBindings.ServiceManager.WatchNewTaskCreated(&bind.WatchOpts{},
+			agg.NewTaskCreatedChan, nil)
+
+		if err != nil {
+			message := fmt.Sprintf("Failed to create task subscriber, waiting %d seconds before retrying", RetryInterval/time.Second)
+			agg.AggregatorConfig.BaseConfig.Logger.Info(message, "err", err)
+			time.Sleep(RetryInterval)
+		} else {
+			break
+		}
+	}
+
+	return err
 }
