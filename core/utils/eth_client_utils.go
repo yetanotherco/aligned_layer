0d8ab3664c791278ac803a87832a1b7a03c4f859
JuArce <52429267+JuArce@users.noreply.github.com>
2024-04-30 11:22:25 -0300

    chore: add max retries for waitForTransactionReceipt

--- core/utils/eth_client_utils.go
+++ core/utils/eth_client_utils.go
@@ -2,19 +2,24 @@ package utils
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/Layr-Labs/eigensdk-go/chainio/clients/eth"
 	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"time"
 )
 
-func WaitForTransactionReceipt(client eth.Client, ctx context.Context, txHash gethcommon.Hash) *types.Receipt {
-	for {
+var maxRetries = 10
+
+func WaitForTransactionReceipt(client eth.Client, ctx context.Context, txHash gethcommon.Hash) (*types.Receipt, error) {
+	for i := 0; i < maxRetries; i++ {
 		receipt, err := client.TransactionReceipt(ctx, txHash)
 		if err != nil {
 			time.Sleep(2 * time.Second)
 		} else {
-			return receipt
+			return receipt, nil
 		}
 	}
+	return nil, errors.New(fmt.Sprintf("Transaction receipt not found for txHash: %s", txHash.String()))
 }
