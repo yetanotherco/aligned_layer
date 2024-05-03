0a90cf0d3caf436ba2eda4c4aa11b18ce5c65d2e
Urix <43704209+uri-99@users.noreply.github.com>
2024-05-03 18:16:10 -0300

    refactor: changed integration test core to bash, because bash is king

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -8,95 +8,38 @@ import (
 	"math/big"
 	"net/http"
 	"os"
-	// "time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/stretchr/testify/assert"
-	"github.com/yetanotherco/aligned_layer/aggregator/pkg"
-	"github.com/yetanotherco/aligned_layer/core/config"
-	operator "github.com/yetanotherco/aligned_layer/operator/pkg"
-
-	"bytes"
-	"os/exec"
 
 	"regexp"
 	"strings"
 	"testing"
 )
 
-func TestIntegration(t *testing.T) {
+func TestEventsReader(t *testing.T) {
 	fmt.Println("Running integration test")
 	err := os.Chdir("../")
 	assert.Nil(t, err, "Could not change directory to project root")
 
 	configFilePath := "config-files/config-test.yaml"
 
-	// check anvil is running
 	result, anvilPort, alignedContractAddress := checkAnvilIsRunning(configFilePath)
 	if !result {
 		t.Fatalf("Expected Anvil to be running, in port %s but it was not.", anvilPort)
 	}
-	// fmt.Println("alignedContractAddress: " + alignedContractAddress)
 
-	// Setup RPC client
 	client, err := ethclient.Dial("http://localhost:" + anvilPort)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// start aggregator
-	aggregator := buildAggregator(t, configFilePath)
-	go func() {
-		err := aggregator.ServeOperators()
-		assert.Nil(t, err, "Could not start aggregator")
-	}()
-	fmt.Println("Aggregator started")
-
-	// register operator
-	var out bytes.Buffer
-	cmd := exec.Command("make", "operator-full-registration") //this is running a bit wonky?
-	cmd.Stdout = &out
-	err = cmd.Run()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-	}
-	fmt.Println("Operator registered")
-	fmt.Println("Output:", out.String())
-
-	// start operator
-	op := buildOperator(t, configFilePath)
-	go func() {
-		err := op.Start(context.Background())
-		assert.Nil(t, err, "Could not start operator")
-	}()
-	fmt.Println("Operator started")
-
-	// send task
-	cmd = exec.Command("make", "send-plonk_bls12_381-proof")
-	cmd.Stdout = &out
-	err = cmd.Run()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-	}
-	fmt.Println("Task sent successfully")
-
-	// send task #2
-	// var out bytes.Buffer
-	cmd = exec.Command("make", "send-plonk_bn254-proof")
-	// cmd.Stdout = &out
-	err = cmd.Run()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-	}
-	fmt.Println("Task #2 sent successfully")
-
 	// check if contract sent event
 	query := ethereum.FilterQuery{
 		FromBlock: big.NewInt(0),
-		// ToBlock:   big.NewInt(2394201),
 		Addresses: []common.Address{
 			common.HexToAddress(alignedContractAddress),
 		},
@@ -111,54 +54,44 @@ func TestIntegration(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(contractAbi)
 
-	// fmt.Println("value")
-	// fmt.Print(logs)
+	NewTaskCreatedEventSignature := contractAbi.Events["NewTaskCreated"].ID.Hex()
+	TaskRespondedEventSignature := contractAbi.Events["TaskResponded"].ID.Hex()
+
 	assert.NotEmpty(t, logs, "No New Events found")
-	fmt.Println("Events found")
-	fmt.Println("logs:")
-	fmt.Println(logs)
+
+	var taskCreatedEvents = 0
+	var taskRespondedEvents = 0
 
 	for _, vLog := range logs {
-		value, err := contractAbi.Unpack("NewTaskCreated", vLog.Data)
-		if err != nil {
-			fmt.Println("err")
-			log.Fatal(err)
-		} else {
-			fmt.Println("value")
-			fmt.Println(value)
+		switch vLog.Topics[0].Hex() {
+		case NewTaskCreatedEventSignature:
+			if taskCreatedEvents == 0 {
+				assert.Equal(t, "0x0000000000000000000000000000000000000000000000000000000000000000", vLog.Topics[1].Hex(), "Expected NewTaskCreated event with taskId 0")
+			} else if taskCreatedEvents == 1 {
+				assert.Equal(t, "0x0000000000000000000000000000000000000000000000000000000000000001", vLog.Topics[1].Hex(), "Expected NewTaskCreated event with taskId 1")
+			} else {
+				assert.Fail(t, "Too many NewTaskCreated events")
+			}
+			taskCreatedEvents++
+
+		case TaskRespondedEventSignature:
+			if taskRespondedEvents == 0 {
+				assert.Equal(t, "0x0000000000000000000000000000000000000000000000000000000000000000", vLog.Topics[1].Hex(), "Expected NewTaskCreated event with taskId 0")
+			} else if taskRespondedEvents == 1 {
+				assert.Equal(t, "0x0000000000000000000000000000000000000000000000000000000000000001", vLog.Topics[1].Hex(), "Expected NewTaskCreated event with taskId 1")
+			} else {
+				assert.Fail(t, "Too many TaskResponded events")
+			}
+			taskRespondedEvents++
+
+		default:
+			assert.Fail(t, "Unknown event")
+			fmt.Println("Unknown event")
 		}
-
-		// fmt.Println("topics")
-		// var topics [4]string
-		// for i := range vLog.Topics {
-		// 	fmt.Println("topic")
-		// 	topics[i] = vLog.Topics[i].Hex()
-		// 	fmt.Println(topics[i])
-		// }
-		// if id == 0 {
-		// 	assert.Equal(t, topics[1], 0x0, "Expected NewTaskCreated event with taskId 0")
-		// } else if id == 1 {
-		// 	assert.Equal(t, topics[1], 0x1, "Expected NewTaskCreated event with taskId 1")
-		// }
 	}
-
-	assert.Equal(t, "0x0000000000000000000000000000000000000000000000000000000000000000", logs[0].Topics[1].Hex(), "Expected NewTaskCreated event with taskId 0")
-	assert.Equal(t, "0x0000000000000000000000000000000000000000000000000000000000000001", logs[1].Topics[1].Hex(), "Expected NewTaskCreated event with taskId 0")
-
-	// 	assert.NotEmpty(t, value)
-	// 	fmt.Println("TaskCreated Event found? ::")
-	// 	fmt.Println(t)
-
-	// 	// fmt.Println(string(event.Key[:]))   // foo
-	// 	// fmt.Println(string(event.Value[:])) // bar
-	// }
-
-	//check if aggregator send to operator
-
-	//check if aggregator
-
+	assert.Equal(t, 2, taskCreatedEvents, "Expected 2 NewTaskCreated events")
+	assert.Equal(t, 2, taskRespondedEvents, "Expected 2 TaskResponded events")
 }
 
 func getAlignedABI(abiFilePath string) (abi.ABI, error) {
@@ -211,23 +144,4 @@ func checkAnvilIsRunning(configFilePath string) (bool, string, string) {
 	}
 
 	return true, port, avsServiceManagerAddress
-}
-
-func buildAggregator(t *testing.T, configFile string) *pkg.Aggregator {
-	aggregatorConfig := config.NewAggregatorConfig(configFile)
-
-	aggregator, err := pkg.NewAggregator(*aggregatorConfig)
-	assert.Nil(t, err)
-
-	return aggregator
-}
-
-func buildOperator(t *testing.T, configFile string) *operator.Operator {
-	//TODO missing register Operator to Aggregator
-	operatorConfig := config.NewOperatorConfig(configFile)
-
-	opereator, err := operator.NewOperatorFromConfig(*operatorConfig)
-	assert.Nil(t, err)
-
-	return opereator
 }
--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -1,8 +1,31 @@
-
-make anvil-start &> /dev/null &
-
+echo starting anvil
+make anvil-start & #&> /dev/null &
 export ANVIL_PID=$!
 
-go test tests/integration_test.go -v
+echo registering operator
+make operator-full-registration #&> /dev/null
 
+# echo starting aggregator
+make aggregator-start & #&> /dev/null &
+export AGGREGATOR_PID=$!
+
+sleep 5
+
+echo starting operator
+make operator-start &> /dev/null &
+export OPERATOR_PID=$!
+
+sleep 3
+
+echo sending task
+make send-plonk_bls12_381-proof &> /dev/null
+
+echo sending task
+make send-plonk_bn254-proof &> /dev/null
+
+sleep 3600
+
+# go test tests/integration_test.go -v
 kill ${ANVIL_PID}
+kill ${AGGREGATOR_PID}
+kill ${OPERATOR_PID}
