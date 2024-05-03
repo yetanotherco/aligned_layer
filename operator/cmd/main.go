8e8de9cfa7aa4149d1528b946a8c45a46bcd5346
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-05-03 16:05:27 -0300

    Refactor operator main

--- operator/cmd/main.go
+++ operator/cmd/main.go
@@ -3,23 +3,45 @@ package main
 import (
 	"context"
 	"log"
+	"math/big"
 	"os"
+	"time"
 
+	"github.com/Layr-Labs/eigensdk-go/types"
 	sdkutils "github.com/Layr-Labs/eigensdk-go/utils"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/urfave/cli/v2"
+	"github.com/yetanotherco/aligned_layer/core/chainio"
 	"github.com/yetanotherco/aligned_layer/core/config"
 	operator "github.com/yetanotherco/aligned_layer/operator/pkg"
 )
 
-var flags = []cli.Flag{
+var registerFlags = []cli.Flag{
+	config.ConfigFileFlag,
+}
+var startFlags = []cli.Flag{
 	config.ConfigFileFlag,
 }
 
 func main() {
 	app := &cli.App{
-		Name:   "Aligned Layer Operator",
-		Flags:  flags,
-		Action: operatorMain,
+		Name: "Aligned Layer Node Operator",
+		Commands: []*cli.Command{
+			{
+				Name:        "register",
+				Usage:       "Send a single task to the verifier",
+				Description: "Service that sends proofs to verify by operator nodes.",
+				Flags:       registerFlags,
+				Action:      registerOperatorMain,
+			},
+			{
+				Name:        "start",
+				Usage:       "Send a task every `INTERVAL` seconds",
+				Description: "Service that sends proofs to verify by operator nodes.",
+				Flags:       startFlags,
+				Action:      operatorMain,
+			},
+		},
 	}
 
 	err := app.Run(os.Args)
@@ -48,6 +70,61 @@ func operatorMain(ctx *cli.Context) error {
 	}
 
 	log.Println("Operator started")
+
+	return nil
+}
+
+func registerOperatorMain(ctx *cli.Context) error {
+	config := config.NewOperatorConfig(ctx.String(config.ConfigFileFlag.Name))
+
+	quorumNumbers := []byte{0}
+
+	// Generate salt and expiry
+	privateKeyBytes := []byte(config.BlsConfig.KeyPair.PrivKey.String())
+	salt := [32]byte{}
+
+	copy(salt[:], crypto.Keccak256([]byte("churn"), []byte(time.Now().String()), quorumNumbers, privateKeyBytes))
+
+	expiry := big.NewInt(time.Now().Add(10 * time.Minute).Unix())
+	quorumNumbersArr := types.QuorumNums{0}
+	socket := "Not Needed"
+
+	err := registerOperator(context.Background(), config,
+		socket, quorumNumbersArr, salt, expiry)
+	if err != nil {
+		config.BaseConfig.Logger.Error("Failed to register operator", "err", err)
+		return err
+	}
+
+	return nil
+}
+
+// RegisterOperator operator registers the operator with the given public key for the given quorum IDs.
+// RegisterOperator registers a new operator with the given public key and socket with the provided quorum ids.
+// If the operator is already registered with a given quorum id, the transaction will fail (noop) and an error
+// will be returned.
+func registerOperator(
+	ctx context.Context,
+	configuration *config.OperatorConfig,
+	socket string,
+	quorumNumbers types.QuorumNums,
+	operatorToAvsRegistrationSigSalt [32]byte,
+	operatorToAvsRegistrationSigExpiry *big.Int,
+) error {
+	writer, err := chainio.NewAvsWriterFromConfig(configuration.BaseConfig, configuration.EcdsaConfig)
+	if err != nil {
+		configuration.BaseConfig.Logger.Error("Failed to create AVS writer", "err", err)
+		return err
+	}
+
+	_, err = writer.RegisterOperatorInQuorumWithAVSRegistryCoordinator(ctx, configuration.EcdsaConfig.PrivateKey,
+		operatorToAvsRegistrationSigSalt, operatorToAvsRegistrationSigExpiry, configuration.BlsConfig.KeyPair,
+		quorumNumbers, socket)
+
+	if err != nil {
+		configuration.BaseConfig.Logger.Error("Failed to register operator", "err", err)
+		return err
+	}
 
 	return nil
 }
--- operator/scripts/register_with_aligned_layer/main.go
+++ operator/scripts/register_with_aligned_layer/main.go
@@ -1,87 +1,87 @@
-package main
+// package main
 
-import (
-	"context"
-	"log"
-	"math/big"
-	"os"
-	"time"
+// import (
+// 	"context"
+// 	"log"
+// 	"math/big"
+// 	"os"
+// 	"time"
 
-	"github.com/Layr-Labs/eigensdk-go/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/urfave/cli/v2"
-	"github.com/yetanotherco/aligned_layer/core/chainio"
-	"github.com/yetanotherco/aligned_layer/core/config"
-)
+// 	"github.com/Layr-Labs/eigensdk-go/types"
+// 	"github.com/ethereum/go-ethereum/crypto"
+// 	"github.com/urfave/cli/v2"
+// 	"github.com/yetanotherco/aligned_layer/core/chainio"
+// 	"github.com/yetanotherco/aligned_layer/core/config"
+// )
 
-var flags = []cli.Flag{
-	config.ConfigFileFlag,
-}
+// var flags = []cli.Flag{
+// 	config.ConfigFileFlag,
+// }
 
-func main() {
-	app := cli.NewApp()
-	app.Name = "aligned_layer"
-	app.Usage = "Tool for registering operator to AlignedLayer AVS"
-	app.Flags = flags
-	app.Action = registerWithAlignedLayer
+// func main() {
+// 	app := cli.NewApp()
+// 	app.Name = "aligned_layer"
+// 	app.Usage = "Tool for registering operator to AlignedLayer AVS"
+// 	app.Flags = flags
+// 	app.Action = registerWithAlignedLayer
 
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+// 	err := app.Run(os.Args)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
 
-func registerWithAlignedLayer(ctx *cli.Context) error {
-	configuration := config.NewOperatorConfig(ctx.String(config.ConfigFileFlag.Name))
+// func registerWithAlignedLayer(ctx *cli.Context) error {
+// 	configuration := config.NewOperatorConfig(ctx.String(config.ConfigFileFlag.Name))
 
-	quorumNumbers := []byte{0}
+// 	quorumNumbers := []byte{0}
 
-	// Generate salt and expiry
-	privateKeyBytes := []byte(configuration.BlsConfig.KeyPair.PrivKey.String())
-	salt := [32]byte{}
+// 	// Generate salt and expiry
+// 	privateKeyBytes := []byte(configuration.BlsConfig.KeyPair.PrivKey.String())
+// 	salt := [32]byte{}
 
-	copy(salt[:], crypto.Keccak256([]byte("churn"), []byte(time.Now().String()), quorumNumbers, privateKeyBytes))
+// 	copy(salt[:], crypto.Keccak256([]byte("churn"), []byte(time.Now().String()), quorumNumbers, privateKeyBytes))
 
-	expiry := big.NewInt(time.Now().Add(10 * time.Minute).Unix())
-	quorumNumbersArr := types.QuorumNums{0}
-	socket := "Not Needed"
+// 	expiry := big.NewInt(time.Now().Add(10 * time.Minute).Unix())
+// 	quorumNumbersArr := types.QuorumNums{0}
+// 	socket := "Not Needed"
 
-	err := RegisterOperator(context.Background(), configuration,
-		socket, quorumNumbersArr, salt, expiry)
-	if err != nil {
-		configuration.BaseConfig.Logger.Error("Failed to register operator", "err", err)
-		return err
-	}
+// 	err := RegisterOperator(context.Background(), configuration,
+// 		socket, quorumNumbersArr, salt, expiry)
+// 	if err != nil {
+// 		configuration.BaseConfig.Logger.Error("Failed to register operator", "err", err)
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-// RegisterOperator operator registers the operator with the given public key for the given quorum IDs.
-// RegisterOperator registers a new operator with the given public key and socket with the provided quorum ids.
-// If the operator is already registered with a given quorum id, the transaction will fail (noop) and an error
-// will be returned.
-func RegisterOperator(
-	ctx context.Context,
-	configuration *config.OperatorConfig,
-	socket string,
-	quorumNumbers types.QuorumNums,
-	operatorToAvsRegistrationSigSalt [32]byte,
-	operatorToAvsRegistrationSigExpiry *big.Int,
-) error {
-	writer, err := chainio.NewAvsWriterFromConfig(configuration.BaseConfig, configuration.EcdsaConfig)
-	if err != nil {
-		configuration.BaseConfig.Logger.Error("Failed to create AVS writer", "err", err)
-		return err
-	}
+// // RegisterOperator operator registers the operator with the given public key for the given quorum IDs.
+// // RegisterOperator registers a new operator with the given public key and socket with the provided quorum ids.
+// // If the operator is already registered with a given quorum id, the transaction will fail (noop) and an error
+// // will be returned.
+// func RegisterOperator(
+// 	ctx context.Context,
+// 	configuration *config.OperatorConfig,
+// 	socket string,
+// 	quorumNumbers types.QuorumNums,
+// 	operatorToAvsRegistrationSigSalt [32]byte,
+// 	operatorToAvsRegistrationSigExpiry *big.Int,
+// ) error {
+// 	writer, err := chainio.NewAvsWriterFromConfig(configuration.BaseConfig, configuration.EcdsaConfig)
+// 	if err != nil {
+// 		configuration.BaseConfig.Logger.Error("Failed to create AVS writer", "err", err)
+// 		return err
+// 	}
 
-	_, err = writer.RegisterOperatorInQuorumWithAVSRegistryCoordinator(ctx, configuration.EcdsaConfig.PrivateKey,
-		operatorToAvsRegistrationSigSalt, operatorToAvsRegistrationSigExpiry, configuration.BlsConfig.KeyPair,
-		quorumNumbers, socket)
+// 	_, err = writer.RegisterOperatorInQuorumWithAVSRegistryCoordinator(ctx, configuration.EcdsaConfig.PrivateKey,
+// 		operatorToAvsRegistrationSigSalt, operatorToAvsRegistrationSigExpiry, configuration.BlsConfig.KeyPair,
+// 		quorumNumbers, socket)
 
-	if err != nil {
-		configuration.BaseConfig.Logger.Error("Failed to register operator", "err", err)
-		return err
-	}
+// 	if err != nil {
+// 		configuration.BaseConfig.Logger.Error("Failed to register operator", "err", err)
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
