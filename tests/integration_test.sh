3ac7ded3e914628bde0cc8c292f30683e50d4b61
Urix <43704209+uri-99@users.noreply.github.com>
2024-05-06 18:25:56 -0300

    refactor: prints

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -4,34 +4,41 @@ export ANVIL_PID=$!
 
 sleep 3
 
-echo Registering Operator
+echo "\nRegistering Operator"
 make operator-full-registration #&> /dev/null
 
-echo Starting Aggregator
+echo "\nStarting Aggregator"
 make aggregator-start & #> /dev/null &
 export AGGREGATOR_PID=$!
 
 sleep 5
 
-echo Starting Operator
+echo "\nStarting Operator"
 make operator-start & #> /dev/null &
 export OPERATOR_PID=$!
 
 sleep 15
 
-echo "Sending Task 1" 
+echo "\nSending Task 1" 
 make send-plonk_bls12_381-proof #> /dev/null
 
-echo "Sending Task 2"
+echo "\nSending Task 2"
 make send-plonk_bn254-proof #> /dev/null
 
 sleep 10
 
-echo "Verifying Tasks sent & accepted"
+echo "\nVerifying Tasks sent & accepted"
 
-go test tests/verify_test.go -v
+output=$(go test tests/verify_test.go -v)
+echo $output
 
-## Uncomment if running locally:
-# kill ${ANVIL_PID}
-# kill ${AGGREGATOR_PID}
-# kill ${OPERATOR_PID}
+## Crashes the CI, only run locally
+if $LOCAL ; then
+    echo "\nStopping Anvil"
+    kill ${ANVIL_PID}
+    echo "\nStopping Aggregator"
+    kill ${AGGREGATOR_PID}
+    echo "\nStopping Operator"
+    kill ${OPERATOR_PID}
+fi
+
