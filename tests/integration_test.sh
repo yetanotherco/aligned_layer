b089fca66099106b466210c307fc3fe65e435f9e
Urix <43704209+uri-99@users.noreply.github.com>
2024-05-06 16:26:32 -0300

    refactor: sending output to /dev/null

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -1,29 +1,29 @@
 echo Starting anvil
-make anvil-start &> /dev/null &
+make anvil-start > /dev/null &
 export ANVIL_PID=$!
 
 sleep 3
 
 echo Registering Operator
-make operator-full-registration #&> /dev/null
+make operator-full-registration > /dev/null
 
 echo Starting Aggregator
-make aggregator-start &> /dev/null &
+make aggregator-start > /dev/null &
 export AGGREGATOR_PID=$!
 
 sleep 5
 
 echo Starting Operator
-make operator-start &> /dev/null &
+make operator-start > /dev/null &
 export OPERATOR_PID=$!
 
 sleep 15
 
 echo "Sending Task 1" 
-make send-plonk_bls12_381-proof &> /dev/null
+make send-plonk_bls12_381-proof > /dev/null
 
 echo "Sending Task 2"
-make send-plonk_bn254-proof &> /dev/null
+make send-plonk_bn254-proof > /dev/null
 
 sleep 10
 
@@ -33,6 +33,7 @@ go test tests/integration_test.go -v
 
 echo "DONE"
 
-kill ${ANVIL_PID}
-kill ${AGGREGATOR_PID}
-kill ${OPERATOR_PID}
+# if running locally:
+# kill ${ANVIL_PID}
+# kill ${AGGREGATOR_PID}
+# kill ${OPERATOR_PID}
