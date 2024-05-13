5146b6532b366e0c1a5d63d23c5ffce7a7fed39c
Santos Rosati <rosatisantos@gmail.com>
2024-05-13 18:28:43 -0300

    fix: uncomment posting to blob

--- task_sender/pkg/blobs.go
+++ task_sender/pkg/blobs.go
@@ -8,11 +8,15 @@ import (
 	"errors"
 	"fmt"
 	"github.com/Layr-Labs/eigensdk-go/chainio/clients/eth"
+	"github.com/ethereum/go-ethereum/consensus/misc/eip4844"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/crypto/kzg4844"
 	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/holiman/uint256"
 	"github.com/yetanotherco/aligned_layer/common"
 	serviceManager "github.com/yetanotherco/aligned_layer/contracts/bindings/AlignedLayerServiceManager"
+	"github.com/yetanotherco/aligned_layer/core/utils"
 	"io"
 	"log"
 	"math/big"
@@ -68,118 +72,109 @@ func (ts *TaskSender) PostProofOnBlobs(proof []byte) (*serviceManager.AlignedLay
 		proofs[idx] = BlobProof
 
 	}
-	//
-	//// Prepare the sidecar data for the transaction, which includes the blob and its cryptographic proof
-	//sidecar := types.BlobTxSidecar{
-	//	Blobs:       blobs,
-	//	Commitments: commitments,
-	//	Proofs:      proofs,
-	//}
-	//
-	//// Compute the sender's address from the public key
-	//fromAddress := crypto.PubkeyToAddress(ts.EcdsaPrivKey.PublicKey)
-	//
-	//// Retrieve the nonce for the transaction
-	//nonce, err := ts.blobsConfig.EthRpcClient.PendingNonceAt(context.Background(), fromAddress)
-	//if err != nil {
-	//	return nil, fmt.Errorf("failed to get nonce: %s", err)
-	//}
-	//
-	//lastBlock, err := ts.blobsConfig.EthRpcClient.BlockByNumber(context.Background(), nil)
-	//if err != nil {
-	//	return nil, fmt.Errorf("failed to get last block: %s", err)
-	//}
-	//
-	//excessBlobGas := lastBlock.ExcessBlobGas()
-	//calcBlobFee := eip4844.CalcBlobFee(*excessBlobGas)
-	//
-	//blobFeeCap := uint256.MustFromBig(calcBlobFee)
-	//
-	//blobFeeCap.Mul(blobFeeCap, uint256.NewInt(10))
-	//
-	//tip := lastBlock.BaseFee().Mul(lastBlock.BaseFee(), big.NewInt(2))
-	//maxFeePerGas := lastBlock.BaseFee().Add(lastBlock.BaseFee(), tip)
-	//
-	//blobGasUsed := *lastBlock.BlobGasUsed()
-	//transactions := lastBlock.Transactions()
-	//count := 0
-	//
-	//// Calculate the total number of blob txs
-	//for _, tx := range transactions {
-	//	if tx.BlobHashes() != nil {
-	//		count++
-	//	}
-	//}
-	//
-	//var gas uint64
-	//if count == 0 {
-	//	// No blob transactions in the last block, default gas
-	//	gas = 100_000
-	//} else {
-	//	gas = blobGasUsed / uint64(count)
-	//}
-	//
-	//chainId, err := ts.blobsConfig.EthRpcClient.ChainID(context.Background())
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//tx := types.NewTx(&types.BlobTx{
-	//	ChainID:    uint256.MustFromBig(chainId),
-	//	Nonce:      nonce,
-	//	GasTipCap:  uint256.MustFromBig(tip),
-	//	GasFeeCap:  uint256.MustFromBig(maxFeePerGas),
-	//	Gas:        gas,
-	//	Value:      uint256.NewInt(0),
-	//	Data:       nil,
-	//	BlobFeeCap: blobFeeCap,
-	//	BlobHashes: sidecar.BlobHashes(),
-	//	Sidecar:    &sidecar,
-	//})
-	//
-	//signedTx, err := types.SignTx(tx, types.LatestSignerForChainID(chainId), ts.EcdsaPrivKey)
-	//if err != nil {
-	//	return nil, fmt.Errorf("failed to sign transaction: %s", err)
-	//}
-	//
-	//// Send the signed transaction to the Ethereum network
-	//err = ts.blobsConfig.EthRpcClient.SendTransaction(context.Background(), signedTx)
-	//if err != nil {
-	//	return nil, fmt.Errorf("failed to send transaction: %s", err)
-	//}
-	//
-	//txHash := signedTx.Hash()
-	//
-	//log.Println("Blobs sent. Transaction hash:", txHash.String())
-	//log.Println("Waiting for transaction receipt...")
-	//
-	//receipt, err := utils.WaitForTransactionReceiptMaxRetries(ts.blobsConfig.EthRpcClient, context.Background(), txHash, 20)
-	//if err != nil {
-	//	return nil, fmt.Errorf("failed to get transaction receipt: %s", err)
-	//}
-	//
-	//blockNumber := receipt.BlockNumber
-	//log.Println("Waiting for the next block to get the beacon root...")
-	//
-	//// Add one because the beacon root of the current block is the parent beacon root of the next block.
-	//blockNumber.Add(blockNumber, big.NewInt(1))
-	//block, err := waitForBlock(ts.blobsConfig.EthRpcClient, blockNumber)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//
-	//consensusResponse, err := ts.getResponseFromBeaconRoot(block.BeaconRoot().Bytes())
-	//if err != nil {
-	//	return nil, err
-	//}
 
-	beaconRoot, err := hex.DecodeString("29f1563f4286c02b6eeb4b8bd84ab363c6271741e2826a824f73605bb8d7572d")
+	// Prepare the sidecar data for the transaction, which includes the blob and its cryptographic proof
+	sidecar := types.BlobTxSidecar{
+		Blobs:       blobs,
+		Commitments: commitments,
+		Proofs:      proofs,
+	}
+
+	// Compute the sender's address from the public key
+	fromAddress := crypto.PubkeyToAddress(ts.EcdsaPrivKey.PublicKey)
+
+	// Retrieve the nonce for the transaction
+	nonce, err := ts.blobsConfig.EthRpcClient.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get nonce: %s", err)
+	}
+
+	lastBlock, err := ts.blobsConfig.EthRpcClient.BlockByNumber(context.Background(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get last block: %s", err)
+	}
+
+	excessBlobGas := lastBlock.ExcessBlobGas()
+	calcBlobFee := eip4844.CalcBlobFee(*excessBlobGas)
+
+	blobFeeCap := uint256.MustFromBig(calcBlobFee)
+
+	blobFeeCap.Mul(blobFeeCap, uint256.NewInt(10))
+
+	tip := lastBlock.BaseFee().Mul(lastBlock.BaseFee(), big.NewInt(2))
+	maxFeePerGas := lastBlock.BaseFee().Add(lastBlock.BaseFee(), tip)
+
+	blobGasUsed := *lastBlock.BlobGasUsed()
+	transactions := lastBlock.Transactions()
+	count := 0
+
+	// Calculate the total number of blob txs
+	for _, tx := range transactions {
+		if tx.BlobHashes() != nil {
+			count++
+		}
+	}
+
+	var gas uint64
+	if count == 0 {
+		// No blob transactions in the last block, default gas
+		gas = 100_000
+	} else {
+		gas = blobGasUsed / uint64(count)
+	}
+
+	chainId, err := ts.blobsConfig.EthRpcClient.ChainID(context.Background())
 	if err != nil {
 		return nil, err
 	}
 
-	consensusResponse, err := ts.getResponseFromBeaconRoot(beaconRoot)
+	tx := types.NewTx(&types.BlobTx{
+		ChainID:    uint256.MustFromBig(chainId),
+		Nonce:      nonce,
+		GasTipCap:  uint256.MustFromBig(tip),
+		GasFeeCap:  uint256.MustFromBig(maxFeePerGas),
+		Gas:        gas,
+		Value:      uint256.NewInt(0),
+		Data:       nil,
+		BlobFeeCap: blobFeeCap,
+		BlobHashes: sidecar.BlobHashes(),
+		Sidecar:    &sidecar,
+	})
+
+	signedTx, err := types.SignTx(tx, types.LatestSignerForChainID(chainId), ts.EcdsaPrivKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to sign transaction: %s", err)
+	}
+
+	// Send the signed transaction to the Ethereum network
+	err = ts.blobsConfig.EthRpcClient.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send transaction: %s", err)
+	}
+
+	txHash := signedTx.Hash()
+
+	log.Println("Blobs sent. Transaction hash:", txHash.String())
+	log.Println("Waiting for transaction receipt...")
+
+	receipt, err := utils.WaitForTransactionReceiptMaxRetries(ts.blobsConfig.EthRpcClient, context.Background(), txHash, 20)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get transaction receipt: %s", err)
+	}
+
+	blockNumber := receipt.BlockNumber
+	log.Println("Waiting for the next block to get the beacon root...")
+
+	// Add one because the beacon root of the current block is the parent beacon root of the next block.
+	blockNumber.Add(blockNumber, big.NewInt(1))
+	block, err := waitForBlock(ts.blobsConfig.EthRpcClient, blockNumber)
+	if err != nil {
+		return nil, err
+	}
+
+	beaconRoot := block.BeaconRoot().Bytes()
+
+	consensusResponse, err := ts.getResponseFromBeaconRoot(block.BeaconRoot().Bytes())
 	if err != nil {
 		return nil, err
 	}
@@ -209,7 +204,6 @@ func (ts *TaskSender) PostProofOnBlobs(proof []byte) (*serviceManager.AlignedLay
 		}
 
 		daChunks[idx] = serviceManager.AlignedLayerServiceManagerDAPayloadChunk{
-			//ProofAssociatedData: block.BeaconRoot().Bytes(),
 			ProofAssociatedData: beaconRoot,
 			Index:               uint64(txIdx),
 		}
