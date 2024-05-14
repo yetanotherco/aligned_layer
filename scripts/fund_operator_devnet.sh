3d261c0bd3d3c55c2d1d1f1e7ca43cc2f0872f80
Uriel Mihura <43704209+uri-99@users.noreply.github.com>
2024-05-14 12:16:08 -0300

    fix: fund_operator_devnet.sh bug (#112)
    Co-authored-by: Mauro Toscano <12560266+MauroToscano@users.noreply.github.com>
    Co-authored-by: Mariano A. Nicolini <mariano.nicolini.91@gmail.com>

--- scripts/fund_operator_devnet.sh
+++ scripts/fund_operator_devnet.sh
@@ -3,14 +3,14 @@
 # Check that OPERATOR_ADDRESS is not empty
 if [[ "$OPERATOR_ADDRESS" -eq "" ]]; then
   echo "OPERATOR_ADDRESS is empty, using default value 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-  OPERATOR_ADDRESS="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  OPERATOR_ADDRESS=0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 fi;
 
-# Ethereum sender address - anvil address 1
-sender_address="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+# Ethereum sender address - anvil address 2
+sender_address="0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
 
-# Ethereum sender private key - anvil private key 1
-sender_private_key="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+# Ethereum sender private key - anvil private key 2
+sender_private_key="0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
 
 # Recipient address
 recipient_address=$OPERATOR_ADDRESS
