5459a6ec3ffb82ea51db5e4527d5edb7798fd3ed
MauroFab <maurotoscano2@gmail.com>
2024-05-15 15:58:27 -0300

    Simplified first compiling version

--- contracts/src/core/AlignedLayerServiceManager.sol
+++ contracts/src/core/AlignedLayerServiceManager.sol
@@ -17,27 +17,30 @@ import {IStakeRegistry} from "eigenlayer-middleware/interfaces/IStakeRegistry.so
  * - freezing operators as the result of various "challenges"
  */
 contract AlignedLayerServiceManager is ServiceManagerBase, BLSSignatureChecker {
+
     address aggregator;
 
     // EVENTS
     event NewTaskCreated(
         bytes32 batchTaskHash,
-        string batchDataPointer,
+        string batchDataPointer
     );
 
     event BatchVerified(bytes32 batchMerkleRoot);
 
     uint256 internal constant _THRESHOLD_DENOMINATOR = 100;
-    bytes[] internal constant QUORUM_NUMBERS = [0];
     uint8 internal constant QUORUM_THRESHOLD_PERCENTAGE = 67;
 
     struct BatchState {
         uint32 taskCreatedBlock;
         bool responded;
     }
+    //mapping(uint32 => bytes32) public taskHashes;
 
     /* STORAGE */
-    mapping(bytes32 => BatchState)
+    mapping(bytes32 => BatchState) batchesState;
+
+    bytes constant ONE_ZERO = hex"00";
 
     constructor(
         IAVSDirectory __avsDirectory,
@@ -70,18 +73,13 @@ contract AlignedLayerServiceManager is ServiceManagerBase, BLSSignatureChecker {
         return aggregator == _aggregator;
     }
 
-    // NOTE(marian): Dummy function for testing contract integration
-    function getMeaning() external view returns (uint) {
-        return 42;
-    }
-
     function createNewTask(
         bytes32 batchMerkleRoot,
         string calldata dataStorePointer
     ) external payable {
         BatchState memory batchState;
 
-        batchState.taskCreatedBlock = block.number;
+        batchState.taskCreatedBlock = uint32(block.number);
         batchState.responded = false;
 
         batchesState[batchMerkleRoot] = batchState; 
@@ -102,40 +100,32 @@ contract AlignedLayerServiceManager is ServiceManagerBase, BLSSignatureChecker {
         // Check task hasn't been responsed yet
         // Note: This is a hacky solidity way to see that the element exists
         // Value 0 would mean that the task is in block 0 so this can't happen.
+
         require(
-            batchesState[taskIndex] == 0,
+            batchesState[batchMerkleRoot].taskCreatedBlock == 0,
             "Batch doesn't exists"
         );
-
-        // Validate the root in the index hint coincides with the signed information
-        require(
-            batchesState[taskIndex].batchMerkleRoot == batchMerkleRoot,
-            "Task in index doesn't match the provided root"
-        );
-
+        
         /* CHECKING SIGNATURES & WHETHER THRESHOLD IS MET OR NOT */
-
         // check that aggregated BLS signature is valid
         (
             QuorumStakeTotals memory quorumStakeTotals,
             bytes32 hashOfNonSigners
         ) = checkSignatures(
                 batchMerkleRoot,
-                QUORUM_NUMBERS,
-                taskCreatedBlock,
+                batchesState[batchMerkleRoot].taskCreatedBlock,
                 nonSignerStakesAndSignature
             );
 
+            
         // check that signatories own at least a threshold percentage of each quourm
-        for (uint i = 0; i < quorumNumbers.length; i++) {
-            require(
-                quorumStakeTotals.signedStakeForQuorum[i] *
-                    _THRESHOLD_DENOMINATOR >=
-                    quorumStakeTotals.totalStakeForQuorum[i] *
-                        QUORUM_THRESHOLD_PERCENTAGE,
-                "Signatories do not own at least threshold percentage of a quorum"
-            );
-        }
+        require(
+            quorumStakeTotals.signedStakeForQuorum[0] *
+                _THRESHOLD_DENOMINATOR >=
+                quorumStakeTotals.totalStakeForQuorum[0] *
+                    QUORUM_THRESHOLD_PERCENTAGE,
+            "Signatories do not own at least threshold percentage of a quorum"
+        );
 
         emit BatchVerified(
             batchMerkleRoot
