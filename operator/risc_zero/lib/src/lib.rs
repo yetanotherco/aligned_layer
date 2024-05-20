b0784c3b8cf7e64e94b5b1d78b5ca348c296788f
jotabulacios <jbulacios@fi.uba.ar>
2024-05-20 10:58:23 -0300

    Generate and ignore fibonacci_id.txt for Image ID in tests

--- operator/risc_zero/lib/src/lib.rs
+++ operator/risc_zero/lib/src/lib.rs
@@ -1,4 +1,6 @@
 use risc0_zkvm::Receipt;
+use std::fs::File;
+use std::io::{BufReader, Read};
 
 pub const MAX_RECEIPT_SIZE: usize = 215523;
 pub const MAX_IMAGE_ID_SIZE: usize = 8;
@@ -8,7 +10,7 @@ pub extern "C" fn verify_risc_zero_receipt_ffi(
     receipt_bytes: &[u8; MAX_RECEIPT_SIZE],
     receipt_len: usize,
     image_id: &[u32; MAX_IMAGE_ID_SIZE],
-    ) -> bool {
+) -> bool {
     if let Ok(receipt) = bincode::deserialize::<Receipt>(&receipt_bytes[..receipt_len]) {
         return receipt.verify(*image_id).is_ok();
     }
@@ -18,20 +20,43 @@ pub extern "C" fn verify_risc_zero_receipt_ffi(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::File;
+    use std::io::{BufReader, Read};
 
-    const RECEIPT: &[u8] =
-        include_bytes!("../../../../task_sender/test_examples/risc_zero/fibonacci_proof_generator/risc_zero_fibonacci.proof");
-    const IMAGE_ID: &[u32; 8] = &[316158856, 2947247145, 681508048, 729072451, 1635382859, 3265258586, 1254443731, 1018622456];
+    const RECEIPT: &[u8] = include_bytes!("../../../../task_sender/test_examples/risc_zero/fibonacci_proof_generator/risc_zero_fibonacci.proof");
+
+    fn read_image_id_from_file(file_name: &str) -> Result<[u32; 8], Box<dyn std::error::Error>> {
+        let file = File::open(file_name)?;
+        let mut buf_reader = BufReader::new(file);
+        let mut contents = String::new();
+        buf_reader.read_to_string(&mut contents)?;
+
+        // Remover los corchetes y los espacios en blanco adicionales
+        let contents = contents.trim_matches(|c: char| c == '[' || c == ']' || c.is_whitespace());
+        let parts: Vec<&str> = contents.split(',').collect();
+        if parts.len() != 8 {
+            return Err("El archivo no contiene exactamente 8 números".into());
+        }
+        let mut image_id = [0u32; 8];
+
+        for (i, part) in parts.iter().enumerate() {
+            image_id[i] = part.trim().parse().map_err(|e| format!("Error al parsear el número en la posición {}: {}", i, e))?;
+        }
+
+        Ok(image_id)
+    }
 
     #[test]
     fn verify_risc_zero_receipt_with_image_id_works() {
-       const RECEIPT_SIZE: usize = RECEIPT.len();
-       let mut receipt_buffer = [0u8; super::MAX_RECEIPT_SIZE];
-       receipt_buffer[..RECEIPT_SIZE].clone_from_slice(RECEIPT);
+        const RECEIPT_SIZE: usize = RECEIPT.len();
+        let mut receipt_buffer = [0u8; super::MAX_RECEIPT_SIZE];
+        receipt_buffer[..RECEIPT_SIZE].clone_from_slice(RECEIPT);
 
+        // Leer el IMAGE_ID desde el archivo
+        let image_id = read_image_id_from_file("../../../task_sender/test_examples/risc_zero/fibonacci_proof_generator/fibonacci_id.txt").expect("Failed to read image ID from file");
 
-       let result = verify_risc_zero_receipt_ffi(&receipt_buffer, RECEIPT_SIZE, IMAGE_ID);
-       assert!(result)
+        let result = verify_risc_zero_receipt_ffi(&receipt_buffer, RECEIPT_SIZE, &image_id);
+        assert!(result)
     }
 
     #[test]
@@ -40,7 +65,10 @@ mod tests {
         let mut receipt_buffer = [42u8; super::MAX_RECEIPT_SIZE];
         receipt_buffer[..RECEIPT_SIZE].clone_from_slice(RECEIPT);
 
-        let result = verify_risc_zero_receipt_ffi(&receipt_buffer, RECEIPT_SIZE - 1, IMAGE_ID);
+        // Leer el IMAGE_ID desde el archivo
+        let image_id = read_image_id_from_file("../../../task_sender/test_examples/risc_zero/fibonacci_proof_generator/fibonacci_id.txt").expect("Failed to read image ID from file");
+
+        let result = verify_risc_zero_receipt_ffi(&receipt_buffer, RECEIPT_SIZE - 1, &image_id);
         assert!(!result)
     }
 }
--- task_sender/test_examples/risc_zero/fibonacci_proof_generator/host/src/main.rs
+++ task_sender/test_examples/risc_zero/fibonacci_proof_generator/host/src/main.rs
@@ -2,16 +2,18 @@
 // The ELF is used for proving and the ID is used for verification.
 use methods::{FIBONACCI_ELF, FIBONACCI_ID};
 use risc0_zkvm::{default_prover, ExecutorEnv};
+use std::io::{BufWriter, Write}; // Añadido Write para usar writeln!
+use std::fs::File;
 
 const PROOF_FILE_PATH: &str = "risc_zero_fibonacci.proof";
 
-fn main() {
+fn main() -> std::io::Result<()> {
     // Initialize tracing. In order to view logs, run `RUST_LOG=info cargo run`
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
         .init();
 
-   println!("Image ID to be copied to operator/risc_zero/lib/src/lib.rs and operator/risc_zero/risc_zero_test.go: {:?}", FIBONACCI_ID);
+    println!("Image ID to be copied to operator/risc_zero/lib/src/lib.rs and operator/risc_zero/risc_zero_test.go: {:?}", FIBONACCI_ID);
 
     // An executor environment describes the configurations for the zkVM
     // including program inputs.
@@ -19,7 +21,7 @@ fn main() {
     // `let env = ExecutorEnv::builder().build().unwrap();`
     // However, this `env` does not have any inputs.
     //
-    // To add add guest input to the executor environment, use
+    // To add guest input to the executor environment, use
     // ExecutorEnvBuilder::write().
     // To access this method, you'll need to use ExecutorEnv::builder(), which
     // creates an ExecutorEnvBuilder. When you're done adding input, call
@@ -46,6 +48,7 @@ fn main() {
 
     println!("a: {}", a);
     println!("b: {}", b);
+    println!("ID: {:?}", FIBONACCI_ID);
 
     let verification_result = receipt.verify(FIBONACCI_ID).is_ok();
 
@@ -53,5 +56,15 @@ fn main() {
 
     let serialized = bincode::serialize(&receipt).unwrap();
 
-    std::fs::write(PROOF_FILE_PATH, serialized).unwrap();
+    std::fs::write(PROOF_FILE_PATH, serialized)?;
+
+    // Abrir archivo para escribir el ID de Fibonacci
+    let file_name = "fibonacci_id.txt";
+    let file = File::create(file_name)?;
+    let mut writer = BufWriter::new(file);
+
+    // Escribir la matriz en el archivo
+    writeln!(writer, "{:?}", FIBONACCI_ID)?;
+
+    Ok(())
 }
