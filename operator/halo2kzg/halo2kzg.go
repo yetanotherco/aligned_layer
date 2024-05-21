ecf0ff165c9b8e6ed0f4039c402efc509561a5e2
PatStiles <pdstiles78@gmail.com>
2024-05-21 15:23:43 -0300

    halo2kzg test works with weird ffi error fix.

--- operator/halo2kzg/halo2kzg.go
+++ operator/halo2kzg/halo2kzg.go
@@ -7,10 +7,17 @@ package halo2kzg
 #include "lib/halo2kzg.h"
 */
 import "C"
+import "fmt"
 import "unsafe"
 
 // MaxProofSize 2MB
 const MaxProofSize = 2048;
+
+// MaxVerificationKeySize 4 MB
+const MaxParamsSize = 4 * 1024;
+
+// MaxConstraintSystemSize 1 KB
+const MaxConstraintSystemSize = 791;
 
 // MaxVerificationKeySize 1 KB
 const MaxVerifierKeySize = 518;
@@ -18,23 +25,36 @@ const MaxVerifierKeySize = 518;
 // MaxVerificationKeySize 4 MB
 const MaxKzgParamsSize = 2308;
 
-// MaxVerificationKeySize 4 MB
-const MaxPublicInputSize = 4 * 1024 * 1024;
+// MaxPublicInputSize 4 MB
+const MaxPublicInputSize = 1024 * 1024;
 
+//Merge all pointers into one array and send across interface
 func VerifyHalo2KzgProof(
 	proofBuffer [MaxProofSize]byte, proofLen uint, 
+	csBuffer [MaxConstraintSystemSize]byte, csLen uint, 
 	vkBuffer [MaxVerifierKeySize]byte, vkLen uint, 
 	kzgParamBuffer [MaxKzgParamsSize]byte, kzgParamLen uint, 
 	publicInputBuffer [MaxPublicInputSize]byte, publicInputLen uint,
 ) bool {
+	fmt.Printf("Public Input Len: %d\n", publicInputLen)
+	(C.print_pub_len((C.uint)(publicInputLen)))
 	proofPtr := (*C.uchar)(unsafe.Pointer(&proofBuffer[0]))
+	csPtr := (*C.uchar)(unsafe.Pointer(&csBuffer[0]))
 	vkPtr := (*C.uchar)(unsafe.Pointer(&vkBuffer[0]))
 	kzgParamPtr := (*C.uchar)(unsafe.Pointer(&kzgParamBuffer[0]))
 	publicInputPtr := (*C.uchar)(unsafe.Pointer(&publicInputBuffer[0]))
+
+	/*
+	publicInputPtr = proofPtr;	
+	publicInputLenForReal := uint(1)
+	*/
+
 	return (bool)(C.verify_halo2_kzg_proof_ffi(
 		proofPtr, (C.uint)(proofLen), 
+		csPtr, (C.uint)(csLen),
 		vkPtr, (C.uint)(vkLen),
 		kzgParamPtr, (C.uint)(kzgParamLen),
+		// 4 or 8 bytes
 		publicInputPtr, (C.uint)(publicInputLen)),
 	)
 }
--- operator/halo2kzg/lib/halo2kzg.h
+++ operator/halo2kzg/lib/halo2kzg.h
@@ -1,6 +1,9 @@
 #include <stdbool.h>
 
+void print_pub_len(unsigned int public_input_len);
+
 bool verify_halo2_kzg_proof_ffi(unsigned char *proof_bytes, unsigned int proof_len,
+                                unsigned char *constraint_system_bytes, unsigned int cs_len,
                                 unsigned char *verifier_params_bytes, unsigned int vk_len,
                                 unsigned char *kzg_params_bytes, unsigned int kzg_params_len,
                                 unsigned char *public_input_bytes, unsigned int public_input_len);
