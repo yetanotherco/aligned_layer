185d1e1840900b62e164577760cb95416cf06792
Santos Rosati <rosatisantos@gmail.com>
2024-05-24 13:16:54 -0300

    feat: upgrade aligned contracts (WIP)

--- contracts/script/upgrade/upgrade.sh
+++ contracts/script/upgrade/upgrade.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-RESULT_LOG=$(forge script AlignedLayerUpgrader.s.sol --broadcast)
-echo "$RESULT_LOG"
+
+anvil --load-state scripts/anvil/state/alignedlayer-deployed-anvil-state.json --dump-state scripts/anvil/state/alignedlayer-upgraded-anvil-state.json &
+
+sleep 2
+
+forge script script/upgrade/AlignedLayerUpgrader.s.sol \
+    --rpc-url "http://localhost:8545" \
+    --private-key "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" \
+    --broadcast \
+
+pkill anvil
