aab891d1219c068a8bf86edb5ae24f4af1801d30
Urix <43704209+uri-99@users.noreply.github.com>
2024-05-24 19:29:31 -0300

    fix: make run_local

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -2,42 +2,63 @@
 
 echo "Starting local execution"
 
-echo "\nStarting anvil"
+echo "Starting anvil"
 make anvil_start > /dev/null 2>&1 & 
 export ANVIL_PID=$!
 
 sleep 1
 
-echo "\nStarting Aggregator"
+echo "Starting Aggregator"
 make aggregator_start > /dev/null 2>&1 & 
 
 export AGGREGATOR_PID=$!
 
 sleep 3
 
-echo "\nRegistering Operator"
+echo "Registering Operator"
 make operator_full_registration
 sleep 3
-echo "\nStarting Operator"
+echo "Starting Operator"
 make operator_start > /dev/null 2>&1 & 
 export OPERATOR_PID=$!
 
 sleep 2
 
-echo "\nSending 1 task"
-make send_plonk_bls12_381_proof > /dev/null 2>&1
+echo "Starting Batcher"
+make batcher_start > /dev/null 2>&1
+export BATCHER_PID=$!
+
+echo "Sending 15 tasks to Batcher"
+make batcher_send_sp1_task
+make batcher_send_sp1_task
+make batcher_send_sp1_task
+make batcher_send_sp1_task
+make batcher_send_sp1_task
+make batcher_send_groth16_task
+make batcher_send_groth16_task
+make batcher_send_groth16_task
+make batcher_send_groth16_task
+make batcher_send_groth16_task
+make batcher_send_sp1_task
+make batcher_send_groth16_task
+make batcher_send_sp1_task
+make batcher_send_groth16_task
+make batcher_send_sp1_task
 
 echo "Ready"
 read  -n 1 -p "Press anything to stop execution of Anvil & Aggregator & Operator" wait
 echo "Stopping execution"
 
-echo "\nStopping Operator"
+echo "Stopping Operator"
 kill ${OPERATOR_PID}
 
-echo "\nStopping Aggregator"
+echo "Stopping Aggregator"
 kill ${AGGREGATOR_PID}
 
-echo "\nStopping Anvil"
+echo "Stopping Anvil"
 kill ${ANVIL_PID}
+
+echo "Stopping Batcher"
+kill ${BATCHER_PID}
 
 exit 0
