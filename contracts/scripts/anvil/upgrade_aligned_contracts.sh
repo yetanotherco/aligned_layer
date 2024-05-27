51e51a3d2479f06c3e7338e95fb5c4208775ace7
Santos Rosati <rosatisantos@gmail.com>
2024-05-27 13:04:51 -0300

    fix: upgrade script delete block state at beggining instead of end

--- contracts/scripts/anvil/upgrade_aligned_contracts.sh
+++ contracts/scripts/anvil/upgrade_aligned_contracts.sh
@@ -7,6 +7,12 @@ cd "$parent_path"
 
 cd ../../
 
+jq 'del(.block)' scripts/anvil/state/alignedlayer-deployed-anvil-state.json > scripts/anvil/state/alignedlayer-deployed-anvil-state-tmp.json
+
+cp -f scripts/anvil/state/alignedlayer-deployed-anvil-state-tmp.json scripts/anvil/state/alignedlayer-deployed-anvil-state.json
+
+rm scripts/anvil/state/alignedlayer-deployed-anvil-state-tmp.json
+
 anvil --load-state scripts/anvil/state/alignedlayer-deployed-anvil-state.json --dump-state scripts/anvil/state/alignedlayer-deployed-anvil-state.json &
 
 sleep 2
@@ -36,9 +42,3 @@ mv "script/output/devnet/alignedlayer_deployment_output.temp.json" "script/outpu
 # Delete the temporary file
 rm -f "script/output/devnet/alignedlayer_deployment_output.temp.json"
 
-# Anvil adds a block state, making the code to fail.
-jq 'del(.block)' scripts/anvil/state/alignedlayer-deployed-anvil-state.json > scripts/anvil/state/alignedlayer-deployed-anvil-state-tmp.json
-
-cp -f scripts/anvil/state/alignedlayer-deployed-anvil-state-tmp.json scripts/anvil/state/alignedlayer-deployed-anvil-state.json
-
-rm scripts/anvil/state/alignedlayer-deployed-anvil-state-tmp.json
