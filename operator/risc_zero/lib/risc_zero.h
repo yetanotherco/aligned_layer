71c805fbeb115c2cb8108c28cdad53faffacb72b
PatStiles <pdstiles78@gmail.com>
2024-05-29 00:13:27 -0700

    change risc_zero ffi to use uint32_t

--- operator/risc_zero/lib/risc_zero.h
+++ operator/risc_zero/lib/risc_zero.h
@@ -1,3 +1,4 @@
 #include <stdbool.h>
+#include <stdint.h>
 
-bool verify_risc_zero_receipt_ffi(unsigned char *receipt_bytes, unsigned int receipt_len, unsigned int *image_id);
+bool verify_risc_zero_receipt_ffi(unsigned char *receipt_bytes, uint32_t receipt_len, uint32_t *image_id);
