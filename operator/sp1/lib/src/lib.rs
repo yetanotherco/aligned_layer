8a958863b1cd06c074fc324c796f616d1028af06
Santos Rosati <rosatisantos@gmail.com>
2024-05-30 14:30:17 -0300

    chore: remove debug statements

--- operator/sp1/lib/src/lib.rs
+++ operator/sp1/lib/src/lib.rs
@@ -13,32 +13,21 @@ pub extern "C" fn verify_sp1_proof_ffi(
     elf_bytes: *const u8,
     elf_len: u32,
 ) -> bool {
-    println!("Reading proof of length: {:?}", proof_len);
     let proof_bytes = unsafe {
         assert!(!proof_bytes.is_null());
-
         slice::from_raw_parts(proof_bytes, proof_len as usize)
     };
 
-    println!("Reading elf of length: {:?}", elf_len);
     let elf_bytes = unsafe {
         assert!(!elf_bytes.is_null());
-
         slice::from_raw_parts(elf_bytes, elf_len as usize)
     };
-
-    println!("Deserializing proof");
-
+    
     if let Ok(proof) = bincode::deserialize(proof_bytes) {
-        println!("Deserialized proof");
         let (_pk, vk) = PROVER_CLIENT.setup(elf_bytes);
-
-        println!("Verifying proof");
         return PROVER_CLIENT.verify_compressed(&proof, &vk).is_ok();
     }
-
-    println!("Failed to deserialize proof");
-
+    
     false
 }
 
@@ -54,9 +43,7 @@ mod tests {
     #[test]
     fn verify_sp1_proof_with_elf_works() {
         let proof_bytes = PROOF.as_ptr();
-        println!("actual proof len: {:?}", PROOF.len());
         let elf_bytes = ELF.as_ptr();
-        println!("actual elf len: {:?}", ELF.len());
 
         let result = verify_sp1_proof_ffi(proof_bytes, PROOF.len() as u32, elf_bytes, ELF.len() as u32);
         assert!(result)
@@ -64,14 +51,8 @@ mod tests {
 
     #[test]
     fn verify_sp1_aborts_with_bad_proof() {
-        let mut proof_buffer = [42u8; PROOF.len()];
-        proof_buffer.clone_from_slice(PROOF);
-
-        let mut elf_buffer = [42u8; ELF.len()];
-        elf_buffer.clone_from_slice(ELF);
-
-        let proof_bytes = proof_buffer.as_ptr();
-        let elf_bytes = elf_buffer.as_ptr();
+        let proof_bytes = PROOF.as_ptr();
+        let elf_bytes = ELF.as_ptr();
 
         let result = verify_sp1_proof_ffi(proof_bytes, (PROOF.len() - 1) as u32, elf_bytes, ELF.len() as u32);
         assert!(!result)
--- operator/sp1/sp1.go
+++ operator/sp1/sp1.go
@@ -10,10 +10,8 @@ import "C"
 import "unsafe"
 
 func VerifySp1Proof(proofBuffer []byte, proofLen uint, elfBuffer []byte, elfLen uint) bool {
-	println("VerifySp1Proof")
 	proofPtr := (*C.uchar)(unsafe.Pointer(&proofBuffer[0]))
 	elfPtr := (*C.uchar)(unsafe.Pointer(&elfBuffer[0]))
 
-	println("Calling C.verify_sp1_proof_ffi")
 	return (bool)(C.verify_sp1_proof_ffi(proofPtr, (C.uint)(proofLen), elfPtr, (C.uint)(elfLen)))
 }
