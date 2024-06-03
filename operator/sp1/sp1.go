0fac425742fbeba038c76bcfcf7c667823225d37
Tatu <65305492+srosati@users.noreply.github.com>
2024-06-03 11:39:21 -0300

    chore: update sp1 version + use compressed sp1 proofs + docs to generate sp1 proofs and verify them on aligned (#240)
    Co-authored-by: Mauro Toscano <12560266+MauroToscano@users.noreply.github.com>
    Co-authored-by: jotabulacios <jbulacios@fi.uba.ar>

--- operator/sp1/sp1.go
+++ operator/sp1/sp1.go
@@ -1,7 +1,7 @@
 package sp1
 
 /*
-#cgo linux LDFLAGS: ${SRCDIR}/lib/libsp1_verifier.a -ldl -lrt -lm -lssl -lcrypto -Wl,--allow-multiple-definition
+#cgo linux LDFLAGS: ${SRCDIR}/lib/libsp1_verifier.so -ldl -lrt -lm -lssl -lcrypto -Wl,--allow-multiple-definition
 #cgo darwin LDFLAGS: -L./lib -lsp1_verifier
 
 #include "lib/sp1.h"
@@ -9,14 +9,9 @@ package sp1
 import "C"
 import "unsafe"
 
-// MaxProofSize 2 MB
-const MaxProofSize = 2 * 1024 * 1024
-
-// MaxElfBufferSize 1 MB
-const MaxElfBufferSize = 1024 * 1024
-
-func VerifySp1Proof(proofBuffer [MaxProofSize]byte, proofLen uint32, elfBuffer [MaxElfBufferSize]byte, elfLen uint32) bool {
+func VerifySp1Proof(proofBuffer []byte, proofLen uint32, elfBuffer []byte, elfLen uint32) bool {
 	proofPtr := (*C.uchar)(unsafe.Pointer(&proofBuffer[0]))
 	elfPtr := (*C.uchar)(unsafe.Pointer(&elfBuffer[0]))
+
 	return (bool)(C.verify_sp1_proof_ffi(proofPtr, (C.uint32_t)(proofLen), elfPtr, (C.uint32_t)(elfLen)))
 }
