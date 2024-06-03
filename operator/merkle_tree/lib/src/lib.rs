3919847bd58b80f53a452e0028393ee1a23b4fe9
jotabulacios <jbulacios@fi.uba.ar>
2024-06-03 14:06:07 -0300

    save work

--- operator/merkle_tree/lib/src/lib.rs
+++ operator/merkle_tree/lib/src/lib.rs
@@ -1,7 +1,6 @@
 use lambdaworks_crypto::merkle_tree::merkle::MerkleTree;
 use batcher::types::{VerificationCommitmentBatch, VerificationData};
 
-// TODO: Ten times the size of one proof, could be changed later
 const MAX_BATCH_SIZE: usize = 2 * 1024 * 1024 * 10;
 
 #[no_mangle]
@@ -10,7 +9,6 @@ pub extern "C" fn verify_merkle_tree_batch_ffi(
     batch_len: u32,
     merkle_root: &[u8; 32]
 ) -> bool {
-
     let batch: Vec<VerificationData> = serde_json::from_slice(&batch_bytes[..batch_len as usize]).unwrap();
     let batch_commitment = VerificationCommitmentBatch::from(&batch);
     let batch_merkle_tree: MerkleTree<VerificationCommitmentBatch> = MerkleTree::build(&batch_commitment.0);
@@ -21,12 +19,14 @@ pub extern "C" fn verify_merkle_tree_batch_ffi(
 
 #[cfg(test)]
 mod tests {
-    use std::fs::File;
-    use std::io::Read;
     use super::*;
+    use alloy_primitives::Address;
+    use std::fs::{File, OpenOptions};
+    use std::io::{Read, Write};
+    use std::str::FromStr;
 
     #[test]
-    fn test_verify_merkle_tree_batch_ffi() {
+    fn test_verify_merkle_tree_batch_ffi_works() {
         let path = "./test_files/7a3d9215cfac21a4b0e94382e53a9f26bc23ed990f9c850a31ccf3a65aec1466.json";
 
         let mut file = File::open(path).unwrap();
@@ -46,10 +46,9 @@ mod tests {
     }
 
     #[test]
-    fn test_verify_merkle_tree_batch_ffi() {
+    fn test_verify_merkle_tree_batch_ffi_bad_proof() {
         let addr_str = "0x66f9664f97F2b50F62D13eA064982f936dE76657";
-        let proof_generator_addr: Address = Address::parse_checksummed(addr_str, None).unwrap();
-        
+        let _proof_generator_addr: Address = Address::from_str(addr_str).unwrap();
 
         let path = "./test_files/copy.json";
 
@@ -58,21 +57,24 @@ mod tests {
         let mut bytes_vec = Vec::new();
         file.read_to_end(&mut bytes_vec).unwrap();
 
+        // Print the content of the file for debugging
+        println!("Content of copy.json: {:?}", String::from_utf8_lossy(&bytes_vec));
+
         let mut bytes = [0; MAX_BATCH_SIZE];
         bytes[..bytes_vec.len()].copy_from_slice(&bytes_vec);
+
+        // Define batch_bytes and batch_len
+        let batch_bytes = &bytes;
+        let batch_len = bytes_vec.len() as u32;
 
         let batch: Vec<VerificationData> = serde_json::from_slice(&batch_bytes[..batch_len as usize]).unwrap();
         let batch_commitment = VerificationCommitmentBatch::from(&batch);
         let batch_merkle_tree: MerkleTree<VerificationCommitmentBatch> = MerkleTree::build(&batch_commitment.0);
-        let batch_merkle_root = hex::encode(batch_merkle_tree.root);
+        let _batch_merkle_root = hex::encode(batch_merkle_tree.root);
 
-        let mut bytes = [0; MAX_BATCH_SIZE];
-        bytes[..bytes_vec.len()].copy_from_slice(&bytes_vec);
-
-        /*
         let mut merkle_root = [0; 32];
         merkle_root.copy_from_slice(&hex::decode("7a3d9215cfac21a4b0e94382e53a9f26bc23ed990f9c850a31ccf3a65aec1466").unwrap());
-        */
 
+        let _result = verify_merkle_tree_batch_ffi(&batch_bytes, batch_len, &merkle_root);
     }
 }
