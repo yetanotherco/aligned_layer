30043485b25cf9bb47b83dd41c5a6ea1d94152f7
jotabulacios <jbulacios@fi.uba.ar>
2024-06-07 15:25:56 -0300

    remove comments

--- batcher/client/send_burst_tasks.sh
+++ batcher/client/send_burst_tasks.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Valores por defecto
 counter=1
 burst=8
 
-# Procesar el argumento para el valor de burst
 if [ -z "$1" ]; then
     echo "Using default burst value: 10"
 elif ! [[ "$1" =~ ^[0-9]+$ ]]; then
@@ -15,7 +13,6 @@ else
     echo "Using burst value: $burst"
 fi
 
-# Procesar el argumento para el valor inicial del counter
 if [ -z "$2" ]; then
     echo "Using default counter start value: 1"
 elif ! [[ "$2" =~ ^[0-9]+$ ]]; then
@@ -26,10 +23,9 @@ else
     echo "Starting counter from: $counter"
 fi
 
-# Bucle para procesar exactamente el número de tareas definido por burst
 for ((i=0; i<burst; i++))
 do
-    # Ejecutar en segundo plano
+  # Run in backaground to be able to run onece per second, and not wait for the previous one to finish
     ./batcher/client/generate_proof_and_send.sh $counter $burst &
     sleep 1
     counter=$((counter + 1))
