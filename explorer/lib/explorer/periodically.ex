15aa70e8d7ba329b408488920327e17f9f4c74dd
Urix <43704209+uri-99@users.noreply.github.com>
2024-06-07 16:55:20 -0300

    refactor: change redundancy check in periodic query to rpc

--- explorer/lib/explorer/periodically.ex
+++ explorer/lib/explorer/periodically.ex
@@ -6,14 +6,14 @@ defmodule Explorer.Periodically do
   end
 
   def init(state) do
-    schedule_work(0, 0)
+    schedule_work()
     {:ok, state}
   end
 
-  def handle_info({:work, previous_last_read_block, last_read_block}, state) do
+  def handle_info({:work}, state) do
     latest_block_number = AlignedLayerServiceManager.get_latest_block_number()
     try do
-      read_from_block = previous_last_read_block #for redundancy
+      read_from_block = max(0, latest_block_number - 3600) # read last 3600 blocks, for redundancy
       AlignedLayerServiceManager.get_new_batch_events(%{fromBlock: read_from_block, toBlock: latest_block_number})
       |> Enum.map(&AlignedLayerServiceManager.find_if_batch_was_responded/1)
       |> Enum.map(&Utils.extract_batch_data_pointer_info/1)
@@ -24,7 +24,7 @@ defmodule Explorer.Periodically do
         case Explorer.Repo.get_by(Batches, merkle_root: changeset.changes.merkle_root) do
           nil -> Explorer.Repo.insert(changeset)
           existing_batch ->
-            if existing_batch.is_verified == false and changeset.changes.is_verified == true do
+            if existing_batch.is_verified != changeset.changes.is_verified do # catches changes of state
               updated_changeset = Ecto.Changeset.change(existing_batch, changeset.changes)
               Explorer.Repo.update(updated_changeset)
             end
@@ -34,12 +34,12 @@ defmodule Explorer.Periodically do
       error -> IO.puts("An error occurred during batch processing:\n#{inspect(error)}")
     end
 
-    schedule_work(last_read_block, latest_block_number) # Reschedule once more
+    schedule_work() # Reschedule once more
     {:noreply, state}
   end
 
-  defp schedule_work(previous_last_read_block, last_read_block) do
-    Process.send_after(self(), {:work, previous_last_read_block, last_read_block}, 5 * 1000) # 10 seconds
+  defp schedule_work() do
+    Process.send_after(self(), {:work}, 5 * 1000) # 10 seconds
   end
 
 end
