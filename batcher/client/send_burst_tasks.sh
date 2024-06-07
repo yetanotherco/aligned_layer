840b2232a9d34c51b428c0e6d08e73a21a02edbf
jotabulacios <jbulacios@fi.uba.ar>
2024-06-07 14:03:22 -0300

    burst starts from given number

--- batcher/client/send_burst_tasks.sh
+++ batcher/client/send_burst_tasks.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
+# Valores por defecto
 counter=1
 burst=8
 
+# Procesar el argumento para el valor de burst
 if [ -z "$1" ]; then
     echo "Using default burst value: 10"
 elif ! [[ "$1" =~ ^[0-9]+$ ]]; then
-    echo "Error: Argument must be a number."
+    echo "Error: First argument must be a number."
     exit 1
 else
     burst=$1
     echo "Using burst value: $burst"
 fi
 
-counter=1
-while true
-do
-  # Run in backaground to be able to run onece per second, and not wait for the previous one to finish
-  ./batcher/client/generate_proof_and_send.sh $counter $burst &
-  sleep 1
-  counter=$((counter + 1))
-done
+# Procesar el argumento para el valor inicial del counter
+if [ -z "$2" ]; then
+    echo "Using default counter start value: 1"
+elif ! [[ "$2" =~ ^[0-9]+$ ]]; then
+    echo "Error: Second argument must be a number."
+    exit 1
+else
+    counter=$2
+    echo "Starting counter from: $counter"
+fi
 
+# Bucle para procesar exactamente el número de tareas definido por burst
+for ((i=0; i<burst; i++))
+do
+    # Ejecutar en segundo plano
+    ./batcher/client/generate_proof_and_send.sh $counter $burst &
+    sleep 1
+    counter=$((counter + 1))
+done
