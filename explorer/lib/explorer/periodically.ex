5b5eedd3b5ff818562c8af1640a564586bb8e74a
Urix <43704209+uri-99@users.noreply.github.com>
2024-06-12 19:06:03 -0300

    refactor: periodically

--- explorer/lib/explorer/periodically.ex
+++ explorer/lib/explorer/periodically.ex
@@ -13,7 +13,10 @@ defmodule Explorer.Periodically do
   #wip new db version:
   def handle_info({:work}, state) do
     # This reads and process last n blocks for new batches or batch changes
-    process_last_n_blocks(12) # new batch every 4-5 blocks
+    read_block_qty = 8 # There is a new batch every 4-5 blocks
+    latest_block_number = AlignedLayerServiceManager.get_latest_block_number()
+    read_from_block = max(0, latest_block_number - read_block_qty)
+    process_from_to_blocks(read_from_block, latest_block_number)
 
     # It gets previous unverified batches and checks if they were verified
     # This is to avoid having the -3600 because it takes 3-4-5 seconds to fetch each batch from S3
@@ -25,16 +28,15 @@ defmodule Explorer.Periodically do
   end
 
   defp schedule_work() do
-    seconds = 5 # edit to modify process frequency
+    seconds = 1 # edit to modify process frequency
     Process.send_after(self(), {:work}, seconds * 1000)
   end
 
-  defp process_last_n_blocks(n) do
-    latest_block_number = AlignedLayerServiceManager.get_latest_block_number()
+  def process_from_to_blocks(fromBlock, toBlock) do
+    "Processing from block #{fromBlock} to block #{toBlock}..." |> IO.inspect()
+    # TODO transform to Stream
     try do
-      "inserting batches..." |> IO.inspect()
-      read_from_block = max(0, latest_block_number - n) # read last n (3600) blocks, for redundancy
-      AlignedLayerServiceManager.get_new_batch_events(%{fromBlock: read_from_block, toBlock: latest_block_number})
+      AlignedLayerServiceManager.get_new_batch_events(%{fromBlock: fromBlock, toBlock: toBlock})
       |> Enum.map(&AlignedLayerServiceManager.extract_batch_response/1)
       |> Enum.map(&Utils.extract_amount_of_proofs/1)
       |> Enum.map(&Batches.generate_changeset/1)
