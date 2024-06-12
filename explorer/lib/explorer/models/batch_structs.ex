5a3ddbfcaebfdf830bd9fa1abb49d7f397b4b4d7
Urix <43704209+uri-99@users.noreply.github.com>
2024-06-12 16:12:47 -0300

    refactor: batch structs

--- explorer/lib/explorer/models/batch_structs.ex
+++ explorer/lib/explorer/models/batch_structs.ex
@@ -1,17 +1,4 @@
-# struct BatchState {
-#   uint32 taskCreatedBlock;
-#   bool responded;
-# }
-defmodule BatchState do
-  @enforce_keys [:taskCreatedBlock, :responded]
-  defstruct [:taskCreatedBlock, :responded]
-end
-
-# event NewBatch(
-#   bytes32 batchMerkleRoot,
-#   uint32 taskCreatedBlock,
-#   string batchDataPointer
-# );
+# Raised event in batch creation
 defmodule NewBatchEvent do
   @enforce_keys [:batchMerkleRoot, :taskCreatedBlock, :batchDataPointer]
   defstruct [:batchMerkleRoot, :taskCreatedBlock, :batchDataPointer]
@@ -21,56 +8,19 @@ defmodule NewBatchEvent do
   end
 end
 
-# %Aligned.NewBatchInfo{
-#   address: event |> Map.get(:address),
-#   block_hash: event |> Map.get(:block_hash),
-#   block_number: event |> Map.get(:block_number),
-#   transaction_hash: event |> Map.get(:transaction_hash),
-#   new_batch: new_batch
-# }
+# Blockchain Information about the new batch event
 defmodule NewBatchInfo do
-  @enforce_keys [:address, :block_hash, :block_number, :transaction_hash, :new_batch]
-  defstruct [:address, :block_hash, :block_number, :transaction_hash, :new_batch]
+  @enforce_keys [:address, :block_number, :block_timestamp, :transaction_hash, :new_batch]
+  defstruct [:address, :block_number, :block_timestamp, :transaction_hash, :new_batch]
 end
 
-# event BatchVerified(
-#   bytes32 batchMerkleRoot
-# );
-defmodule BatchVerifiedEvent do
-  @enforce_keys [:batchMerkleRoot]
-  defstruct [:batchMerkleRoot]
-end
-
-# %Aligned.BatchVerifiedInfo{
-#   address: event |> Map.get(:address),
-#   block_hash: event |> Map.get(:block_hash),
-#   block_number: event |> Map.get(:block_number),
-#   transaction_hash: event |> Map.get(:transaction_hash),
-#   batch_verified: batch_verified
-# }
+# Blockchain Information about the batch response event
 defmodule BatchVerifiedInfo do
-  @enforce_keys [:address, :block_hash, :block_number, :transaction_hash, :batch_verified]
-  defstruct [:address, :block_hash, :block_number, :transaction_hash, :batch_verified]
+  @enforce_keys [:address, :block_number, :block_timestamp, :transaction_hash, :batch_verified]
+  defstruct [:address, :block_number, :block_timestamp, :transaction_hash, :batch_verified]
 end
 
-# following data structs will be replaced by BatchDB, wip
-# defmodule BatchPageItem do
-#   @enforce_keys [
-#     :batch_merkle_root,
-#     :task_created_block_number,
-#     :task_created_tx_hash,
-#     :task_responded_block_number,
-#     :task_responded_tx_hash,
-#     :batch_data_pointer,
-#     :responded
-#   ]
-#   defstruct [:batch_merkle_root, :task_created_block_number, :task_created_tx_hash, :task_responded_block_number, :task_responded_tx_hash, :batch_data_pointer, :responded]
-# end
-# defmodule Batch do
-#   @enforce_keys [:batch_merkle_root, :batch_data_pointer, :is_verified]
-#   defstruct [:batch_merkle_root, :batch_data_pointer, :is_verified]
-# end
-
+# Database model for batches
 defmodule BatchDB do
   @enforce_keys [
     :merkle_root,
