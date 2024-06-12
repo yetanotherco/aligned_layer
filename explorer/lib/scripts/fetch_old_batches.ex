322ace165ee26ba672e338a77ff25a9e4730ee6f
Urix <43704209+uri-99@users.noreply.github.com>
2024-06-12 19:05:34 -0300

    refactor: fetch_old_batches using new structures

--- explorer/lib/scripts/fetch_old_batches.ex
+++ explorer/lib/scripts/fetch_old_batches.ex
@@ -26,36 +26,71 @@ defmodule Scripts.FetchOldBatches do
   defp make_request({fromBlock, toBlock}) do
     "making request from #{fromBlock} to #{toBlock}" |> IO.inspect()
     try do
-      read_from_block = fromBlock
-      AlignedLayerServiceManager.get_new_batch_events(%{fromBlock: read_from_block, toBlock: toBlock})
-      |> Enum.map(&transform_to_batch/1)
-      |> Enum.map(&transfrom_to_batch_db/1)
-      |> Enum.map(&Batches.cast_to_batches/1)
-      |> Enum.map(&Map.from_struct/1)
-      |> Enum.map(fn batch -> Ecto.Changeset.cast(%Batches{}, batch, [:merkle_root, :amount_of_proofs, :is_verified]) end)
-      |> Enum.map(fn changeset ->
-        Explorer.Repo.insert_or_update(changeset)
-      end)
+      process_from_to_blocks(fromBlock, toBlock)
     rescue
-      error -> IO.puts("An error occurred during batch processing:\n#{inspect(error)}")
+      error -> IO.puts("An error occurred during batch processing*:\n#{inspect(error)}")
     end
   end
 
-  def transform_to_batch(%Ethers.Event{} = new_batch_event) do
-    new_batch = AlignedLayerServiceManager.parse_new_batch_event(new_batch_event)
-    %Batch{
-      batch_merkle_root: new_batch.batchMerkleRoot,
-      batch_data_pointer: new_batch.batchDataPointer,
-      is_verified: true
+  # This is a copy of the function in periodically.ex
+  # But it doesn't try to read from S3 and:
+    # inserts amount of proofs = average
+    # skips getting block timestamp
+  def process_from_to_blocks(fromBlock, toBlock) do
+    "Processing from block #{fromBlock} to block #{toBlock}..." |> IO.inspect()
+    # TODO transform to Stream
+    try do
+      get_new_batch_events(%{fromBlock: fromBlock, toBlock: toBlock})
+      |> Enum.map(&extract_batch_response/1)
+      |> Enum.map(&Batches.generate_changeset/1)
+      |> Enum.map(&Batches.insert_or_update/1)
+    rescue
+      error -> IO.puts("An error occurred during batch processing**:\n#{inspect(error)}")
+    end
+  end
+
+  def extract_batch_response({_status, %NewBatchInfo{} = batch_creation}) do
+    created_batch = batch_creation.new_batch
+    %BatchDB{
+      merkle_root: created_batch.batchMerkleRoot,
+      data_pointer: created_batch.batchDataPointer,
+      is_verified: true,
+      submition_block_number: batch_creation.block_number,
+      submition_transaction_hash: batch_creation.transaction_hash,
+      submition_timestamp: batch_creation.block_timestamp,
+      response_block_number: nil,
+      response_transaction_hash: nil,
+      response_timestamp: nil,
+      amount_of_proofs: 502,
     }
   end
 
-  def transfrom_to_batch_db(%Batch{} = batch) do
-    %BatchDB {
-      batch_merkle_root: batch.batch_merkle_root,
-      amount_of_proofs: 470,
-      is_verified: batch.is_verified
-    }
+  def get_new_batch_events(%{fromBlock: fromBlock, toBlock: toBlock}) do
+    "From block" |> IO.inspect()
+    fromBlock |> IO.inspect()
+    "To block" |> IO.inspect()
+    toBlock |> IO.inspect()
+
+    events =
+      AlignedLayerServiceManager.EventFilters.new_batch(nil)
+      |> Ethers.get_logs(fromBlock: fromBlock, toBlock: toBlock)
+
+    case events do
+      {:ok, []} -> []
+      {:ok, list} -> Enum.map(list, &extract_new_batch_event_info/1)
+      {:error, reason } -> raise("Error fetching events: #{Map.get(reason, "message")}")
+    end
+  end
+  def extract_new_batch_event_info(event) do
+    new_batch = AlignedLayerServiceManager.parse_new_batch_event(event)
+    {:ok,
+     %NewBatchInfo{
+       address: event |> Map.get(:address),
+       block_number: event |> Map.get(:block_number),
+       block_timestamp: nil,
+       transaction_hash: event |> Map.get(:transaction_hash),
+       new_batch: new_batch
+     }}
   end
 
 end
