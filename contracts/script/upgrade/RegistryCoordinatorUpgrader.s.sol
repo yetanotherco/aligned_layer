329602d54390f77c8dfb4d406dac338818f54db6
Nicolas Rampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-06-12 18:21:07 -0300

    feat: add upgrade script for contracts (#203)
    Co-authored-by: Santos Rosati <rosatisantos@gmail.com>
    Co-authored-by: JuArce <52429267+JuArce@users.noreply.github.com>
    Co-authored-by: Tatu <65305492+srosati@users.noreply.github.com>

---
+++ contracts/script/upgrade/RegistryCoordinatorUpgrader.s.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.12;
+
+import {Script} from "forge-std/Script.sol";
+import "eigenlayer-contracts/src/contracts/core/AVSDirectory.sol";
+import {RegistryCoordinator} from "eigenlayer-middleware/RegistryCoordinator.sol";
+import {StakeRegistry} from "eigenlayer-middleware/StakeRegistry.sol";
+import {BLSApkRegistry} from "eigenlayer-middleware/BLSApkRegistry.sol";
+import {IndexRegistry} from "eigenlayer-middleware/IndexRegistry.sol";
+import {AlignedLayerServiceManager} from "src/core/AlignedLayerServiceManager.sol";
+import {IServiceManager} from "eigenlayer-middleware/interfaces/IServiceManager.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "forge-std/StdJson.sol";
+
+contract RegistryCoordinatorUpgrader is Script {
+    function run(
+        string memory eigenLayerDeploymentFilePath,
+        string memory alignedLayerDeploymentFilePath
+    ) external returns (address, address) {
+        // Load files
+        string memory eigen_deployment_file = vm.readFile(
+            eigenLayerDeploymentFilePath
+        );
+        string memory aligned_deployment_file = vm.readFile(
+            alignedLayerDeploymentFilePath
+        );
+        
+        // Load proxy admin
+        ProxyAdmin alignedLayerProxyAdmin = ProxyAdmin(
+            stdJson.readAddress(
+                aligned_deployment_file,
+                ".addresses.alignedLayerProxyAdmin"
+            )
+        );
+
+        // Load RegistryCoordinator Proxy
+        TransparentUpgradeableProxy registryCoordinator = TransparentUpgradeableProxy(
+            payable(stdJson.readAddress(
+                aligned_deployment_file,
+                ".addresses.registryCoordinator"
+            ))
+        );
+
+        // Load RegistryCoordinator dependencies
+        AlignedLayerServiceManager alignedLayerServiceManager = AlignedLayerServiceManager(
+            stdJson.readAddress(
+                aligned_deployment_file,
+                ".addresses.alignedLayerServiceManager"
+            )
+        );
+        StakeRegistry stakeRegistry = StakeRegistry(
+            stdJson.readAddress(
+                aligned_deployment_file,
+                ".addresses.stakeRegistry"
+            )
+        );
+        BLSApkRegistry apkRegistry = BLSApkRegistry(
+            stdJson.readAddress(
+                aligned_deployment_file,
+                ".addresses.blsApkRegistry"
+            )
+        );
+        IndexRegistry indexRegistry = IndexRegistry(
+            stdJson.readAddress(
+                aligned_deployment_file,
+                ".addresses.indexRegistry"
+            )
+        );
+
+        // Create a new instance of the RegistryCoordinatorImplementation
+        vm.startBroadcast();
+        RegistryCoordinator registryCoordinatorImplementation = new RegistryCoordinator(
+            IServiceManager(address(alignedLayerServiceManager)),
+            stakeRegistry,
+            apkRegistry,
+            indexRegistry
+        );
+        vm.stopBroadcast();
+
+        vm.startBroadcast();
+        alignedLayerProxyAdmin.upgrade(
+            registryCoordinator,
+            address(registryCoordinatorImplementation)
+        ); 
+        vm.stopBroadcast();
+        
+        return (
+            address(registryCoordinator),
+            address(registryCoordinatorImplementation)
+        );
+    }
+}
