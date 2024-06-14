1a9ac34954d40ef54ddeec5c00e3258fe03fc38f
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-06-14 17:50:10 -0300

    Remove unused code

--- batcher/aligned/src/eth.rs
+++ batcher/aligned/src/eth.rs
@@ -19,13 +19,7 @@ pub async fn aligned_service_manager(
     contract_address: &str,
 ) -> Result<AlignedLayerServiceManager, BatcherClientError> {
     let chain_id = provider.get_chainid().await?;
-
-    // let wallet = Wallet::decrypt_keystore(private_key_store_path, private_key_store_password)?
-    //     .with_chain_id(chain_id.as_u64());
-
-    // let signer = Arc::new(SignerMiddleware::new(provider, wallet));
     let client = Arc::new(provider);
-
     let contract_addr = H160::from_str(contract_address)
         .map_err(|e| BatcherClientError::EthError(e.to_string()))?;
 
