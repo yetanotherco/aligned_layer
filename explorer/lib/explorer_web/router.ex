cd858eabde30c569f4d5e29e94c55f282435f548
Elias Rad <146735585+nnsW3@users.noreply.github.com>
2024-06-18 15:26:11 +0300

    fix typos router.ex

--- explorer/lib/explorer_web/router.ex
+++ explorer/lib/explorer_web/router.ex
@@ -25,7 +25,7 @@ defmodule ExplorerWeb.Router do
   # To Enable LiveDashboard: (only enable behind auth)
   # if Application.compile_env(:explorer, :dev_routes) do
   #   # If you want to use the LiveDashboard in production, you should put
-  #   # it behind authentication and allow only admins to access it.
+  #   # it behind authentication and allows only admins to access it.
   #   # If your application does not have an admins-only section yet,
   #   # you can use Plug.BasicAuth to set up some basic authentication
   #   # as long as you are also using SSL (which you should anyway).
