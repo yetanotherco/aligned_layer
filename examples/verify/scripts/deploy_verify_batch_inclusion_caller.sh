6da7aa5fd5279a4ba60f64632ce4451d93d084c1
Santos Rosati <rosatisantos@gmail.com>
2024-06-18 14:51:58 -0300

    chore: forge init

---
+++ examples/verify/scripts/deploy_verify_batch_inclusion_caller.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# cd to the directory of this script so that this can be run from anywhere
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1 ; pwd -P )
+# At this point we are in contracts/scripts/anvil
+cd "$parent_path" || exit 1
+
+# At this point we are in verify dir
+cd ../ || exit 1
+
+ALIGNED_LAYER_SERVICE_MANAGER_ADDRESS=$(jq -r '.addresses.alignedLayerServiceManager' "$ALIGNED_DEPLOYMENT_OUTPUT")
+
+forge script deploy/VerifyBatchInclusionCallerDeployer.s.sol \
+    "$ALIGNED_LAYER_SERVICE_MANAGER_ADDRESS" \
+    --rpc-url "$RPC_URL" \
+    --private-key "$PRIVATE_KEY" \
+    --broadcast \
+    --slow \
+    --legacy \
+    --sig "run(address _targetContract)"
