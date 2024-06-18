71fca691ec1066f5afabcbd37f38e0afde58e1a5
Urix <43704209+uri-99@users.noreply.github.com>
2024-06-18 15:57:39 -0300

    remove: dashboard for prod

--- explorer/lib/explorer_web/router.ex
+++ explorer/lib/explorer_web/router.ex
@@ -25,19 +25,19 @@ defmodule ExplorerWeb.Router do
     end
   end
 
-  # To Enable LiveDashboard: (only enable behind auth)
-  if Application.compile_env(:explorer, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
+  # # To Enable LiveDashboard: (only enable behind auth)
+  # if Application.compile_env(:explorer, :dev_routes) do
+  #   # If you want to use the LiveDashboard in production, you should put
+  #   # it behind authentication and allow only admins to access it.
+  #   # If your application does not have an admins-only section yet,
+  #   # you can use Plug.BasicAuth to set up some basic authentication
+  #   # as long as you are also using SSL (which you should anyway).
+  #   import Phoenix.LiveDashboard.Router
 
-    scope "/dev" do
-      pipe_through :browser
+  #   scope "/dev" do
+  #     pipe_through :browser
 
-      live_dashboard "/dashboard", metrics: ExplorerWeb.Telemetry
-    end
-  end
+  #     live_dashboard "/dashboard", metrics: ExplorerWeb.Telemetry
+  #   end
+  # end
 end
