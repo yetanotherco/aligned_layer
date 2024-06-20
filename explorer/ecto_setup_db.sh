14454a7b5b3e9663bce09c20d59e616ad40e052d
Uriel Mihura <43704209+uri-99@users.noreply.github.com>
2024-06-20 15:31:09 -0300

    feat(explorer): extend database to reduce amount of rpc calls (#382)
    Co-authored-by: Gian <58370608+glpecile@users.noreply.github.com>
    Co-authored-by: Mauro Toscano <12560266+MauroToscano@users.noreply.github.com>

--- explorer/ecto_setup_db.sh
+++ explorer/ecto_setup_db.sh
@@ -9,6 +9,7 @@ export DB_NAME=$DB_NAME
 export DB_USER=$DB_USER
 export DB_PASS=$DB_PASS
 export DB_HOST=$DB_HOST
+export ALIGNED_CONFIG_FILE=$ALIGNED_CONFIG_FILE
 
 mix compile --force #force recompile to get the latest .env values
 
