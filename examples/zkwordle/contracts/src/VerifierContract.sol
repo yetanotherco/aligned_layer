9fd2338421245c4b0e39684c59d214459d25d0fc
Santos Rosati <rosatisantos@gmail.com>
2024-06-24 16:21:00 -0300

    feat: mint nft

--- examples/zkwordle/contracts/src/VerifierContract.sol
+++ examples/zkwordle/contracts/src/VerifierContract.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.12;
+pragma solidity ^0.8.12;
 
-contract VerifierContract {
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract VerifierContract is ERC721URIStorage {
+    uint256 private _nextTokenId;
+
     address public alignedServiceManager;
 
     bytes32 public elfCommitment = 0x4b9d9da7c31481ab20cc689580306796871409002bb2c21b56cc4a56ca0cb01b;
 
-    constructor(address _alignedServiceManager) {
+    constructor(address _alignedServiceManager) ERC721("ZK Wordle Solved", "ZKW") {
         alignedServiceManager = _alignedServiceManager;
     }
 
@@ -18,7 +23,7 @@ contract VerifierContract {
         bytes32 batchMerkleRoot,
         bytes memory merkleProof,
         uint256 verificationDataBatchIndex
-    ) external view returns (bool) {
+    ) external returns (uint256) {
         require(elfCommitment == provingSystemAuxDataCommitment, "ELF does not match");
 
         (bool callWasSuccessfull, bytes memory proofIsIncluded) = alignedServiceManager.staticcall(
@@ -37,9 +42,12 @@ contract VerifierContract {
         require(callWasSuccessfull, "static_call failed");
 
         bool proofIsIncludedBool = abi.decode(proofIsIncluded, (bool));
+        require(proofIsIncludedBool, "proof not included in batch");
 
-        // TODO: mint NFT if proofIsIncludedBool is true
+        uint256 tokenId = _nextTokenId++;
+        _mint(address(proofGeneratorAddr), tokenId);
+        _setTokenURI(tokenId, "https://zkwordle.com/proofs/1");
 
-        return proofIsIncludedBool;
+        return tokenId;
     }
 }
