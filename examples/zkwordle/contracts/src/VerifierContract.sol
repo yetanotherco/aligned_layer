6f353330a7230b7cccef5e35fb8bcec6fa68d0a9
Santos Rosati <rosatisantos@gmail.com>
2024-06-24 15:40:17 -0300

    feat: initial version

--- examples/zkwordle/contracts/src/VerifierContract.sol
+++ examples/zkwordle/contracts/src/VerifierContract.sol
@@ -4,6 +4,8 @@ pragma solidity =0.8.12;
 contract VerifierContract {
     address public alignedServiceManager;
 
+    bytes32 public elfCommitment = 0x4b9d9da7c31481ab20cc689580306796871409002bb2c21b56cc4a56ca0cb01b;
+
     constructor(address _alignedServiceManager) {
         alignedServiceManager = _alignedServiceManager;
     }
@@ -17,6 +19,8 @@ contract VerifierContract {
         bytes memory merkleProof,
         uint256 verificationDataBatchIndex
     ) external view returns (bool) {
+        require(elfCommitment == provingSystemAuxDataCommitment, "ELF does not match");
+
         (bool callWasSuccessfull, bytes memory proofIsIncluded) = alignedServiceManager.staticcall(
             abi.encodeWithSignature(
                 "verifyBatchInclusion(bytes32,bytes32,bytes32,bytes20,bytes32,bytes,uint256)",
---
+++ examples/zkwordle/wordle/script/src/main.rs
@@ -0,0 +1,34 @@
+//! A simple script to generate and verify the proof of a given program.
+
+use sp1_sdk::{ProverClient, SP1Stdin};
+
+const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+
+fn main() {
+    // Generate proof.
+    let mut stdin = SP1Stdin::new();
+    let n = 10u32;
+    stdin.write(&n);
+    let client = ProverClient::new();
+    let (pk, vk) = client.setup(ELF);
+    let mut proof = client.prove_compressed(&pk, stdin)
+        .expect("proving failed");
+
+    // Read output.
+    let a = proof.public_values.read::<u128>();
+    let b = proof.public_values.read::<u128>();
+    println!("a: {}", a);
+    println!("b: {}", b);
+
+    // Verify proof.
+    client
+        .verify_compressed(&proof, &vk)
+        .expect("verification failed");
+
+    // Save proof.
+    proof
+        .save("proof-with-io.json")
+        .expect("saving proof failed");
+
+    println!("successfully generated and verified proof for the program!")
+}
