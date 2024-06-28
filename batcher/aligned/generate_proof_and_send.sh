9ff219c35b5ac88db660c83abe904135fa259c45
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-06-28 14:42:11 -0300

    Add keystore_path arg to send burst script

--- batcher/aligned/generate_proof_and_send.sh
+++ batcher/aligned/generate_proof_and_send.sh
@@ -16,10 +16,11 @@ fi
 echo "Generating proof $x != 0"
 go run task_sender/test_examples/gnark_groth16_bn254_infinite_script/cmd/main.go $x
 
-./batcher/target/release/aligned submit \
+echo "" | ./batcher/target/release/aligned submit \
   --proving_system Groth16Bn254 \
   --repetitions $repetitions \
   --proof task_sender/test_examples/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.proof \
   --public_input task_sender/test_examples/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.pub \
   --vk task_sender/test_examples/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.vk \
+  --keystore_path config-files/anvil.ecdsa.key.json \
   --proof_generator_addr 0x66f9664f97F2b50F62D13eA064982f936dE76657
