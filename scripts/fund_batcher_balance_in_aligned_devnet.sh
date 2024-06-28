2815f147815187ba1996a10cb7f79287bacd1caf
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-06-28 14:07:24 -0300

    Add script for for funding batcher

--- scripts/fund_batcher_balance_in_aligned_devnet.sh
+++ scripts/fund_batcher_balance_in_aligned_devnet.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+# Check that OPERATOR_ADDRESS is not empty
+if [[ "$USER_ADDRESS" -eq "" ]]; then
+  echo "USER_ADDRESS is empty, using default value 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  USER_ADDRESS=0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+  USER_PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+fi;
+
 cd contracts
 
-BATCHER_ADDRESS=$(jq -r '.address.batcherWallet' ./script/deploy/config/devnet/batcher-payments.devnet.config.json)
-BATCHER_PRIVATE_KEY=$(jq -r '.address.batcherPrivateKey' ./script/deploy/config/devnet/batcher-payments.devnet.config.json)
-alignedLayerServiceManager=$(jq -r '.addresses.alignedLayerServiceManager' ./script/output/devnet/alignedlayer_deployment_output.json)
+batcher_payment_service_address=$(jq -r '.addresses.batcherPaymentService' ./script/output/devnet/alignedlayer_deployment_output.json)
 
 # Amount of ETH to send (in Ether)
 amount_in_eth="100ether"
 
 # Send Ether transaction
-cast send --from $BATCHER_ADDRESS \
+cast send --from $USER_ADDRESS \
     --value $amount_in_eth \
-    --private-key $BATCHER_PRIVATE_KEY \
+    --private-key $USER_PRIVATE_KEY \
     --rpc-url "http://localhost:8545" \
-    "$alignedLayerServiceManager"
+    "$batcher_payment_service_address"
