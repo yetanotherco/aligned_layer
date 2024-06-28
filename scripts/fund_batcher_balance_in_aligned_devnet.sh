aef7f8a49de61a9d3e710224ed09cf82b302f211
Uriel Mihura <43704209+uri-99@users.noreply.github.com>
2024-06-28 11:55:27 -0300

    feat: implement user batcher payment system (#469)
    Co-authored-by: Mariano Nicolini <mariano.nicolini.91@gmail.com>
    Co-authored-by: Santos Rosati <rosatisantos@gmail.com>
    Co-authored-by: JuArce <52429267+JuArce@users.noreply.github.com>

--- scripts/fund_batcher_balance_in_aligned_devnet.sh
+++ scripts/fund_batcher_balance_in_aligned_devnet.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Check that BATCHER_ADDRESS is not empty
-if [[ "$BATCHER_ADDRESS" -eq "" ]]; then
-  echo "BATCHER_ADDRESS is empty, using default value 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-  BATCHER_ADDRESS=0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
-  BATCHER_PRIVATE_KEY=ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-fi;
+cd contracts
 
-aligned_service_manager_address=$(cat "contracts/script/output/devnet/alignedlayer_deployment_output.json" | jq -r '.addresses.alignedLayerServiceManager')
+BATCHER_ADDRESS=$(jq -r '.address.batcherWallet' ./script/deploy/config/devnet/batcher-payments.devnet.config.json)
+BATCHER_PRIVATE_KEY=$(jq -r '.address.batcherPrivateKey' ./script/deploy/config/devnet/batcher-payments.devnet.config.json)
+alignedLayerServiceManager=$(jq -r '.addresses.alignedLayerServiceManager' ./script/output/devnet/alignedlayer_deployment_output.json)
 
 # Amount of ETH to send (in Ether)
 amount_in_eth="100ether"
@@ -17,4 +14,4 @@ cast send --from $BATCHER_ADDRESS \
     --value $amount_in_eth \
     --private-key $BATCHER_PRIVATE_KEY \
     --rpc-url "http://localhost:8545" \
-    "$aligned_service_manager_address"
+    "$alignedLayerServiceManager"
