cea7b8eac636e1f60571893f18d824a3af412715
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-01 14:33:02 -0300

    Clean some calls

--- operator/mina/lib/src/pickles_preproc/preprocess.rs
+++ operator/mina/lib/src/pickles_preproc/preprocess.rs
@@ -1,6 +1,9 @@
 use kimchi::{circuits::wires::COLUMNS, mina_curves::pasta::Pallas, poly_commitment::PolyComm};
 
-use crate::pickles_preproc::type_aliases::{WrapECPoint, WrapProverCommitments};
+use crate::pickles_preproc::{
+    state_proof::Commitments,
+    type_aliases::{WrapECPoint, WrapProverCommitments},
+};
 
 use super::{
     state_proof::StateProof,
@@ -10,30 +13,27 @@ use super::{
 pub fn deserialize_state_proof(
     state_proof: StateProof,
 ) -> Result<(WrapVerifierIndex, WrapProverProof), String> {
+    let Commitments {
+        w_comm: hex_w_comm,
+        z_comm: hex_z_comm,
+        t_comm: hex_t_comm,
+    } = state_proof.proof.commitments;
+
     // w_comm are single-point commitments
     let mut w_comm: [PolyComm<Pallas>; COLUMNS] = std::array::from_fn(|_| PolyComm {
         elems: Vec::with_capacity(1),
     });
-    for (hex_comm, comm) in state_proof
-        .proof
-        .commitments
-        .w_comm
-        .into_iter()
-        .zip(w_comm.iter_mut())
-    {
-        comm.elems.push(WrapECPoint::try_from(hex_comm)?.0);
+    for (hex_point, comm) in hex_w_comm.into_iter().zip(w_comm.iter_mut()) {
+        comm.elems.push(WrapECPoint::try_from(hex_point)?.0);
     }
 
     // z_comm is a single-point commitment
     let z_comm = PolyComm {
-        elems: vec![WrapECPoint::try_from(state_proof.proof.commitments.z_comm)?.0],
+        elems: vec![WrapECPoint::try_from(hex_z_comm)?.0],
     };
     // t_comm is a multi-point commitment
     let t_comm = PolyComm {
-        elems: state_proof
-            .proof
-            .commitments
-            .t_comm
+        elems: hex_t_comm
             .into_iter()
             .map(|hex_point| WrapECPoint::try_from(hex_point).map(|point| point.0))
             .collect::<Result<_, _>>()?,
