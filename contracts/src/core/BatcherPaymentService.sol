c94b7eceebba498bb701df802437e7764ea48560
Urix <43704209+uri-99@users.noreply.github.com>
2024-07-01 16:00:15 -0300

    feat(wip): first version of new payment checks in .sol

--- contracts/src/core/BatcherPaymentService.sol
+++ contracts/src/core/BatcherPaymentService.sol
@@ -21,8 +21,19 @@ contract BatcherPaymentService is
 
     mapping(address => uint256) public UserBalances;
 
+    mapping(uint256 => bool) public BatchWasSubmitted;
+
     // storage gap for upgradeability
     uint256[25] private __GAP;
+
+    struct ProofSubmitterData {
+        //user signs batch_id + merkle_root + amount_of_proofs_in_batch
+        uint256 amount_of_proofs_in_batch;
+        //signature:
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+    }
 
     // CONSTRUCTOR & INITIALIZER
     constructor() {
@@ -50,9 +61,10 @@ contract BatcherPaymentService is
 
     // PUBLIC FUNCTIONS
     function createNewTask(
+        uint256 batchId,
         bytes32 batchMerkleRoot,
+        ProofSubmitterData[] calldata proofSubmitters, // one address for each payer proof, 1 user has 2 proofs? send twice that address
         string calldata batchDataPointer,
-        address[] calldata proofSubmitters, // one address for each payer proof, 1 user has 2 proofs? send twice that address
         uint256 gasForAggregator,
         uint256 gasPerProof
     ) external onlyBatcher whenNotPaused {
@@ -62,19 +74,34 @@ contract BatcherPaymentService is
         uint256 amountOfSubmitters = proofSubmitters.length;
 
         require(amountOfSubmitters > 0, "No proof submitters");
+        require(BatchWasSubmitted(batchId) == false, "Batch already submitted"); // stops exploit of batcher making a user sign many times the same batch. only one of those proofs can be submitted
 
-        require(feePerProof * amountOfSubmitters > feeForAggregator, "Not enough gas to pay the batcher");
+        // This check was moved to after the for loop below, since 
+        // can't be exactly the same because new struct enables 1 submitter to have >1 proofs.
+        // require(feePerProof * amountOfSubmitters > feeForAggregator, "Not enough gas to pay the batcher");
 
         // discount from each payer
         // will revert if one of them has insufficient balance
+        ProofSubmitterData payerData;
+        address signer;
+        bytes32 hash_of_message;
+        uint256 totalFee = 0;
         for (uint256 i = 0; i < amountOfSubmitters; i++) {
-            address payer = proofSubmitters[i];
+            payerData = proofSubmitters[i];
+            hash_of_message = keccak256(abi.encodePacked(batchId, batchMerkleRoot, payerData.amount_of_proofs_in_batch));
+            // If user signed for another batchId, or another batchMerkleRoot, or another amount_of_proofs_in_batch, it would have a different signer.
+            // If wrong data was proportioned, it would have a random signer, and it won't have balance, because you can't precompute to get a desired signer. % of getting a signer with funds is almost 0.
+            // Because of this, I don't think we need to compare with an "expected signer"
+            signer = ecrecover(hash_of_message, payerData.v, payerData.r, payerData.s);
             require(
-                UserBalances[payer] >= feePerProof,
+                UserBalances[signer] >= (feePerProof * payerData.amount_of_proofs_in_batch,
                 "Payer has insufficient balance"
             );
-            UserBalances[payer] -= feePerProof;
+            UserBalances[payer] -= feePerProof * payerData.amount_of_proofs_in_batch;
+            totalFee += feePerProof * payerData.amount_of_proofs_in_batch; // accum of total fee
         }
+
+        require(totalFee > feeForAggregator, "Not enough fee for aggregator and batcher");
 
         // call alignedLayerServiceManager
         // with value to fund the task's response
@@ -90,7 +117,7 @@ contract BatcherPaymentService is
 
         require(success, "createNewTask call failed");
 
-        uint256 feeForBatcher = (feePerProof * amountOfSubmitters) - feeForAggregator;
+        uint256 feeForBatcher = (totalFee) - feeForAggregator;
 
         payable(BatcherWallet).transfer(feeForBatcher);
     }
