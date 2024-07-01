80aa1483b53f336c9b7d5dc6e6386a169e2e04ce
Santos Rosati <rosatisantos@gmail.com>
2024-07-01 18:04:29 -0300

    feat: submit proof to aligned using sdk

--- examples/zkquiz/quiz/script/src/main.rs
+++ examples/zkquiz/quiz/script/src/main.rs
@@ -1,16 +1,57 @@
-//! A simple script to generate and verify the proof of a given program.
+#![feature(slice_flatten)]
 
 use std::io;
-use sp1_sdk::{ProverClient, SP1Stdin};
+use std::sync::Arc;
 
+use aligned_sdk::sdk::{submit, verify_proof_onchain};
+use aligned_sdk::types::{AlignedVerificationData, Chain, ProvingSystemId, VerificationData};
+use clap::Parser;
+use ethers::middleware::SignerMiddleware;
+use ethers::prelude::abigen;
+use ethers::signers::{LocalWallet, Signer};
+use ethers::types::{Bytes, H160, U256};
+use log::info;
+use sp1_sdk::{ProverClient, SP1Stdin};
+use tokio::io::AsyncWriteExt;
+
+abigen!(
+    VerifierContract,
+    "../../contracts/out/VerifierContract.sol/VerifierContract.json"
+);
+
+const BATCHER_URL: &str = "wss://stage.batcher.alignedlayer.com";
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
-fn main() {
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    keystore_path: String,
+    #[arg(short, long, default_value = "https://ethereum-holesky-rpc.publicnode.com")]
+    rpc_url: Option<String>,
+    #[arg(short, long)]
+    verifier_contract_address: H160,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    let keystore_password = rpassword::prompt_password("Enter keystore password: ")
+        .expect("Failed to read keystore password");
+
+    let wallet = LocalWallet::decrypt_keystore(args.keystore_path, &keystore_password)
+        .expect("Failed to decrypt keystore");
+
     // Generate proof.
     let mut stdin = SP1Stdin::new();
 
     println!("Welcome to the quiz! Please answer the following questions to generate a proof for the program.");
-    println!("You will be asked 3 questions. Please answer with the corresponding letter (a, b or c).");
+    println!(
+        "You will be asked 3 questions. Please answer with the corresponding letter (a, b or c)."
+    );
 
     let mut user_awnsers = "".to_string();
     let question1 = "Who invented bitcoin";
@@ -38,16 +79,75 @@ fn main() {
                 .verify_compressed(&proof, &vk)
                 .expect("verification failed");
 
-            // Save proof.
-            proof
-                .save("proof-with-io.json")
-                .expect("saving proof failed");
+            // Serialize proof into bincode (format used by sp1)
+            let proof = bincode::serialize(&proof).expect("Failed to serialize proof");
 
-            println!("Successfully generated and verified proof for the program!")
+            println!("Proof verified successfully. Submitting proof to the batcher...");
+
+            let verification_data = VerificationData {
+                proving_system: ProvingSystemId::SP1,
+                proof,
+                verification_key: Some(ELF.to_vec()),
+                proof_generator_addr: wallet.address(),
+                vm_program_code: None,
+                pub_input: None,
+            };
+
+            match submit(BATCHER_URL, &verification_data, wallet.clone()).await {
+                Ok(Some(aligned_verification_data)) => {
+                    println!("Proof submitted successfully");
+                    let rpc_url = args.rpc_url.unwrap_or_default();
+
+                    if let Err(e) =
+                        wait_for_proof_to_be_verified(aligned_verification_data.clone(), rpc_url.clone())
+                            .await
+                    {
+                        println!("Proof verification failed: {:?}", e);
+                    }
+
+                    info!("Proof verified in Aligned, claiming prize...");
+                    let provider = ethers::providers::Provider::connect(rpc_url.as_str()).await
+                        .expect("Failed to connect to provider");
+
+                    let signer = Arc::new(SignerMiddleware::new(provider, wallet));
+
+                    let verifier_contract =
+                        VerifierContract::new(args.verifier_contract_address, signer.clone());
+
+                    let index_in_batch = U256::from(aligned_verification_data.index_in_batch);
+
+                    match verifier_contract.verify_batch_inclusion(
+                        aligned_verification_data.verification_data_commitment.proof_commitment,
+                        aligned_verification_data.verification_data_commitment.pub_input_commitment,
+                        aligned_verification_data.verification_data_commitment.proving_system_aux_data_commitment,
+                        aligned_verification_data.verification_data_commitment.proof_generator_addr,
+                        aligned_verification_data.batch_merkle_root,
+                        Bytes::from(aligned_verification_data.batch_inclusion_proof.merkle_path.as_slice().flatten().to_vec()),
+                        index_in_batch,
+                    ).await {
+                        Ok(tx) => {
+                            println!("Prize claimed successfully. Transaction hash: {:x}", tx);
+                        }
+                        Err(e) => {
+                            println!("Failed to claim prize: {:?}", e);
+                        }
+                    }
+                    // verifier_contract.verify_batch_inclusion(
+                    //
+                    //     aligned_verification_data.,
+                    //     aligned_verification_data.,
+                    // )
+                }
+                Ok(None) => {
+                    println!("Proof submission failed, no verification data");
+                }
+                Err(e) => {
+                    println!("Proof submission failed: {:?}", e);
+                }
+            }
         }
         Err(_) => {
             println!("Proof generation failed. Incorrect answer");
-            return;
         }
     }
 }
@@ -69,7 +169,9 @@ fn read_answer() -> char {
     loop {
         let mut answer = String::new();
 
-        io::stdin().read_line(&mut answer).expect("Failed to read from stdin");
+        io::stdin()
+            .read_line(&mut answer)
+            .expect("Failed to read from stdin");
         answer = answer.trim().to_string();
         if answer.len() != 1 {
             println!("Please enter a valid answer (a, b or c)");
@@ -86,3 +188,20 @@ fn read_answer() -> char {
     }
 }
 
+async fn wait_for_proof_to_be_verified(
+    verification_data: AlignedVerificationData,
+    rpc_url: String,
+) -> anyhow::Result<()> {
+    for _ in 0..10 {
+        if let Ok(_) =
+            verify_proof_onchain(verification_data.clone(), Chain::Holesky, rpc_url.as_str()).await
+        {
+            return Ok(());
+        }
+
+        info!("Proof not verified yet. Waiting 10 seconds before checking again...");
+        tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+    }
+
+    anyhow::bail!("Proof not verified after 10 attempts");
+}
