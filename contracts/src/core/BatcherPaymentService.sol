bc30c06a7ac8dda3ff9eaa283b3c308f0f72a9e5
Urix <43704209+uri-99@users.noreply.github.com>
2024-07-02 15:36:45 -0300

    feat: first working version of new payment system with signatures

--- contracts/src/core/BatcherPaymentService.sol
+++ contracts/src/core/BatcherPaymentService.sol
@@ -30,9 +30,9 @@ contract BatcherPaymentService is
         //user signs batch_id + merkle_root + amount_of_proofs_in_batch
         uint256 amount_of_proofs_in_batch;
         //signature:
-        uint8 v;
         bytes32 r;
         bytes32 s;
+        uint8 v;
     }
 
     // CONSTRUCTOR & INITIALIZER
@@ -59,6 +59,25 @@ contract BatcherPaymentService is
         emit PaymentReceived(msg.sender, msg.value);
     }
 
+// cast send 0x7969c5eD335650692Bc04293B07F5BF2e7A673C0 \
+// "createNewTask(uint256,bytes32,(uint256,uint8,bytes32,bytes32)[],string,uint256,uint256)" \
+// 1 \
+// 0xaeab82486c6c23487b4c475218db19e33e88bc21543ca2f625d185fddd3d26df \
+// "[(1,0x1c,0x3fbde0481c48a7d9408aab36a32666aa8572cd854fb88379da71b1dda95c9593,0xcf1b9774b6c47e0f0b98c99794636f1f790feb1a6e0073dea9b64a387f783a8e)]" \
+// "http://storage.alignedlayer.com/aeab82486c6c23487b4c475218db19e33e88bc21543ca2f625d185fddd3d26df.json" \
+// 1 10 \
+// --private-key 0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba
+
+// cast wallet sign \
+// "0000000000000000000000000000000000000000000000000000000000000001aeab82486c6c23487b4c475218db19e33e88bc21543ca2f625d185fddd3d26df0000000000000000000000000000000000000000000000000000000000000001" \
+// --private-key 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a
+
+// signature:
+// 0xcf1b9774b6c47e0f0b98c99794636f1f790feb1a6e0073dea9b64a387f783a8e3fbde0481c48a7d9408aab36a32666aa8572cd854fb88379da71b1dda95c95931c
+// :
+// cf1b9774b6c47e0f0b98c99794636f1f790feb1a6e0073dea9b64a387f783a8e
+// 3fbde0481c48a7d9408aab36a32666aa8572cd854fb88379da71b1dda95c9593
+// 1c
     // PUBLIC FUNCTIONS
     function createNewTask(
         uint256 batchId,
@@ -74,34 +93,34 @@ contract BatcherPaymentService is
         uint256 amountOfSubmitters = proofSubmitters.length;
 
         require(amountOfSubmitters > 0, "No proof submitters");
-        require(BatchWasSubmitted(batchId) == false, "Batch already submitted"); // stops exploit of batcher making a user sign many times the same batch. only one of those proofs can be submitted
-
-        // This check was moved to after the for loop below, since 
-        // can't be exactly the same because new struct enables 1 submitter to have >1 proofs.
-        // require(feePerProof * amountOfSubmitters > feeForAggregator, "Not enough gas to pay the batcher");
+        require(BatchWasSubmitted[batchId] == false, "Batch already submitted"); // stops exploit of batcher making a user sign many times the same batch. only one of those proofs can be submitted
 
         // discount from each payer
         // will revert if one of them has insufficient balance
-        ProofSubmitterData payerData;
-        address signer;
-        bytes32 hash_of_message;
-        uint256 totalFee = 0;
+        uint256 accumulatedFee = 0;
         for (uint256 i = 0; i < amountOfSubmitters; i++) {
-            payerData = proofSubmitters[i];
-            hash_of_message = keccak256(abi.encodePacked(batchId, batchMerkleRoot, payerData.amount_of_proofs_in_batch));
+            ProofSubmitterData memory user = proofSubmitters[i];
+
+            // TODO sign with --no-hash
+            bytes32 messageHash = keccak256(abi.encodePacked(batchId, batchMerkleRoot, user.amount_of_proofs_in_batch));
+            // todo sign with --no-header
+            bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+
             // If user signed for another batchId, or another batchMerkleRoot, or another amount_of_proofs_in_batch, it would have a different signer.
             // If wrong data was proportioned, it would have a random signer, and it won't have balance, because you can't precompute to get a desired signer. % of getting a signer with funds is almost 0.
             // Because of this, I don't think we need to compare with an "expected signer"
-            signer = ecrecover(hash_of_message, payerData.v, payerData.r, payerData.s);
+            address signer = ecrecover(ethSignedMessageHash, user.v, user.r, user.s);
             require(
-                UserBalances[signer] >= (feePerProof * payerData.amount_of_proofs_in_batch,
+                UserBalances[signer] >= (feePerProof * user.amount_of_proofs_in_batch),
                 "Payer has insufficient balance"
             );
-            UserBalances[payer] -= feePerProof * payerData.amount_of_proofs_in_batch;
-            totalFee += feePerProof * payerData.amount_of_proofs_in_batch; // accum of total fee
+            UserBalances[signer] -= feePerProof * user.amount_of_proofs_in_batch;
+            accumulatedFee += feePerProof * user.amount_of_proofs_in_batch; // accum of total fee
         }
 
-        require(totalFee > feeForAggregator, "Not enough fee for aggregator and batcher");
+        require(accumulatedFee > feeForAggregator, "Not enough fee for aggregator and batcher");
+        
+        BatchWasSubmitted[batchId] = true; // Before calling AlignedLayerServiceManager, to follow CEI, to prevent reentrancy
 
         // call alignedLayerServiceManager
         // with value to fund the task's response
@@ -117,9 +136,42 @@ contract BatcherPaymentService is
 
         require(success, "createNewTask call failed");
 
-        uint256 feeForBatcher = (totalFee) - feeForAggregator;
+        uint256 feeForBatcher = (accumulatedFee) - feeForAggregator;
 
         payable(BatcherWallet).transfer(feeForBatcher);
+    }
+
+
+
+// cast send 0x7969c5eD335650692Bc04293B07F5BF2e7A673C0 \
+// "createNewTask(uint256,bytes32,(uint256,uint8,bytes32,bytes32)[],string,uint256,uint256)" \
+// 1 \
+// 0xaeab82486c6c23487b4c475218db19e33e88bc21543ca2f625d185fddd3d26df \
+// "[(1,0x1c,0x3fbde0481c48a7d9408aab36a32666aa8572cd854fb88379da71b1dda95c9593,0xcf1b9774b6c47e0f0b98c99794636f1f790feb1a6e0073dea9b64a387f783a8e)]" \
+// "http://storage.alignedlayer.com/aeab82486c6c23487b4c475218db19e33e88bc21543ca2f625d185fddd3d26df.json" \
+// 1 10 \
+// --private-key 0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba
+
+// cast wallet sign \
+// "0000000000000000000000000000000000000000000000000000000000000001aeab82486c6c23487b4c475218db19e33e88bc21543ca2f625d185fddd3d26df0000000000000000000000000000000000000000000000000000000000000001" \
+// --private-key 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a
+
+// signature:
+// 0xa1ab85d36c4acca489cf89c512970147e3469dde7e7410bf94abf8c316f9e1b80e9c29e88dbab0d0021bd3caf03eac7ac4005f800087e60aa3975d929219b0fc1b
+// :
+// a1ab85d36c4acca489cf89c512970147e3469dde7e7410bf94abf8c316f9e1b8
+// 0e9c29e88dbab0d0021bd3caf03eac7ac4005f800087e60aa3975d929219b0fc
+// 1b
+
+// 0x9cbb5b624b022bcfb0523d10c229c3c9ced007ac9c3aa00c627760b41d2b03fa066dc100a8b79ac78819639fd8d994aa70e161e48fa13f005d32ec77651b7f9f1b
+// 9cbb5b624b022bcfb0523d10c229c3c9ced007ac9c3aa00c627760b41d2b03fa
+// 066dc100a8b79ac78819639fd8d994aa70e161e48fa13f005d32ec77651b7f9f
+// 1b
+    function recoverSigner(uint256 batchId, bytes32 batchMerkleRoot, uint256 amount_of_proofs_in_batch, bytes32 r, bytes32 s, uint8 v) public pure returns (address) {
+        bytes32 messageHash = keccak256(abi.encodePacked(batchId, batchMerkleRoot, amount_of_proofs_in_batch));
+        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        
+        return ecrecover(ethSignedMessageHash, v, r, s);
     }
 
     function withdraw(uint256 amount) external whenNotPaused {
@@ -153,3 +205,9 @@ contract BatcherPaymentService is
         _;
     }
 }
+
+
+// signature:
+// d5c1b37094ae4c9204617e47b6800afc7835773c635b463f354c7c683e1fcb06
+// 70b1c92f03665fcf20a94bee1e0456b475bc626ff6eec8c65695a26f4f37998b
+// 1b
