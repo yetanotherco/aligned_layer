b0b0abd2b0a1214f8b6e2f4bd9d2f31f6bee0cf6
JuArce <52429267+JuArce@users.noreply.github.com>
2024-07-03 18:08:48 -0300

    fix: drop last_uploaded_batch_block to avoid deadlocks

--- batcher/aligned-batcher/src/lib.rs
+++ batcher/aligned-batcher/src/lib.rs
@@ -384,10 +384,13 @@ impl Batcher {
 
         {
             let mut last_uploaded_batch_block = self.last_uploaded_batch_block.lock().await;
-            self.submit_batch(&batch_bytes, &batch_merkle_tree.root, submitter_addresses)
-                .await;
             // update last uploaded batch block
             *last_uploaded_batch_block = block_number;
+            // This is a hotfix until we merge https://github.com/yetanotherco/aligned_layer/pull/365
+            drop(last_uploaded_batch_block);
+            info!("Batch Finalizer: Last uploaded batch block updated to: {}. Lock unlocked", block_number);
+            self.submit_batch(&batch_bytes, &batch_merkle_tree.root, submitter_addresses)
+                .await;
         }
 
         if !wait_for_verification {
