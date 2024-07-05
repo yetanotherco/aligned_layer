6a167f5e2c9a363e7e1b10857eb765c586590dcf
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-05 17:10:25 -0300

    Verify with null app_state

--- operator/mina/lib/src/openmina_block_verifier/mod.rs
+++ operator/mina/lib/src/openmina_block_verifier/mod.rs
@@ -1,19 +1,39 @@
+use kimchi::mina_curves::pasta::Fp;
+use mina_tree::{
+    proofs::{
+        verification::verify_block,
+        verifier_index::{get_verifier_index, VerifierKind},
+    },
+    verifier::get_srs,
+};
+use protocol_state_proof::parse_base64;
+
 mod protocol_state_proof;
 
-pub fn parse_query_to_mina_block_header(mina_state_proof_vk_query: &str) {
-    todo!()
+pub fn verify_protocol_state_proof(
+    mina_protocol_state_proof_base64_query: &str,
+) -> Result<bool, String> {
+    let protocol_state_proof = parse_base64(mina_protocol_state_proof_base64_query)?;
+    let verifier_index = get_verifier_index(VerifierKind::Blockchain);
+    let srs = get_srs::<Fp>();
+    let srs = srs.lock().unwrap();
+
+    Ok(verify_block(&protocol_state_proof, &verifier_index, &srs))
 }
 
 #[cfg(test)]
 mod test {
-    use super::parse_query_to_mina_block_header;
+    use super::verify_protocol_state_proof;
 
-    const MINA_STATE_PROOF_VK_QUERY: &str = include_str!(
-        "../../../../../batcher/aligned/test_files/mina/mina_state_proof_vk_query.json"
+    const MINA_PROTOCOL_STATE_PROOF_BASE64_QUERY: &str = include_str!(
+        "../../../../../batcher/aligned/test_files/mina/mina_protocol_state_proof_base64.json"
     );
 
     #[test]
-    fn test_parse_query_to_mina_block_header() {
-        parse_query_to_mina_block_header(MINA_STATE_PROOF_VK_QUERY);
+    fn test_verify_protocol_state_proof() {
+        assert!(
+            verify_protocol_state_proof(MINA_PROTOCOL_STATE_PROOF_BASE64_QUERY).unwrap(),
+            "proof isn't valid!"
+        );
     }
 }
