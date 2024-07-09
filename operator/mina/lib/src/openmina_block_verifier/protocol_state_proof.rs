f5e66f7b411ee100e4f9631b9350796ddacf74d2
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-09 17:03:08 -0300

    Update proof data parser

--- operator/mina/lib/src/openmina_block_verifier/protocol_state_proof.rs
+++ operator/mina/lib/src/openmina_block_verifier/protocol_state_proof.rs
@@ -1,24 +1,7 @@
 use base64::prelude::*;
 use mina_p2p_messages::{binprot::BinProtRead, v2::MinaBaseProofStableV2};
 
-pub fn parse_base64(
-    mina_protocol_state_proof_base64_query: &str,
-) -> Result<MinaBaseProofStableV2, String> {
-    let mina_state_proof_vk_query: serde_json::Map<String, serde_json::Value> =
-        serde_json::from_str(mina_protocol_state_proof_base64_query)
-            .map_err(|err| format!("Could not parse mina state proof vk query: {err}"))?;
-
-    let protocol_state_proof_base64 = mina_state_proof_vk_query
-            .get("data")
-            .and_then(|d| d.get("block"))
-            .and_then(|d| d.get("protocolStateProof"))
-            .and_then(|d| d.get("base64"))
-            .ok_or("Could not parse protocol state proof: JSON structure upto protocolStateProof is unexpected")?.to_owned();
-
-    let protocol_state_proof_base64: String =
-        serde_json::from_value(protocol_state_proof_base64)
-            .map_err(|err| format!("Could not parse mina state proof: {err}"))?;
-
+pub fn parse_base64(protocol_state_proof_base64: &str) -> Result<MinaBaseProofStableV2, String> {
     let protocol_state_proof_binprot = BASE64_URL_SAFE
         .decode(protocol_state_proof_base64)
         .map_err(|err| err.to_string())?;
@@ -31,12 +14,11 @@ pub fn parse_base64(
 mod tests {
     use super::parse_base64;
 
-    const MINA_PROTOCOL_STATE_PROOF_BASE64_QUERY: &str = include_str!(
-        "../../../../../batcher/aligned/test_files/mina/mina_devnet_protocol_query.json"
-    );
+    const PROTOCOL_STATE_PROOF: &str =
+        include_str!("../../../../../batcher/aligned/test_files/mina/protocol_state_proof.proof");
 
     #[test]
-    fn parse_protocol_state_proof() {
-        parse_base64(MINA_PROTOCOL_STATE_PROOF_BASE64_QUERY).unwrap();
+    fn parse_base64_does_not_fail() {
+        parse_base64(PROTOCOL_STATE_PROOF).unwrap();
     }
 }
