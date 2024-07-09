17d4d00baaed52f31f7228318581eadec3a72b7e
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-09 17:06:44 -0300

    Update operator

--- operator/mina/lib/src/openmina_block_verifier/mod.rs
+++ operator/mina/lib/src/openmina_block_verifier/mod.rs
@@ -12,9 +12,13 @@ use protocol_state_proof::parse_base64;
 mod protocol_state;
 mod protocol_state_proof;
 
-pub fn verify_protocol_state_proof(mina_protocol_query: &str) -> Result<bool, String> {
-    let protocol_state_proof = parse_base64(mina_protocol_query)?;
-    let protocol_state_hash = parse_base58(mina_protocol_query)?;
+pub fn verify_protocol_state_proof(
+    protocol_state_proof_base64: &str,
+    protocol_state_hash_base58: &str,
+) -> Result<bool, String> {
+    let protocol_state_proof = parse_base64(protocol_state_proof_base64)?;
+    let protocol_state_hash = parse_base58(protocol_state_hash_base58)?;
+
     let verifier_index = get_verifier_index(VerifierKind::Blockchain);
     let srs = get_srs::<Fp>();
     let srs = srs.lock().unwrap();
@@ -31,14 +35,15 @@ pub fn verify_protocol_state_proof(mina_protocol_query: &str) -> Result<bool, St
 mod test {
     use super::verify_protocol_state_proof;
 
-    const MINA_PROTOCOL_STATE_PROOF_BASE64_QUERY: &str = include_str!(
-        "../../../../../batcher/aligned/test_files/mina/mina_devnet_protocol_query.json"
-    );
+    const PROTOCOL_STATE_PROOF: &str =
+        include_str!("../../../../../batcher/aligned/test_files/mina/protocol_state_proof.proof");
+    const PROTOCOL_STATE_HASH: &str =
+        include_str!("../../../../../batcher/aligned/test_files/mina/protocol_state_hash.pub");
 
     #[test]
     fn test_verify_protocol_state_proof() {
         assert!(
-            verify_protocol_state_proof(MINA_PROTOCOL_STATE_PROOF_BASE64_QUERY).unwrap(),
+            verify_protocol_state_proof(PROTOCOL_STATE_PROOF, PROTOCOL_STATE_HASH).unwrap(),
             "proof isn't valid!"
         );
     }
