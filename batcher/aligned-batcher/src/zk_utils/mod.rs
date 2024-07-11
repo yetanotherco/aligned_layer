43e81f89af312905ae9e91cbfd70ab269df34ec0
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-11 17:19:14 -0300

    Add pre verification steps

--- batcher/aligned-batcher/src/zk_utils/mod.rs
+++ batcher/aligned-batcher/src/zk_utils/mod.rs
@@ -1,8 +1,8 @@
-use crate::gnark::verify_gnark;
 use crate::halo2::ipa::verify_halo2_ipa;
 use crate::halo2::kzg::verify_halo2_kzg;
 use crate::risc_zero::verify_risc_zero_proof;
 use crate::sp1::verify_sp1_proof;
+use crate::{gnark::verify_gnark, mina::verify_protocol_state_proof_integrity};
 use aligned_batcher_lib::types::{ProvingSystemId, VerificationData};
 use log::{debug, warn};
 
@@ -78,7 +78,10 @@ pub(crate) fn verify(verification_data: &VerificationData) -> bool {
                 .pub_input
                 .as_ref()
                 .expect("Public input is required");
-            true // FIXME: pre-verify proof before submitting
+            verify_protocol_state_proof_integrity(&verification_data.proof, pub_input)
+            // TODO(xqft): add Pickles aggregator checks which are run alongside the Kimchi
+            // verifier. These checks are fast and if they aren't successful then the Pickles proof
+            // isn't valid.
         }
     }
 }
