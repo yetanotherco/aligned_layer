30ebda7d32a334800d5eab09265f84833380e1ab
Urix <43704209+uri-99@users.noreply.github.com>
2024-07-15 11:51:11 -0300

    chore: rename add column to add table in ecto migrate

--- explorer/priv/repo/migrations/20240708192338_add_proof_hashes_table.exs
+++ explorer/priv/repo/migrations/20240708192338_add_proof_hashes_table.exs
@@ -1,10 +1,7 @@
-defmodule Explorer.Repo.Migrations.AddProofHashesColumn do
+defmodule Explorer.Repo.Migrations.AddProofHashesTable do
   use Ecto.Migration
 
   def change do
-    # alter table("batches") do # add new columns
-    #   add :proof_hashes, {:array, :string}
-    # end
     create table("proofs", primary_key: false) do
       add :id, :bigserial, primary_key: true
       add :batch_merkle_root, references(:batches, column: :merkle_root, type: :string, size: 66), null: false
