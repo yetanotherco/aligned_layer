08b3216237a92dfcebbe7d74597cbd6f8c71f98a
NicolasRampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-07-15 18:31:52 -0300

    fix: remove changes

--- batcher/aligned/send_burst_tasks.sh
+++ batcher/aligned/send_burst_tasks.sh
@@ -4,7 +4,7 @@ counter=1
 burst=8
 
 if [ -z "$1" ]; then
-    echo "Using default burst value: $burst"
+    echo "Using default burst value: 10"
 elif ! [[ "$1" =~ ^[0-9]+$ ]]; then
     echo "Error: Argument must be a number."
     exit 1
@@ -26,7 +26,7 @@ fi
 
 while true
 do
-    # Run in backaground to be able to run once per second, and not wait for the previous one to finish
+    # Run in backaground to be able to run onece per second, and not wait for the previous one to finish
     . ./batcher/aligned/generate_proof_and_send.sh $counter $burst &
     sleep 1
     counter=$((counter + 1))
