bcd089f08ce6fb56e4b92a405e885a91dd18fa18
JuArce <52429267+JuArce@users.noreply.github.com>
2024-07-16 15:04:24 -0300

    fix: fetch from origin

--- scripts/fetch_latest_release.sh
+++ scripts/fetch_latest_release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # The script fetches the latest release tag from the repository and checks out that tag.
-latesttag=$(git describe --tags)
+git fetch --tags
+latesttag=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo checking out ${latesttag}
 git checkout ${latesttag}
