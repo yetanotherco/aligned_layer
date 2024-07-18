1ad2d37cbeaa2a35cb4b8905c97b1bcc52b1b11a
NicolasRampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-07-18 16:39:35 -0300

    fix: downgrade solidity version

--- examples/validating-public-input/contracts/src/FibonacciValidator.sol
+++ examples/validating-public-input/contracts/src/FibonacciValidator.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
 
 contract FibonacciValidator {
     address public alignedServiceManager;
