c2e451810677ba40e7f7150beaa9c6a18fe1c294
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-19 18:12:42 -0300

    Add new query and parse script

--- batcher/aligned/test_files/mina/parse_proof_query.sh
+++ batcher/aligned/test_files/mina/parse_proof_query.sh
@@ -1,2 +1,3 @@
-cat mina_mainnet_protocol_query.json | jq -r '.data.bestChain.[0].protocolState.previousStateHash' >protocol_state_hash.pub
-cat mina_mainnet_protocol_query.json | jq -r '.data.block.protocolStateProof.base64' >protocol_state_proof.proof
+cat mina_mainnet_protocol_query.json | jq -r '.data.bestChain.[0].stateHashField' >protocol_state_hash.pub
+cat mina_mainnet_protocol_query.json | jq -r '.data.protocolState' >protocol_state.pub
+cat mina_mainnet_protocol_query.json | jq -r '.data.bestChain.[0].protocolStateProof.base64' >protocol_state_proof.proof
