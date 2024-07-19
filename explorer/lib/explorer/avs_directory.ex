5d6eb5a01afb8a74a1caba2b0e349b8fb14c491d
Gian <58370608+glpecile@users.noreply.github.com>
2024-07-19 15:07:57 -0300

    refactor: move count operators to avs_directory instead of index

--- explorer/lib/explorer/avs_directory.ex
+++ explorer/lib/explorer/avs_directory.ex
@@ -9,25 +9,29 @@ defmodule AVSDirectory do
   {status, config_json_string} = File.read(file_path)
 
   case status do
-    :ok -> Logger.debug("Eigenlayer deployment file read successfully")
-    :error -> raise("Config file not read successfully, did you run make create-env? If you did,\n make sure Eigenlayer config file is correctly stored")
+    :ok ->
+      Logger.debug("Eigenlayer deployment file read successfully")
+
+    :error ->
+      raise(
+        "Config file not read successfully, did you run make create-env? If you did,\n make sure Eigenlayer config file is correctly stored"
+      )
   end
 
-  @avs_directory_address Jason.decode!(config_json_string) |> Map.get("addresses") |> Map.get("avsDirectory")
+  @avs_directory_address Jason.decode!(config_json_string)
+                         |> Map.get("addresses")
+                         |> Map.get("avsDirectory")
 
-  @first_block (
-    case @environment do
-      "devnet" -> 0
-      "holesky" -> 1600000
-      "mainnet" -> 20020000
-      _ -> raise("Invalid environment")
-    end
-  )
+  @first_block (case @environment do
+                  "devnet" -> 0
+                  "holesky" -> 1_600_000
+                  "mainnet" -> 20_020_000
+                  _ -> raise("Invalid environment")
+                end)
 
   use Ethers.Contract,
     abi_file: "lib/abi/AVSDirectory.json",
-    default_address:
-      @avs_directory_address
+    default_address: @avs_directory_address
 
   def get_avs_directory_address() do
     @avs_directory_address
@@ -39,5 +43,25 @@ defmodule AVSDirectory do
       AlignedLayerServiceManager.get_aligned_layer_service_manager_address()
     )
     |> Ethers.get_logs(fromBlock: @first_block)
+  end
+
+  # tail-call recursion
+  defp count_operators_registered(list), do: sum_operators_registered(list, 0)
+  defp sum_operators_registered([], val), do: val
+
+  defp sum_operators_registered([head | tail], val),
+    do: sum_operators_registered(tail, evaluate_operator(head, val))
+
+  defp evaluate_operator(event, val) do
+    # registered or unregistered
+    case event.data |> hd() == 1 do
+      true -> val + 1
+      false -> val - 1
+    end
+  end
+
+  def get_operators_registered() do
+    AVSDirectory.get_operator_status_updated_events()
+    |> (fn {status, data} when status == :ok -> count_operators_registered(data) end).()
   end
 end
--- explorer/lib/explorer_web/live/pages/home/index.ex
+++ explorer/lib/explorer_web/live/pages/home/index.ex
@@ -6,7 +6,7 @@ defmodule ExplorerWeb.Home.Index do
   def handle_info(_, socket) do
     verified_batches = Batches.get_amount_of_verified_batches()
 
-    operators_registered = get_operators_registered()
+    operators_registered = AVSDirectory.get_operators_registered()
 
     latest_batches =
       Batches.get_latest_batches(%{amount: 5})
@@ -29,7 +29,7 @@ defmodule ExplorerWeb.Home.Index do
   def mount(_, _, socket) do
     verified_batches = Batches.get_amount_of_verified_batches()
 
-    operators_registered = get_operators_registered()
+    operators_registered = AVSDirectory.get_operators_registered()
 
     latest_batches =
       Batches.get_latest_batches(%{amount: 5})
@@ -90,26 +90,6 @@ defmodule ExplorerWeb.Home.Index do
     e ->
       Logger.error("Other error: #{inspect(e)}")
       {:ok, socket |> put_flash(:error, "Something went wrong, please try again later.")}
-  end
-
-  # tail-call recursion
-  defp count_operators_registered(list), do: sum_operators_registered(list, 0)
-  defp sum_operators_registered([], val), do: val
-
-  defp sum_operators_registered([head | tail], val),
-    do: sum_operators_registered(tail, evaluate_operator(head, val))
-
-  defp evaluate_operator(event, val) do
-    # registered or unregistered
-    case event.data |> hd() == 1 do
-      true -> val + 1
-      false -> val - 1
-    end
-  end
-
-  defp get_operators_registered() do
-    AVSDirectory.get_operator_status_updated_events()
-    |> (fn {status, data} when status == :ok -> count_operators_registered(data) end).()
   end
 
   embed_templates "*"
