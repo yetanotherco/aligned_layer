a352af6a2ff28d31ae5de18c51a2e5582a543e41
Urix <43704209+uri-99@users.noreply.github.com>
2024-07-19 21:58:03 -0300

    fix: ecto migrate of create restakings table

--- explorer/priv/repo/migrations/20240719202825_add_restakings_table.exs
+++ explorer/priv/repo/migrations/20240719202825_add_restakings_table.exs
@@ -4,9 +4,8 @@ defmodule Explorer.Repo.Migrations.AddRestakingsTable do
   def change do
     create table("restakings", primary_key: false) do
       add :id, :bigserial, primary_key: true
-      add :staker_id, references(:stakers, column: :id, type: :bigint), null: false
+      add :operator_id, references(:operators, column: :id, type: :bigserial), null: false
       add :amount, :decimal, precision: 22, scale: 0, null: false
-      add :tx_id, references(:transactions, column: :id, type: :bigint), null: false
 
       timestamps()
     end
