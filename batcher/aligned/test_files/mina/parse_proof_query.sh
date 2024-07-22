d11ed56a203b9369df620cf4421224d1cdb708d7
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-07-22 17:14:57 -0300

    Query from new Devnet node

--- batcher/aligned/test_files/mina/parse_proof_query.sh
+++ batcher/aligned/test_files/mina/parse_proof_query.sh
@@ -1,3 +1,4 @@
-cat mina_mainnet_protocol_query.json | jq -r '.data.bestChain.[0].stateHashField' >protocol_state_hash.pub
-cat mina_mainnet_protocol_query.json | jq -r '.data.protocolState' >protocol_state.pub
-cat mina_mainnet_protocol_query.json | jq -r '.data.bestChain.[0].protocolStateProof.base64' >protocol_state_proof.proof
+cat $1 | jq --raw-output0 '.data.bestChain.[0].stateHashField' >protocol_state_hash.pub
+cat $1 | jq --raw-output0 '.data.protocolState' >protocol_state.pub
+cat $1 | jq --raw-output0 '.data.bestChain.[0].protocolStateProof.base64' >protocol_state_proof.proof
+cat $1 | jq '.data.blockchainVerificationKey' >devnet_vk.json
