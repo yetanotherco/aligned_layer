804fe246980e856540a469ce0fa64a83019de23f
Uriel Mihura <43704209+uri-99@users.noreply.github.com>
2024-07-23 11:29:13 -0300

    feat: Require signatures for batcher to charge the users (#632)
    Co-authored-by: Santos Rosati <rosatisantos@gmail.com>
    Co-authored-by: Tatu <65305492+srosati@users.noreply.github.com>
    Co-authored-by: Mariano Nicolini <mariano.nicolini.91@gmail.com>

--- operator/pkg/verification_data.go
+++ operator/pkg/verification_data.go
@@ -11,3 +11,8 @@ type VerificationData struct {
 	VerificationKey []byte                 `json:"verification_key"`
 	VmProgramCode   []byte                 `json:"vm_program_code"`
 }
+
+type NoncedVerificationData struct {
+	VerificationData VerificationData `json:"verification_data"`
+	Nonce            []byte           `json:"nonce"`
+}
