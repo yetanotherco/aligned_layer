105c09cfb4e05e471e2f4363bcaa72f42732b796
Gian <58370608+glpecile@users.noreply.github.com>
2024-07-25 18:32:01 -0300

    style(batches): add out animation on page change

--- explorer/lib/explorer_web/components/core_components.ex
+++ explorer/lib/explorer_web/components/core_components.ex
@@ -655,7 +655,7 @@ defmodule ExplorerWeb.CoreComponents do
           </tr>
         </thead>
         <tbody id={@id} phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}>
-          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="gap-y-2 [&>td]:pt-3">
+          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="gap-y-2 [&>td]:pt-3 animate-out fade-out-0 duration-200">
             <td
               :for={{col, i} <- Enum.with_index(@col)}
               phx-click={@row_click && @row_click.(row)}
