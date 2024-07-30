557076bb4b42b505f6eed4f4b8590a8c48be9df9
Urix <43704209+uri-99@users.noreply.github.com>
2024-07-30 13:57:17 -0300

    chore: remove redundant comment

--- explorer/priv/repo/migrations/20240711163410_add_cost_per_proof_column_in_batches.exs
+++ explorer/priv/repo/migrations/20240711163410_add_cost_per_proof_column_in_batches.exs
@@ -2,7 +2,7 @@ defmodule Explorer.Repo.Migrations.AddCostPerProofColumnInBatches do
   use Ecto.Migration
 
   def change do
-    alter table("batches") do # add new columns
+    alter table("batches") do
       add :cost_per_proof, :bigint
     end
   end
