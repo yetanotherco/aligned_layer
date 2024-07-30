eccc6e4b692d9dd653e84ae52220a2da7cfa8519
Gian <58370608+glpecile@users.noreply.github.com>
2024-07-30 17:17:40 -0300

    feat: add operator name and logo to table

--- explorer/lib/explorer_web/live/pages/operators/index.ex
+++ explorer/lib/explorer_web/live/pages/operators/index.ex
@@ -3,7 +3,13 @@ defmodule ExplorerWeb.Operators.Index do
 
   @impl true
   def mount(_, _, socket) do
-    {:ok, assign(socket, page_title: "Operators", operators: Operators.get_operators())}
+    {:ok, assign(socket, page_title: "Operators")}
+  end
+
+  @impl true
+  def handle_params(_params, _url, socket) do
+    operators = Operators.get_operators()
+    {:noreply, assign(socket, operators: operators)}
   end
 
   embed_templates "*"
