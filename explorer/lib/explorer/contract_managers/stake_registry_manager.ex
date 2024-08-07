7eb54b9a2460573ae1dae894167bf71dd8935d1e
Urix <43704209+uri-99@users.noreply.github.com>
2024-08-07 17:51:27 -0300

    feat: stake_registry_manager and restakings model

--- explorer/lib/explorer/contract_managers/stake_registry_manager.ex
+++ explorer/lib/explorer/contract_managers/stake_registry_manager.ex
@@ -13,7 +13,7 @@ defmodule StakeRegistryManager do
 
   case status do
     :ok ->
-      Logger.debug("Eigenlayer deployment file read successfully")
+      Logger.debug("Aligned deployment file read successfully")
 
     :error ->
       raise(
@@ -26,11 +26,95 @@ defmodule StakeRegistryManager do
                          |> Map.get("stakeRegistry")
 
   use Ethers.Contract,
-    abi_file: "lib/abi/IStakeRegistry.json",
+    abi_file: "lib/abi/StakeRegistry.json",
     default_address: @stake_registry_manager
 
   def get_stake_registry_manager() do
     @stake_registry_manager
   end
 
+  def get_latest_stake_update(%{fromBlock: fromBlock, operator_id: operator_id}) do
+      StakeRegistryManager.EventFilters.operator_stake_update(operator_id)
+        |> Ethers.get_logs(fromBlock: fromBlock)
+        |> case do
+          {:ok, data} ->
+            List.last(data) # most recent entry
+
+          {:error, reason} ->
+            dbg("Error getting latest operator stake update")
+            dbg(reason)
+
+          other ->
+            dbg("Unexpected response:")
+            dbg(other)
+        end
+  end
+
+  def get_strategies_of_quorum(quorum_number) do
+    "get_strategies_of_quorum" |> dbg
+
+    amount_of_strategies = case StakeRegistryManager.strategy_params_length(quorum_number) |> Ethers.call() do
+      {:ok, amount} ->
+        amount
+      {:error, error} ->
+        dbg("Error fetching amount of strategies: #{error}")
+        raise("Error fetching amount of strategies: #{error}")
+    end
+
+    strategies = Enum.reduce(0..(amount_of_strategies - 1), [], fn index, acc ->
+      case StakeRegistryManager.strategies_per_quorum(quorum_number, index) |> Ethers.call() do
+        {:ok, strategy_address} ->
+          [strategy_address | acc]
+        {:error, error} ->
+          dbg("Error fetching strategy at index #{index}: #{error}")
+          acc
+      end
+    end)
+
+    strategies |> Enum.reverse()
+  end
+
 end
+
+  # relevant structs:
+  # /// @ struct used to store the stakes of an individual operator or the sum of all operators' stakes, for storage
+  # struct StakeUpdate {
+  #     // the block number at which the stake amounts were updated and stored
+  #     uint32 updateBlockNumber;
+  #     // the block number at which the *next update* occurred.
+  #     /// @notice This entry has the value **0** until another update takes place.
+  #     uint32 nextUpdateBlockNumber;
+  #     // stake weight for the quorum
+  #     uint96 stake;
+  # }
+
+  # relevant events:
+
+  # /// @ emitted whenever the stake of `operator` is updated
+  # event OperatorStakeUpdate(
+  #     bytes32 indexed operatorId,
+  #     uint8 quorumNumber,
+  #     uint96 stake
+  # );
+
+  # relevant views:
+#   /**
+#   * @ This function computes the total weight of the @param operator in the quorum @param quorumNumber.
+#   */
+#  function weightOfOperatorForQuorum(uint8 quorumNumber, address operator) external view returns (uint96);
+
+# /**
+# * @ Returns the most recent stake weight for the `operatorId` for a certain quorum
+# * @dev Function returns an StakeUpdate struct with **every entry equal to 0** in the event that the operator has no stake history
+# */
+# function getLatestStakeUpdate(bytes32 operatorId, uint8 quorumNumber) external view returns (StakeUpdate memory);
+
+# /**
+# * @ Returns the stake weight from the latest entry in `_totalStakeHistory` for quorum `quorumNumber`.
+# */
+# function getCurrentTotalStake(uint8 quorumNumber) external view returns (uint96);
+
+# /**
+# * @ Returns the most recent stake weight for the `operatorId` for quorum `quorumNumber`
+# */
+# function getCurrentStake(bytes32 operatorId, uint8 quorumNumber) external view returns (uint96);
--- explorer/lib/explorer/models/restakings.ex
+++ explorer/lib/explorer/models/restakings.ex
@@ -1,11 +1,12 @@
 defmodule Restakings do
   use Ecto.Schema
   import Ecto.Changeset
-  # import Ecto.Query
+  import Ecto.Query
 
   schema "restakings" do
-    field :operator_id, :integer
-    field :amount, :decimal
+    field :operator_id, :binary
+    field :stake, :decimal
+    field :quorum_number, :integer
 
     timestamps()
   end
@@ -13,8 +14,59 @@ defmodule Restakings do
   @doc false
   def changeset(restaking, attrs) do
     restaking
-    |> cast(attrs, [:operator_id, :amount])
-    |> validate_required([:operator_id, :amount])
+    |> cast(attrs, [:operator_id, :stake, :quorum_number])
+    |> validate_required([:operator_id, :stake, :quorum_number])
+  end
+
+  def generate_changeset(%Restakings{} = restaking) do
+    Restakings.changeset(%Restakings{}, Map.from_struct(restaking))
+  end
+
+  def process_restaking_changes(%{fromBlock: from_block}) do
+    Operators.get_operators()
+      |> Enum.map(fn operator -> StakeRegistryManager.get_latest_stake_update(%{fromBlock: from_block, operator_id: operator.id}) end)
+      |> Enum.map(&parse_stake_update_event/1)
+      |> Enum.map(&insert_or_update_restakings/1)
+  end
+
+  def parse_stake_update_event(%Ethers.Event{} = event) do
+    %Restakings{
+      operator_id: Enum.at(event.topics, 1),
+      quorum_number: Enum.at(event.data, 0),
+      stake: Enum.at(event.data, 1)
+    }
+  end
+
+  def insert_or_update_restakings(%Restakings{} = restaking) do
+    dbg restaking
+    changeset = restaking |> generate_changeset()
+    Quorums.handle_quorum(%Quorums{id: restaking.quorum_number})
+
+    case Restakings.get_by_quorum_and_operator_id(restaking.quorum_number, restaking.operator_id) do
+      nil ->
+        "inserting restaking" |> dbg
+        Explorer.Repo.insert(changeset)
+
+      [] ->
+        "inserting restaking" |> dbg
+        Explorer.Repo.insert(changeset)
+
+      existing_restaking ->
+        "updating restaking" |> dbg
+        Explorer.Repo.update(Ecto.Changeset.change(existing_restaking, changeset.changes))
+
+    end
+  end
+
+  def get_by_quorum_and_operator_id(quorum_number, operator_id) do
+    query = from(
+      r in Restakings,
+      where: r.quorum_number == ^quorum_number
+           and r.operator_id == ^operator_id,
+      select: r
+    )
+
+    Explorer.Repo.one(query)
   end
 
 end
