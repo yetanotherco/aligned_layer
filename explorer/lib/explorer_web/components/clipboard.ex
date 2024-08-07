6366d83cc0c58529800939b40a6087bd020f5743
Gian <58370608+glpecile@users.noreply.github.com>
2024-08-07 16:52:53 -0300

    feat: add copy to clipboard live component

---
+++ explorer/lib/explorer_web/components/clipboard.ex
@@ -0,0 +1,34 @@
+defmodule CopyToClipboardButtonComponent do
+  use ExplorerWeb, :live_component
+
+  attr :text_to_copy, :string, required: true
+  attr :class, :string, default: nil
+
+  @impl true
+  def mount(socket) do
+    {:ok, assign(socket, text_to_copy: @text_to_copy)}
+  end
+
+  @impl true
+  def handle_event("copied", _params, socket) do
+    {:noreply, put_flash!(socket, :info, "Copied to clipboard!")}
+  end
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <button
+      class={[
+        "flex items-center justify-center size-7 rounded-full bg-foreground/10 hover:bg-foreground/20 text-foreground/80 hover:text-foreground/100",
+        @class
+      ]}
+      phx-hook="CopyToClipboard"
+      data-clipboard-text={@text_to_copy}
+      id="copy-to-clipboard"
+    >
+      <.icon name="hero-clipboard" class="size-3" />
+      <span class="sr-only">Copy to clipboard</span>
+    </button>
+    """
+  end
+end
