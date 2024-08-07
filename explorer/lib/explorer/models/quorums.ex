a43ec9d34a7a2ba6ecf7fc1e0d065c556b50fa5c
Urix <43704209+uri-99@users.noreply.github.com>
2024-08-07 17:57:52 -0300

    feat: quorums model

---
+++ explorer/lib/explorer/models/quorums.ex
@@ -0,0 +1,117 @@
+defmodule Quorums do
+  use Ecto.Schema
+  import Ecto.Changeset
+  import Ecto.Query
+
+  schema "quorums" do
+    many_to_many :strategies, Strategies, join_through: "quorum_strategies"
+    timestamps()
+  end
+
+
+  @doc false
+  # def changeset(x, attrs) do
+  #   x
+  #     |> cast(attrs, [])
+  # end
+
+  # def generate_changeset(%Quorums{} = quorum) do
+  # end
+
+  def process_quorum_changes() do
+    our_quorums = get_all_quorums()
+
+    Enum.each(our_quorums, &handle_quorum/1)
+  end
+
+  def handle_quorum(%Quorums{} = quorum) do
+    strategy_addresses = StakeRegistryManager.get_strategies_of_quorum(quorum.id)
+
+    insert_quorum_if_not_present(quorum)
+
+    Enum.each(strategy_addresses,
+      fn strategy_address ->
+        QuorumStrategies.insert_quorum_strategy(
+          quorum,
+          Strategies.get_by_strategy_address(strategy_address)
+        )
+      end)
+
+    quorum
+  end
+
+  def insert_quorum_if_not_present(%Quorums{} = quorum) do
+    dbg "inserting quorum?"
+    case get_quorum_by_id(quorum.id) do
+      nil ->
+        dbg "inserted quorum"
+        Explorer.Repo.insert(quorum)
+      _ ->
+        nil
+    end
+  end
+
+  def get_quorum_by_id(quorum_id) do
+    query = from(q in Quorums,
+      where: q.id == ^quorum_id,
+      select: q)
+    Explorer.Repo.one(query)
+  end
+
+  def get_all_quorums() do
+    query = from(q in Quorums,
+      select: q)
+    Explorer.Repo.all(query)
+  end
+end
+
+defmodule QuorumStrategies do
+  use Ecto.Schema
+  import Ecto.Changeset
+  import Ecto.Query
+
+  schema "quorum_strategies" do
+    belongs_to :quorum, Quorum
+    belongs_to :strategy, Strategy
+    timestamps()
+  end
+
+  def changeset(quorum_strategy, attrs) do
+    quorum_strategy
+    |> cast(attrs, [:quorum_id, :strategy_id])
+    |> validate_required([:quorum_id, :strategy_id])
+    |> unique_constraint([:quorum_id, :strategy_id])
+  end
+
+  def generate_changeset(quorum_id, strategy_id) do
+    QuorumStrategies.changeset(%QuorumStrategies{}, Map.from_struct(%QuorumStrategies{quorum_id: quorum_id, strategy_id: strategy_id})) |> tap(&dbg/1)
+  end
+
+
+  def get_quorum_strategy_associations(%Quorums{} = quorum) do
+    dbg "get_quorum_strategy_associations"
+    dbg quorum
+    query = from(qs in "quorum_strategies",
+      where: qs.quorum_id == ^quorum.id,
+      select: qs.strategy_id)
+    Explorer.Repo.all(query)
+  end
+
+  def insert_quorum_strategy(%Quorums{} = quorum, %Strategies{} = strategy) do
+    dbg "pre inserting quorum-strategy"
+
+    existing_strategies = QuorumStrategies.get_quorum_strategy_associations(quorum)
+    dbg "existing_strategies: #{inspect(existing_strategies)}"
+    dbg "strategy: #{inspect(strategy)}"
+
+    unless strategy.id in existing_strategies do
+      dbg "inserting quorum-strategy"
+      QuorumStrategies.generate_changeset(quorum.id, strategy.id) |> Explorer.Repo.insert() |> dbg
+      dbg "done"
+    end
+  end
+  def insert_quorum_strategy(_any, nil) do
+    dbg "trying to insert a nil or errored strategy, skipping"
+    nil
+  end
+end
