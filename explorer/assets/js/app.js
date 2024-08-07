74624c9bd83f5c7837849ebfbab2173871a69e6b
Gian <58370608+glpecile@users.noreply.github.com>
2024-08-07 16:52:32 -0300

    feat: add clipboard hook

--- explorer/assets/js/app.js
+++ explorer/assets/js/app.js
@@ -1,33 +1,16 @@
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
-
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "../vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package --prefix assets` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
-
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html";
-// Establish Phoenix Socket and LiveView configuration.
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
 
 import darkModeHook from "../vendor/dark_mode";
 import searchFocusHook from "../vendor/search_focus";
+import copyToClipboardHook from "../vendor/clipboard";
 
 let Hooks = {};
 Hooks.DarkThemeToggle = darkModeHook;
 Hooks.SearchFocus = searchFocusHook;
+Hooks.CopyToClipboard = copyToClipboardHook;
 
 let csrfToken = document
 	.querySelector("meta[name='csrf-token']")
@@ -38,7 +21,6 @@ let liveSocket = new LiveSocket("/live", Socket, {
 	hooks: Hooks
 });
 
-// Show progress bar on live navigation and form submits
 topbar.config({
 	barColors: { 0: "#18FF7F" },
 	shadowColor: "rgba(0, 0, 0, .3)"
@@ -50,7 +32,6 @@ window.addEventListener("phx:page-loading-stop", (_info) =>
 	topbar.hide()
 );
 
-// connect if there are any LiveViews on the page
 liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
