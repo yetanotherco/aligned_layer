a10bfbf7648412d3a34c231fd42b526290393217
Santos Rosati <rosatisantos@gmail.com>
2024-08-07 13:10:09 -0300

    fix (halo2): dont allow empty inputs

--- operator/halo2kzg/halo2kzg.go
+++ operator/halo2kzg/halo2kzg.go
@@ -9,30 +9,34 @@ import "C"
 import "unsafe"
 
 // MaxProofSize 4KB
-const MaxProofSize =  8 * 1024;
+const MaxProofSize = 8 * 1024
 
 // MaxProofSize 4KB
-const MaxParamsSize =  8 * 1024;
+const MaxParamsSize = 8 * 1024
 
 // MaxConstraintSystemSize 2KB
-const MaxConstraintSystemSize = 2 * 1024;
+const MaxConstraintSystemSize = 2 * 1024
 
 // MaxVerificationKeySize 1KB
-const MaxVerifierKeySize = 1024;
+const MaxVerifierKeySize = 1024
 
 // MaxKzgParamsSize 4KB
-const MaxKzgParamsSize = 4 * 1024;
+const MaxKzgParamsSize = 4 * 1024
 
 // MaxPublicInputSize 4KB
-const MaxPublicInputSize = 4 * 1024;
+const MaxPublicInputSize = 4 * 1024
 
 func VerifyHalo2KzgProof(
-	proofBuffer [MaxProofSize]byte, proofLen_u32 uint32, 
-	csBuffer [MaxConstraintSystemSize]byte, csLen_u32 uint32, 
-	vkBuffer [MaxVerifierKeySize]byte, vkLen_u32 uint32, 
-	kzgParamBuffer [MaxKzgParamsSize]byte, kzgParamLen_u32 uint32, 
+	proofBuffer [MaxProofSize]byte, proofLen_u32 uint32,
+	csBuffer [MaxConstraintSystemSize]byte, csLen_u32 uint32,
+	vkBuffer [MaxVerifierKeySize]byte, vkLen_u32 uint32,
+	kzgParamBuffer [MaxKzgParamsSize]byte, kzgParamLen_u32 uint32,
 	publicInputBuffer [MaxPublicInputSize]byte, publicInputLen_u32 uint32,
 ) bool {
+	if len(proofBuffer) < 0 || len(csBuffer) < 0 || len(vkBuffer) < 0 || len(kzgParamBuffer) < 0 || len(publicInputBuffer) < 0 {
+		return false
+	}
+
 	// Cast data pointers to C-types
 	proofPtr := (*C.uchar)(unsafe.Pointer(&proofBuffer[0]))
 	csPtr := (*C.uchar)(unsafe.Pointer(&csBuffer[0]))
@@ -42,13 +46,13 @@ func VerifyHalo2KzgProof(
 
 	// Cast data lengths to C-Types
 	proofLen := (C.uint32_t)(proofLen_u32)
-	csLen := (C.uint32_t)(csLen_u32) 
+	csLen := (C.uint32_t)(csLen_u32)
 	vkLen := (C.uint32_t)(vkLen_u32)
 	kzgParamLen := (C.uint32_t)(kzgParamLen_u32)
-	publicInputLen := (C.uint32_t)(publicInputLen_u32) 
+	publicInputLen := (C.uint32_t)(publicInputLen_u32)
 
 	return (bool)(C.verify_halo2_kzg_proof_ffi(
-		proofPtr, proofLen, 
+		proofPtr, proofLen,
 		csPtr, csLen,
 		vkPtr, vkLen,
 		kzgParamPtr, kzgParamLen,
