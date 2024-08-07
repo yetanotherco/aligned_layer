ae0599313eef154b3c9a91adda055a92d0c2aa05
Urix <43704209+uri-99@users.noreply.github.com>
2024-08-07 17:59:39 -0300

    fix: strategies model

--- explorer/lib/explorer/models/strategies.ex
+++ explorer/lib/explorer/models/strategies.ex
@@ -3,8 +3,6 @@ defmodule Strategies do
   import Ecto.Changeset
   import Ecto.Query
 
-  use Ethers.Contract,
-  abi_file: "lib/abi/IStrategy.json"
 
   schema "strategies" do
     field :strategy_address, :binary
@@ -12,6 +10,7 @@ defmodule Strategies do
     field :name, :string
     field :symbol, :string
     field :total_staked, :decimal
+    many_to_many :quorums, Quorums, join_through: "quorum_strategies"
 
     timestamps()
   end
@@ -46,60 +45,14 @@ defmodule Strategies do
 
   def extract_info(strategy_address) do
     %Strategies{strategy_address: strategy_address}
-    |> fetch_token_address()
-    |> fetch_token_name()
-    |> fetch_token_symbol()
-    |> tap(&dbg/1)
+    |> StrategyManager.fetch_token_address()
+    |> StrategyManager.fetch_token_name()
+    |> StrategyManager.fetch_token_symbol()
     # Total stake is set when inserting rows to `Restakings` table
-  end
-
-  defp fetch_token_address(%Strategies{strategy_address: strategy_address} = strategy) do
-    case Strategies.underlying_token() |> Ethers.call(to: strategy_address) do
-      {:ok, "0x"} ->
-        dbg("Strategy has invalid underlying token: #{strategy_address}, token_address: '0x'")
-        {:error, :invalid_token_address}
-      {:ok, token_address} -> %{strategy | token_address: token_address}
-
-      {:error, %{"code" => -32015}} ->
-        dbg("Strategy has no underlying token: #{strategy_address}") # thus, its not a strategy contract
-        {:error, :not_strategy}
-
-        other_error ->
-        dbg("Error fetching token address for #{strategy_address}")
-        dbg(other_error)
-        other_error
-    end
-  end
-
-  defp fetch_token_name(%Strategies{token_address: token_address} = strategy) do
-    case ERC20.name(token_address) do
-      {:ok, name} -> %{strategy | name: name}
-      error ->
-        dbg("Error fetching token name")
-        dbg(error)
-        error
-    end
-  end
-  defp fetch_token_name({:error, error}) do
-    {:error, error}
-  end
-
-  defp fetch_token_symbol(%Strategies{token_address: token_address} = strategy) do
-    case ERC20.symbol(token_address) do
-      {:ok, symbol} -> %{strategy | symbol: symbol}
-      error ->
-        dbg("Error fetching token symbol")
-        dbg(error)
-        error
-    end
-  end
-  defp fetch_token_symbol({:error, error}) do
-    {:error, error}
   end
 
   def add_strategy(%Strategies{} = new_strategy) do
     dbg("adding strategy")
-    dbg(new_strategy)
     Strategies.generate_changeset(new_strategy) |> Explorer.Repo.insert()
   end
   def add_strategy({:error, _error}) do
