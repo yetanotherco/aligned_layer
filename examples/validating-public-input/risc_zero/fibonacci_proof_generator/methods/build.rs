df3c9e1552f2f8a517604b7de757923e9ddeddeb
NicolasRampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-08-08 10:38:59 -0300

    chore: bump up risc0 version

--- examples/validating-public-input/risc_zero/fibonacci_proof_generator/methods/build.rs
+++ examples/validating-public-input/risc_zero/fibonacci_proof_generator/methods/build.rs
@@ -1,3 +1,17 @@
+use std::collections::HashMap;
+
+use risc0_build::{DockerOptions, GuestOptions};
+
 fn main() {
-    risc0_build::embed_methods();
+    let docker_options = Some(DockerOptions { root_dir: None });
+
+    let guest_options = HashMap::from([(
+        "risc0-zkvm-methods-guest",
+        GuestOptions {
+            features: vec![],
+            use_docker: docker_options,
+        },
+    )]);
+
+    risc0_build::embed_methods_with_options(guest_options);
 }
