fb0036449b794d67cc990f80f2feebb46f525a41
Gian <58370608+glpecile@users.noreply.github.com>
2024-08-08 16:18:36 -0300

    chore: remove assets from nav

--- explorer/lib/explorer_web/components/nav.ex
+++ explorer/lib/explorer_web/components/nav.ex
@@ -29,9 +29,6 @@ defmodule NavComponent do
           >
             Operators
           </.link>
-          <.link class="text-foreground/80 hover:text-foreground font-semibold" navigate={~p"/assets"}>
-            Assets
-          </.link>
         </div>
       </div>
       <.live_component module={SearchComponent} id="nav_search" />
