836ff0a6615a982a0bd8eae7115a1c06debbfb91
Santos Rosati <rosatisantos@gmail.com>
2024-08-13 16:25:52 -0300

    fix: lint

--- operator/halo2ipa/lib/halo2ipa.h
+++ operator/halo2ipa/lib/halo2ipa.h
@@ -2,6 +2,7 @@
 #include <stdint.h>
 
 bool verify_halo2_ipa_proof_ffi(unsigned char *proof_bytes, uint32_t proof_len,
-                                unsigned char *params_bytes, uint32_t params_len,
-                                unsigned char *public_input_bytes, uint32_t public_input_len);
-                                
+                                unsigned char *params_bytes,
+                                uint32_t params_len,
+                                unsigned char *public_input_bytes,
+                                uint32_t public_input_len);
