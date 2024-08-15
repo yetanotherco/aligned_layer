b75e1e2988f741bcb0387a1219fe1c409f25da04
gabrielbosio <gabrielbosio95@gmail.com>
2024-08-15 17:29:06 -0300

    Measure block verification time

--- operator/mina/mina.go
+++ operator/mina/mina.go
@@ -8,6 +8,8 @@ package mina
 */
 import "C"
 import (
+	"fmt"
+	"time"
 	"unsafe"
 )
 
@@ -15,7 +17,15 @@ import (
 const MAX_PROOF_SIZE = 16 * 1024
 const MAX_PUB_INPUT_SIZE = 6 * 1024
 
+func timer() func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("Mina block verification took %v\n", time.Since(start))
+	}
+}
+
 func VerifyProtocolStateProof(proofBuffer [MAX_PROOF_SIZE]byte, proofLen uint, pubInputBuffer [MAX_PUB_INPUT_SIZE]byte, pubInputLen uint) bool {
+	defer timer()()
 	proofPtr := (*C.uchar)(unsafe.Pointer(&proofBuffer[0]))
 	pubInputPtr := (*C.uchar)(unsafe.Pointer(&pubInputBuffer[0]))
 	return (bool)(C.verify_protocol_state_proof_ffi(proofPtr, (C.uint)(proofLen), pubInputPtr, (C.uint)(pubInputLen)))
