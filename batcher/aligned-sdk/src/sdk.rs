ec485800be18946dd62b063ca1ee7dace538b692
gabrielbosio <gabrielbosio95@gmail.com>
2024-08-16 17:30:41 -0300

    Add Holesky Aligned SM contract addr disclaimer

--- batcher/aligned-sdk/src/sdk.rs
+++ batcher/aligned-sdk/src/sdk.rs
@@ -308,6 +308,7 @@ async fn _verify_proof_onchain(
 ) -> Result<bool, errors::VerificationError> {
     let contract_address = match chain {
         Chain::Devnet => "0x1613beB3B2C4f22Ee086B2b38C1476A3cE7f78E8",
+        // If we re-deploy the Aligned SM contract we need to change this value to the new contract address
         Chain::Holesky => "0x0584313310bD52B77CF0b81b350Ca447B97Df5DF",
         Chain::HoleskyStage => "0x9C5231FC88059C086Ea95712d105A2026048c39B",
     };
