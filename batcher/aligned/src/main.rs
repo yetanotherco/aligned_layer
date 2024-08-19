17c9f0c6f3d02145f798dcdcfe1c9330dc0f6985
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-08-19 16:08:36 -0300

    Added new proving system to batcher

--- batcher/aligned/src/main.rs
+++ batcher/aligned/src/main.rs
@@ -228,6 +228,8 @@ pub enum ProvingSystemArg {
     Risc0,
     #[clap(name = "Mina")]
     Mina,
+    #[clap(name = "MinaAccount")]
+    MinaAccount,
 }
 
 const ANVIL_PRIVATE_KEY: &str = "2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6"; // Anvil address 9
@@ -243,6 +245,7 @@ impl From<ProvingSystemArg> for ProvingSystemId {
             ProvingSystemArg::Halo2IPA => ProvingSystemId::Halo2IPA,
             ProvingSystemArg::Risc0 => ProvingSystemId::Risc0,
             ProvingSystemArg::Mina => ProvingSystemId::Mina,
+            ProvingSystemArg::MinaAccount => ProvingSystemId::MinaAccount,
         }
     }
 }
@@ -572,6 +575,12 @@ fn verification_data_from_args(args: SubmitArgs) -> Result<VerificationData, Sub
             )?);
         }
         ProvingSystemId::Mina => {
+            pub_input = Some(read_file_option(
+                "--public_input",
+                args.pub_input_file_name,
+            )?)
+        }
+        ProvingSystemId::MinaAccount => {
             pub_input = Some(read_file_option(
                 "--public_input",
                 args.pub_input_file_name,
