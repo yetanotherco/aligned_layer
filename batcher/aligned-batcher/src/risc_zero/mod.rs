d3f16256ce77e94a93caa61932f2583a315a4bdf
Nicolas Rampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-08-21 15:46:17 -0300

    fix: risc0 verifier public inputs in journal and public inputs may not match (#730)
    Co-authored-by: Mariano A. Nicolini <mariano.nicolini.91@gmail.com>

--- batcher/aligned-batcher/src/risc_zero/mod.rs
+++ batcher/aligned-batcher/src/risc_zero/mod.rs
@@ -1,14 +1,12 @@
-use risc0_zkvm::Receipt;
+use risc0_zkvm::{InnerReceipt, Receipt};
 
 pub fn verify_risc_zero_proof(
-    receipt_bytes: &[u8],
+    inner_receipt_bytes: &[u8],
     image_id: &[u8; 32],
     public_input: &[u8],
 ) -> bool {
-    if let Ok(receipt) = bincode::deserialize::<Receipt>(receipt_bytes) {
-        if public_input != receipt.journal.bytes {
-            return false;
-        }
+    if let Ok(inner_receipt) = bincode::deserialize::<InnerReceipt>(inner_receipt_bytes) {
+        let receipt = Receipt::new(inner_receipt, public_input.to_vec());
 
         return receipt.verify(*image_id).is_ok();
     }
--- operator/risc_zero/risc_zero.go
+++ operator/risc_zero/risc_zero.go
@@ -11,18 +11,18 @@ import (
 	"unsafe"
 )
 
-func VerifyRiscZeroReceipt(receiptBuffer []byte, receiptLen uint32, imageIdBuffer []byte, imageIdLen uint32, publicInput []byte, publicInputLen uint32) bool {
-	if len(receiptBuffer) == 0 || len(imageIdBuffer) == 0 {
+func VerifyRiscZeroReceipt(innerReceiptBuffer []byte, innerReceiptLen uint32, imageIdBuffer []byte, imageIdLen uint32, publicInput []byte, publicInputLen uint32) bool {
+	if len(innerReceiptBuffer) == 0 || len(imageIdBuffer) == 0 {
 		return false
 	}
 
-	receiptPtr := (*C.uchar)(unsafe.Pointer(&receiptBuffer[0]))
+	receiptPtr := (*C.uchar)(unsafe.Pointer(&innerReceiptBuffer[0]))
 	imageIdPtr := (*C.uchar)(unsafe.Pointer(&imageIdBuffer[0]))
 
 	if len(publicInput) == 0 { // allow empty public input
-		return (bool)(C.verify_risc_zero_receipt_ffi(receiptPtr, (C.uint32_t)(receiptLen), imageIdPtr, (C.uint32_t)(imageIdLen), nil, (C.uint32_t)(0)))
+		return (bool)(C.verify_risc_zero_receipt_ffi(receiptPtr, (C.uint32_t)(innerReceiptLen), imageIdPtr, (C.uint32_t)(imageIdLen), nil, (C.uint32_t)(0)))
 	}
 
 	publicInputPtr := (*C.uchar)(unsafe.Pointer(&publicInput[0]))
-	return (bool)(C.verify_risc_zero_receipt_ffi(receiptPtr, (C.uint32_t)(receiptLen), imageIdPtr, (C.uint32_t)(imageIdLen), publicInputPtr, (C.uint32_t)(publicInputLen)))
+	return (bool)(C.verify_risc_zero_receipt_ffi(receiptPtr, (C.uint32_t)(innerReceiptLen), imageIdPtr, (C.uint32_t)(imageIdLen), publicInputPtr, (C.uint32_t)(publicInputLen)))
 }
