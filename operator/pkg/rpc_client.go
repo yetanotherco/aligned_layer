48d7cc874d5546eff6b2e92a5a83f86f55430688
Urix <43704209+uri-99@users.noreply.github.com>
2024-08-26 19:28:05 -0300

    feat: rpc_client SendSignedTaskResponseToAggregatorV2

--- operator/pkg/rpc_client.go
+++ operator/pkg/rpc_client.go
@@ -62,3 +62,29 @@ func (c *AggregatorRpcClient) SendSignedTaskResponseToAggregator(signedTaskRespo
 		}
 	}
 }
+func (c *AggregatorRpcClient) SendSignedTaskResponseToAggregatorV2(signedTaskResponse *types.SignedTaskResponseV2) {
+	var reply uint8
+	for retries := 0; retries < MaxRetries; retries++ {
+		err := c.rpcClient.Call("Aggregator.ProcessOperatorSignedTaskResponseV2", signedTaskResponse, &reply)
+		if err != nil {
+			c.logger.Error("Received error from aggregator", "err", err)
+			if errors.Is(err, rpc.ErrShutdown) {
+				c.logger.Error("Aggregator is shutdown. Reconnecting...")
+				client, err := rpc.DialHTTP("tcp", c.aggregatorIpPortAddr)
+				if err != nil {
+					c.logger.Error("Could not reconnect to aggregator", "err", err)
+					time.Sleep(RetryInterval)
+				} else {
+					c.rpcClient = client
+					c.logger.Info("Reconnected to aggregator")
+				}
+			} else {
+				c.logger.Infof("Received error from aggregator: %s. Retrying ProcessOperatorSignedTaskResponseV2 RPC call...", err)
+				time.Sleep(RetryInterval)
+			}
+		} else {
+			c.logger.Info("Signed task response header accepted by aggregator.", "reply", reply)
+			return
+		}
+	}
+}
