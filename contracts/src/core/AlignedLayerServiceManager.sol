43b266e59ecded6f626c05fc66752e65a4adaf12
Nicolas Rampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-08-27 16:14:00 -0300

    feat: add a deposit to batcher method in servicemanager (#876)

--- contracts/src/core/AlignedLayerServiceManager.sol
+++ contracts/src/core/AlignedLayerServiceManager.sol
@@ -223,9 +223,20 @@ contract AlignedLayerServiceManager is
         return batchersBalances[account];
     }
 
+    function depositToBatcher(address account) external payable {
+        _depositToBatcher(account, msg.value);
+    }
+
+    function _depositToBatcher(address account, uint256 amount) internal {
+        if (amount <= 0) {
+            revert InvalidDepositAmount(amount);
+        }
+        batchersBalances[account] += amount;
+        emit BatcherBalanceUpdated(account, batchersBalances[account]);
+    }
+
     receive() external payable {
-        batchersBalances[msg.sender] += msg.value;
-        emit BatcherBalanceUpdated(msg.sender, batchersBalances[msg.sender]);
+        _depositToBatcher(msg.sender, msg.value);
     }
 
     function checkPublicInput(
