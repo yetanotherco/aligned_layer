4e509c6bdb1c3f60db13aa4d583299a056f2961f
NicolasRampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-08-29 12:00:23 -0300

    fix: conditions from less equals to equals

--- contracts/src/core/BatcherPaymentService.sol
+++ contracts/src/core/BatcherPaymentService.sol
@@ -133,11 +133,11 @@ contract BatcherPaymentService is
             revert LeavesNotPowerOfTwo(leavesQty);
         }
 
-        if (feeForAggregator <= 0) {
+        if (feeForAggregator == 0) {
             revert NoGasForAggregator();
         }
 
-        if (feePerProof <= 0) {
+        if (feePerProof == 0) {
             revert NoGasPerProof();
         }
 
@@ -170,7 +170,7 @@ contract BatcherPaymentService is
     }
 
     function unlock() external whenNotPaused {
-        if (userData[msg.sender].balance <= 0) {
+        if (userData[msg.sender].balance == 0) {
             revert UserHasNoFundsToUnlock(msg.sender);
         }
 
@@ -179,7 +179,7 @@ contract BatcherPaymentService is
     }
 
     function lock() external whenNotPaused {
-        if (userData[msg.sender].balance <= 0) {
+        if (userData[msg.sender].balance == 0) {
             revert UserHasNoFundsToLock(msg.sender);
         }
         userData[msg.sender].unlockBlock = 0;
