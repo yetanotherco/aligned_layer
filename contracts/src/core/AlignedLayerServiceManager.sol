9aff32978ddfa97cdb222f10a8170c91433d442e
taturosati <taturosati@users.noreply.github.com>
2024-08-29 17:56:26 -0300

    fix (service-manager): merkle inclusion check

--- contracts/src/core/AlignedLayerServiceManager.sol
+++ contracts/src/core/AlignedLayerServiceManager.sol
@@ -198,7 +198,7 @@ contract AlignedLayerServiceManager is
         return
             Merkle.verifyInclusionKeccak(
                 merkleProof,
-                batchIdentifierHash,
+                batchMerkleRoot,
                 hashedLeaf,
                 verificationDataBatchIndex
             );
