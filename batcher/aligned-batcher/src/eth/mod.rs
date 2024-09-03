6f5051f2ce5077807ea91e231037d1a498723456
Urix <43704209+uri-99@users.noreply.github.com>
2024-09-03 18:20:14 -0300

    fix: add new param to create_new_task

--- batcher/aligned-batcher/src/eth/mod.rs
+++ batcher/aligned-batcher/src/eth/mod.rs
@@ -97,6 +97,7 @@ pub async fn try_create_new_task(
             signatures,
             fee_params.fee_for_aggregator,
             fee_params.fee_per_proof,
+            fee_params.fee_per_proof * 2
         )
         .gas_price(fee_params.gas_price);
 
