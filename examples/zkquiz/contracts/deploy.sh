89af0f72c896beab6d13a5f27fc0261e9e0bf097
taturosati <taturosati@users.noreply.github.com>
2024-09-05 19:19:44 -0300

    fix: update zkquiz to new sdk version

--- examples/zkquiz/contracts/deploy.sh
+++ examples/zkquiz/contracts/deploy.sh
@@ -11,6 +11,11 @@ if [ -z "$ALIGNED_SERVICE_MANAGER_ADDRESS" ]; then
     exit 1
 fi
 
+if [ -z "$BATCHER_PAYMENT_SERVICE_ADDRESS" ]; then
+    echo "BATCHER_PAYMENT_SERVICE_ADDRESS is not set. Please set it in .env"
+    exit 1
+fi
+
 if [ -z "$RPC_URL" ]; then
     echo "RPC_URL is not set. Please set it in .env"
     exit 1
@@ -24,8 +29,8 @@ fi
 forge install
 
 forge script script/Deployer.s.sol \
-    "$ALIGNED_SERVICE_MANAGER_ADDRESS" \
+    "$ALIGNED_SERVICE_MANAGER_ADDRESS" "$BATCHER_PAYMENT_SERVICE_ADDRESS" \
     --rpc-url "$RPC_URL" \
     --private-key "$PRIVATE_KEY" \
     --broadcast \
-    --sig "run(address _alignedServiceManager)"
+    --sig "run(address _alignedServiceManager, address _paymentService)"
