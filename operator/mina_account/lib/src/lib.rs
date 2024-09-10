6fe90f68937382c160cae615bd225b7e351d4b39
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-09-10 15:16:39 -0300

    Add new mina account proof

--- operator/mina_account/lib/src/lib.rs
+++ operator/mina_account/lib/src/lib.rs
@@ -1,5 +1,8 @@
 use merkle_verifier::verify_merkle_proof;
-use mina_bridge_core::proof::account_proof::{MinaAccountProof, MinaAccountPubInputs};
+use mina_bridge_core::{
+    proof::account_proof::{MinaAccountProof, MinaAccountPubInputs},
+    sol::account::MinaAccountValidation,
+};
 use mina_tree::Account;
 
 mod merkle_verifier;
@@ -25,14 +28,21 @@ pub extern "C" fn verify_account_inclusion_ffi(
             return false;
         }
     };
-    let MinaAccountPubInputs { ledger_hash, .. } =
-        match bincode::deserialize(&pub_input_buffer[..pub_input_len]) {
-            Ok(pub_inputs) => pub_inputs,
-            Err(err) => {
-                eprintln!("Failed to deserialize account pub inputs: {}", err);
-                return false;
-            }
-        };
+    let MinaAccountPubInputs {
+        ledger_hash,
+        encoded_account,
+    } = match bincode::deserialize(&pub_input_buffer[..pub_input_len]) {
+        Ok(pub_inputs) => pub_inputs,
+        Err(err) => {
+            eprintln!("Failed to deserialize account pub inputs: {}", err);
+            return false;
+        }
+    };
+
+    let expected_encoded_account = MinaAccountValidation::Account::try_from(&account)?.abi_encode();
+    if expected_encoded_account != encoded_account {
+        return Err("ABI encoded account in public inputs doesn't match the account on the proof");
+    }
 
     // the hash function for MinaBaseAccountBinableArgStableV2 produces a panic every
     // time it's called. So we use Account's one.
