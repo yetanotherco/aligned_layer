1476e4c8666066da10041184e72792e437f7de5b
Urix <43704209+uri-99@users.noreply.github.com>
2024-09-17 17:25:02 -0300

    refactor: rename old_ffi to ffi_old

--- operator/merkle_tree_old/lib/merkle_tree.h
+++ operator/merkle_tree_old/lib/merkle_tree.h
@@ -1,3 +1,3 @@
 #include <stdbool.h>
 
-bool verify_merkle_tree_batch_old_ffi(unsigned char *batch_bytes, unsigned int batch_len, unsigned char *merkle_root);
+bool verify_merkle_tree_batch_ffi_old(unsigned char *batch_bytes, unsigned int batch_len, unsigned char *merkle_root);
