6ba703a0ca5c7ab12c05838ff90b49ed08b5dd5e
PatStiles <pdstiles78@gmail.com>
2024-09-19 14:05:01 -0300

    change error name

--- batcher/aligned-batcher/src/types/errors.rs
+++ batcher/aligned-batcher/src/types/errors.rs
@@ -8,7 +8,7 @@ pub enum BatcherError {
     BatchVerifiedEventStreamError(String),
     EthereumSubscriptionError(String),
     SignatureError(SignatureError),
-    S3BatchUploadError(String),
+    BatchUploadError(String),
     TaskCreationError(String),
     ReceiptNotFoundError,
     TransactionSendError,
@@ -44,8 +44,8 @@ impl fmt::Debug for BatcherError {
             BatcherError::SignatureError(e) => {
                 write!(f, "Message signature verification error: {}", e)
             }
-            BatcherError::S3BatchUploadError(e) => {
-                write!(f, "Uploading Batch to S3 was not successful: {}", e)
+            BatcherError::BatchUploadError(e) => {
+                write!(f, "Uploading Batch was not successful: {}", e)
             }
             BatcherError::TaskCreationError(e) => {
                 write!(f, "Task creation error: {}", e)
