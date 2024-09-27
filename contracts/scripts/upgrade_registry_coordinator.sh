83c7a50be77ac07af955a246a22d4224a4193756
PatStiles <33334338+PatStiles@users.noreply.github.com>
2024-09-27 18:48:15 -0300

    feat(audit): Emit events in Operator whitelist (#1107)
    Co-authored-by: Urix <43704209+uri-99@users.noreply.github.com>
    Co-authored-by: JuArce <52429267+JuArce@users.noreply.github.com>

--- contracts/scripts/upgrade_registry_coordinator.sh
+++ contracts/scripts/upgrade_registry_coordinator.sh
@@ -14,6 +14,8 @@ forge_output=$(forge script script/upgrade/RegistryCoordinatorUpgrader.s.sol \
     --rpc-url $RPC_URL \
     --private-key $PRIVATE_KEY \
     --broadcast \
+    --verify \
+    --etherscan-api-key $ETHERSCAN_API_KEY \
     --sig "run(string memory eigenLayerDeploymentFilePath, string memory alignedLayerDeploymentFilePath, )")
 
 echo "$forge_output"
@@ -22,10 +24,10 @@ echo "$forge_output"
 new_registry_coordinator_implementation=$(echo "$forge_output" | awk '/1: address/ {print $3}')
 
 # Use the extracted value to replace the alignedLayerServiceManagerImplementation value in alignedlayer_deployment_output.json and save it to a temporary file
-jq --arg new_registry_coordinator_implementation "$new_registry_coordinator_implementation" '.addresses.registryCoordinatorImplementation = $new_registry_coordinator_implementation' "script/output/holesky/alignedlayer_deployment_output.json" > "script/output/holesky/alignedlayer_deployment_output.temp.json"
+jq --arg new_registry_coordinator_implementation "$new_registry_coordinator_implementation" '.addresses.registryCoordinatorImplementation = $new_registry_coordinator_implementation' $OUTPUT_PATH > "script/output/holesky/alignedlayer_deployment_output.temp.json"
 
 # Replace the original file with the temporary file
-mv "script/output/holesky/alignedlayer_deployment_output.temp.json" "script/output/holesky/alignedlayer_deployment_output.json"
+mv "script/output/holesky/alignedlayer_deployment_output.temp.json" $OUTPUT_PATH
 
 # Delete the temporary file
 rm -f "script/output/holesky/alignedlayer_deployment_output.temp.json"
