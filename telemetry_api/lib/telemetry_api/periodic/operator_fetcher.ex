365616b63caa9847d3089c848d05810073e3e13e
Julian Ventura <julian.ventura@lambdaclass.com>
2024-10-02 16:29:09 -0300

    Add operators data periodic fetcher

---
+++ telemetry_api/lib/telemetry_api/periodic/operator_fetcher.ex
@@ -0,0 +1,32 @@
+defmodule TelemetryApi.Periodic.OperatorFetcher do
+  use Task
+  alias TelemetryApi.Operators
+
+  wait_time_str = System.get_env("OPERATOR_FETCHER_WAIT_TIME_MS") ||
+    raise """
+    environment variable OPERATOR_FETCHER_WAIT_TIME_MS is missing.
+    """
+
+  @wait_time_ms (
+    case Integer.parse(wait_time_str) do
+      :error -> raise("OPERATOR_FETCHER_WAIT_TIME_MS is not a number")
+      {num, _} -> num
+    end
+  )
+
+  def start_link(_) do
+    Task.start_link(&poll_serivce/0)
+  end
+
+  defp poll_serivce() do
+    receive do
+    after
+      @wait_time_ms ->
+        case Operators.fetch_all_operators() do
+          {:ok, _} -> :ok
+          {:error, message} -> IO.inspect "Couldn't fetch operators: #{IO.inspect message}"
+        end
+        poll_serivce()
+    end
+  end
+end
