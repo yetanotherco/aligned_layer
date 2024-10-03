e911d642771db5869a9af7eb5c6960e11b445679
avilagaston9 <gavila@fi.uba.ar>
2024-10-03 10:20:50 -0300

    refactor: remove dead code

--- telemetry_api/lib/telemetry_api_web/controllers/operator_controller.ex
+++ telemetry_api/lib/telemetry_api_web/controllers/operator_controller.ex
@@ -8,9 +8,9 @@ defmodule TelemetryApiWeb.OperatorController do
 
   defp return_error(conn, message) do
     conn
-      |> put_status(:bad_request)
-      |> put_resp_content_type("application/json")
-      |> send_resp(:bad_request, Jason.encode!(%{error: message}))
+    |> put_status(:bad_request)
+    |> put_resp_content_type("application/json")
+    |> send_resp(:bad_request, Jason.encode!(%{error: message}))
   end
 
   def index(conn, _params) do
@@ -22,9 +22,10 @@ defmodule TelemetryApiWeb.OperatorController do
     case Operators.update_operator_version(operator_params) do
       {:ok, %Operator{} = operator} ->
         conn
-          |> put_status(:created)
-          |> put_resp_header("location", ~p"/api/operators/#{operator}")
-          |> render(:show, operator: operator)
+        |> put_status(:created)
+        |> put_resp_header("location", ~p"/api/operators/#{operator}")
+        |> render(:show, operator: operator)
+
       {:error, message} ->
         return_error(conn, message)
     end
@@ -42,20 +43,4 @@ defmodule TelemetryApiWeb.OperatorController do
         |> render("404.json", %{})
     end
   end
-
-  # defp update(conn, operator, operator_params) do
-  #   with {:ok, %Operator{} = operator} <- Operators.update_operator(operator, operator_params) do
-  #     conn
-  #     |> put_status(:ok)
-  #     |> render(:show, operator: operator)
-  #   end
-  # end
-
-  # def delete(conn, %{"id" => id}) do
-  #   operator = Operators.get_operator!(id)
-
-  #   with {:ok, %Operator{}} <- Operators.delete_operator(operator) do
-  #     send_resp(conn, :no_content, "")
-  #   end
-  # end
 end
