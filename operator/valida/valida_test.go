7187718a0d0cceb56e22773538d55ab467f52bac
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2024-10-04 02:51:49 -0300

    test(ffi): validia go

--- operator/valida/valida_test.go
+++ operator/valida/valida_test.go
@@ -1,5 +1,24 @@
 package valida_test
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestValidaProofVerifies(t *testing.T) {}
+	"github.com/yetanotherco/aligned_layer/operator/valida"
+)
+
+func TestCatValidaProofVerifies(t *testing.T) {
+	proofBytes, err := os.ReadFile("../../scripts/test_files/valida/cat/cat.proof")
+	if err != nil {
+		t.Errorf("could not open proof file: %s", err)
+	}
+
+	programCodeBytes, err := os.ReadFile("../../scripts/test_files/valida/cat/cat.bin")
+	if err != nil {
+		t.Errorf("could not open program code file: %s", err)
+	}
+
+	if !valida.VerifyValidaProof(proofBytes, programCodeBytes) {
+		t.Errorf("proof did not verify")
+	}
+}
