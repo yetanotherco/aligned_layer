efa739f7b559cf69c1056948d23c8ff07414e9ba
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-10-08 19:24:21 -0300

    fix: update storage GAP with new bitmap

--- contracts/src/core/AlignedLayerServiceManagerStorage.sol
+++ contracts/src/core/AlignedLayerServiceManagerStorage.sol
@@ -11,17 +11,17 @@ abstract contract AlignedLayerServiceManagerStorage {
     // KEY is keccak256(batchMerkleRoot,senderAddress)
     mapping(bytes32 => BatchState) public batchesState;
 
-    // Bitmap representing disabled verifiers
-    // Each verifier is disabled if its corresponding bit is set to 1
-    // The verifier index follows its corresponding value in the `ProvingSystemId` enum being 0 the first verifier.
-    uint256 public disabledVerifiers;
-
     // Storage for batchers balances. Used by aggregator to pay for respondToTask
     mapping(address => uint256) public batchersBalances;
 
     address public alignedAggregator;
 
+    // Bitmap representing disabled verifiers
+    // Each verifier is disabled if its corresponding bit is set to 1
+    // The verifier index follows its corresponding value in the `ProvingSystemId` enum being 0 the first verifier.
+    uint256 public disabledVerifiers;
+
     // storage gap for upgradeability
     // solhint-disable-next-line var-name-mixedcase
-    uint256[47] private __GAP;
+    uint256[46] private __GAP;
 }
