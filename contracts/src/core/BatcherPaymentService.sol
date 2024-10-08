6c98550250fefe12151710f48af60977b18c3477
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-10-08 12:01:00 -0300

    fix: add pausable init to batcher payment service initialization

--- contracts/src/core/BatcherPaymentService.sol
+++ contracts/src/core/BatcherPaymentService.sol
@@ -82,6 +82,7 @@ contract BatcherPaymentService is
         }
         __Ownable_init(); // default is msg.sender
         __UUPSUpgradeable_init();
+        __Pausable_init();
         _transferOwnership(_batcherPaymentServiceOwner);
 
         alignedLayerServiceManager = _alignedLayerServiceManager;
