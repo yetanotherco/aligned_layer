1a494b4996790b724069ee58e1977f55b7bf2da2
avilagaston9 <gavila@fi.uba.ar>
2024-10-15 14:40:53 -0300

    refactor: rename function validate_operator_registration

--- telemetry_api/lib/telemetry_api/traces.ex
+++ telemetry_api/lib/telemetry_api/traces.ex
@@ -63,7 +63,7 @@ defmodule TelemetryApi.Traces do
   """
   def register_operator_response(merkle_root, operator_id) do
     with {:ok, operator} <- Operators.get_operator(%{id: operator_id}),
-          :ok <- verify_operator_status(operator),
+          :ok <- validate_operator_registration(operator),
          {:ok, trace} <- set_current_trace(merkle_root) do
       operator_stake = Decimal.new(operator.stake)
       new_stake = Decimal.add(trace.current_stake, operator_stake)
@@ -188,7 +188,7 @@ defmodule TelemetryApi.Traces do
     end
   end
 
-  defp verify_operator_status(operator) do
+  defp validate_operator_registration(operator) do
     if Operators.is_registered?(operator) do
       :ok
     else
