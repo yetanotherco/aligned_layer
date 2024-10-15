a7fd0d554e2125ceea18a78332ff0a58cfd67eb4
avilagaston9 <gavila@fi.uba.ar>
2024-10-15 10:57:52 -0300

    fix: formatting issues

--- telemetry_api/lib/telemetry_api_web/endpoint.ex
+++ telemetry_api/lib/telemetry_api_web/endpoint.ex
@@ -11,45 +11,45 @@ defmodule TelemetryApiWeb.Endpoint do
     same_site: "Lax"
   ]
 
-  socket("/live", Phoenix.LiveView.Socket,
+  socket "/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
-  )
+
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug(Plug.Static,
+  plug Plug.Static,
     at: "/",
     from: :telemetry_api,
     gzip: false,
     only: TelemetryApiWeb.static_paths()
-  )
+
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    plug(Phoenix.CodeReloader)
-    plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :telemetry_api)
+    plug Phoenix.CodeReloader
+    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :telemetry_api
   end
 
   # plug Phoenix.LiveDashboard.RequestLogger,
   #   param_key: "request_logger",
   #   cookie_key: "request_logger"
 
-  plug(Plug.RequestId)
-  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
-  plug(Plug.Parsers,
+  plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
-  )
 
-  plug(TelemetryApi.MetricsExporter)
-  plug(Plug.MethodOverride)
-  plug(Plug.Head)
-  plug(Plug.Session, @session_options)
-  plug(TelemetryApiWeb.Router)
+
+  plug TelemetryApi.MetricsExporter
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, @session_options
+  plug TelemetryApiWeb.Router
 end
