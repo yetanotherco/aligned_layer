72343fe7bacd90f31465b36e056d29bda4e2bfeb
PatStiles <pdstiles78@gmail.com>
2024-10-16 03:17:08 -0300

    remove path declaration from build

--- examples/validating-public-input/sp1/fibonacci/script/build.rs
+++ examples/validating-public-input/sp1/fibonacci/script/build.rs
@@ -3,8 +3,6 @@ use sp1_build::{build_program_with_args, BuildArgs};
 fn main() {
     let args = BuildArgs {
         docker: true,
-        output_directory: "../".to_string(),
-        elf_name: "./fibonacci/sp1_fibonacci.elf".to_string(),
         ..Default::default()
     };
     build_program_with_args("../program", args);
