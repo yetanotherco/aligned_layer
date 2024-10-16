6790a4ea2768d060bc1af4921deb90aacf4b9336
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-10-16 16:20:33 -0300

    First version of retry function

--- batcher/aligned-batcher/src/connection.rs
+++ batcher/aligned-batcher/src/connection.rs
@@ -1,9 +1,12 @@
-use std::sync::Arc;
+use std::{future::Future, sync::Arc, time::Duration};
 
+use crate::types::{batch_queue::BatchQueueEntry, errors::BatcherError};
 use aligned_sdk::{
     communication::serialization::cbor_serialize,
     core::types::{BatchInclusionData, ResponseMessage, VerificationCommitmentBatch},
 };
+use backon::ExponentialBuilder;
+use backon::Retryable;
 use futures_util::{stream::SplitSink, SinkExt};
 use lambdaworks_crypto::merkle_tree::merkle::MerkleTree;
 use log::{error, info};
@@ -13,8 +16,6 @@ use tokio_tungstenite::{
     tungstenite::{Error, Message},
     WebSocketStream,
 };
-
-use crate::types::{batch_queue::BatchQueueEntry, errors::BatcherError};
 
 pub(crate) type WsMessageSink = Arc<RwLock<SplitSink<WebSocketStream<TcpStream>, Message>>>;
 
@@ -64,5 +65,80 @@ pub(crate) async fn send_message<T: Serialize>(ws_conn_sink: WsMessageSink, mess
             }
         }
         Err(e) => error!("Error while serializing message: {}", e),
+    }
+}
+
+#[derive(Debug)]
+enum RetryError<E> {
+    Transient,
+    Permanent(E),
+}
+
+impl<E> std::fmt::Display for RetryError<E> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Retry error!")
+    }
+}
+impl<E> std::error::Error for RetryError<E> where E: std::fmt::Debug {}
+
+async fn retry<FutureFn, Fut, T, E>(
+    function: FutureFn,
+    min_delay: u64,
+    factor: f32,
+    max_tries: usize,
+) -> Result<T, RetryError<E>>
+where
+    Fut: Future<Output = Result<T, RetryError<E>>>,
+    FutureFn: FnMut() -> Fut,
+{
+    let backoff = ExponentialBuilder::default()
+        .with_min_delay(Duration::from_millis(min_delay))
+        .with_max_times(max_tries)
+        .with_factor(factor);
+
+    function
+        .retry(backoff)
+        .sleep(tokio::time::sleep)
+        .when(|e| matches!(e, RetryError::Transient))
+        .await
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::eth;
+    use ethers::{providers::Middleware, types::U256};
+    use std::time::SystemTime;
+
+    #[tokio::test]
+    async fn retry_test() {
+        async fn dummy_action(x: u64) -> Result<u64, RetryError<()>> {
+            println!("Doing some operation...");
+            println!("Actual time: {:?}", SystemTime::now());
+            println!("X: {x}");
+
+            Err(RetryError::Permanent(()))
+        }
+
+        assert!(retry(|| dummy_action(10), 2000, 2.0, 3).await.is_err());
+    }
+
+    #[tokio::test]
+    async fn retry_test_eth() {
+        async fn get_gas_price() -> Result<U256, RetryError<()>> {
+            let eth_rpc_provider =
+                eth::get_provider(String::from("https://ethereum-holesky-rpc.publicnode.com"))
+                    .expect("Failed to get provider");
+
+            match eth_rpc_provider.get_gas_price().await {
+                Ok(val) => {
+                    println!("GAS PRICE IS: {:?}", val);
+                    Ok(val)
+                }
+                Err(_) => Err(RetryError::Transient),
+            }
+        }
+
+        assert!(retry(get_gas_price, 2000, 2.0, 3).await.is_ok());
     }
 }
