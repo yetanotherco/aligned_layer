cd43ddc5cae703d0db690369b0a897cd3062f5f3
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-10-16 10:40:34 -0300

    fix: rebuild elf file if its removed

--- examples/validating-public-input/sp1/fibonacci/script/build.rs
+++ examples/validating-public-input/sp1/fibonacci/script/build.rs
@@ -1,6 +1,9 @@
 use sp1_build::{build_program_with_args, BuildArgs};
 
 fn main() {
+    // This ensures that is the elf file gets removed or changed, the build script will be re-run.
+    println!("cargo::rerun-if-changed=../sp1_fibonacci.elf");
+
     let args = BuildArgs {
         docker: true,
         output_directory: "../".to_string(),
