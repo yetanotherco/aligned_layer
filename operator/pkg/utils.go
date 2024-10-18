ba233b24174af992ec6609ce8e8795e9b02362da
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-10-18 10:40:43 -0300

    chore: add comment in the cast for the disabled verifiers bitmap

--- operator/pkg/utils.go
+++ operator/pkg/utils.go
@@ -11,6 +11,9 @@ import (
 
 func IsVerifierDisabled(disabledVerifiersBitmap *big.Int, verifierId common.ProvingSystemId) bool {
 	verifierIdInt := uint8(verifierId)
+	// The cast to uint64 is necessary because we need to use the bitwise AND operator.
+	// This will truncate the bitmap to 64 bits, but we are not expecting to have more than 63 verifiers.
+	// If we set a number that doesn't fit in 64 bits, the bitmap will be truncated and no verifier will be disabled.
 	bit := disabledVerifiersBitmap.Uint64() & (1 << verifierIdInt)
 	return bit != 0
 }
