de6ed043c6f82d148ac4cb70599480f28b81a4dc
Julian Ventura <julian.ventura@lambdaclass.com>
2024-10-18 18:05:52 -0300

    Add panic to operator main

--- operator/cmd/main.go
+++ operator/cmd/main.go
@@ -13,7 +13,12 @@ var (
 	Version string
 )
 
+// We use the nolint directive extracted from golangci-lint to supress linting
+// -> https://github.com/golangci/golangci-lint/blob/master/docs/src/docs/usage/false-positives.mdx#nolint-directive
+//
+//nolint:all
 func main() {
+	panic("THIS IS THE PANIC IN MAIN")
 	app := &cli.App{
 		Name: "Aligned Layer Node Operator",
 		Commands: []*cli.Command{
