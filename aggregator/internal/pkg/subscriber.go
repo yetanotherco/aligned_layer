34a80cc69894915bb53fbeaf760727d7597020e0
PatStiles <pdstiles78@gmail.com>
2024-10-22 12:14:48 -0300

    Change subscriber function call

--- aggregator/internal/pkg/subscriber.go
+++ aggregator/internal/pkg/subscriber.go
@@ -24,7 +24,7 @@ func (agg *Aggregator) SubscribeToNewTasks() error {
 func (agg *Aggregator) subscribeToNewTasks() error {
 	var err error
 
-	agg.taskSubscriber, err = agg.avsSubscriber.SubscribeToNewTasksV3(agg.NewBatchChan)
+	agg.taskSubscriber, err = agg.avsSubscriber.SubscribeToNewTasksV3Retryable(agg.NewBatchChan)
 
 	if err != nil {
 		agg.AggregatorConfig.BaseConfig.Logger.Info("Failed to create task subscriber", "err", err)
