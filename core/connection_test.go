053d1fb8b824e5822c974751b813b1aeceb82e42
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2024-10-23 17:54:21 -0300

    test: connection retries

--- core/connection_test.go
+++ core/connection_test.go
@@ -17,26 +17,65 @@ func DummyFunction(x uint64) (uint64, error) {
 	return x, nil
 }
 
+// here we run the dummy function and with the retry and check:
+// - The number of retries checks based on the `n`
+// - The returned valued matches based on the `n`
+// - The returned err matches based on the `n`
 func TestRetryWithData(t *testing.T) {
-	function := func() (*uint64, error) {
-		x, err := DummyFunction(43)
-		return &x, err
+	retries := -1
+	testFun := func(n uint64) func() (*uint64, error) {
+		return func() (*uint64, error) {
+			retries++
+			x, err := DummyFunction(n)
+			return &x, err
+		}
 	}
-	data, err := connection.RetryWithData(function, 1000, 2, 3)
-	if err != nil {
-		t.Errorf("Retry error!: %s", err)
-	} else {
-		fmt.Printf("DATA: %d\n", data)
+	data, err := connection.RetryWithData(testFun(uint64(41)), 1000, 2, 3)
+	if !(retries == 3 && *data == 0 && err != nil) {
+		t.Error("Incorrect execution when n == 41")
+	}
+	//restart
+	retries = -1
+	data, err = connection.RetryWithData(testFun(42), 1000, 2, 3)
+	if !(retries == 0 && data == nil) {
+		if _, ok := err.(*connection.PermanentError); ok {
+			t.Error("Incorrect execution when n == 42")
+		}
+	}
+	//restart
+	retries = -1
+	data, err = connection.RetryWithData(testFun(43), 1000, 2, 3)
+	if !(retries == 0 && *data == 43 && err == nil) {
+		t.Error("Incorrect execution when n == 43")
 	}
 }
 
+// same as above but without checking returned data
 func TestRetry(t *testing.T) {
-	function := func() error {
-		_, err := DummyFunction(43)
-		return err
+	retries := -1
+	testFun := func(n uint64) func() error {
+		return func() error {
+			retries++
+			_, err := DummyFunction(n)
+			return err
+		}
 	}
-	err := connection.Retry(function, 1000, 2, 3)
-	if err != nil {
-		t.Errorf("Retry error!: %s", err)
+	err := connection.Retry(testFun(uint64(41)), 1000, 2, 3)
+	if !(retries == 3 && err != nil) {
+		t.Error("Incorrect execution when n == 41")
+	}
+	//restart
+	retries = -1
+	err = connection.Retry(testFun(42), 1000, 2, 3)
+	if !(retries == 0) {
+		if _, ok := err.(*connection.PermanentError); ok {
+			t.Error("Incorrect execution when n == 42")
+		}
+	}
+	//restart
+	retries = -1
+	err = connection.Retry(testFun(43), 1000, 2, 3)
+	if !(retries == 0 && err == nil) {
+		t.Error("Incorrect execution when n == 43")
 	}
 }
