0b5126f2f60cc0ac97d2226b79754213155e7bfb
MauroFab <maurotoscano2@gmail.com>
2025-03-19 12:15:53 -0300

    Remove double function in interface

--- contracts/src/core/IAlignedLayerServiceManager.sol
+++ contracts/src/core/IAlignedLayerServiceManager.sol
@@ -63,8 +63,6 @@ interface IAlignedLayerServiceManager {
 
     function balanceOf(address account) external view returns (uint256);
 
-    function withdraw(uint256 amount) external;
-
     function setAggregator(address _aggregator) external;
 
     function isVerifierDisabled(
