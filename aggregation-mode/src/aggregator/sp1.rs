993333dfdb242deffd104a2ca317628534b8bc02
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-03-30 15:04:26 -0300

    refactor: use mock client for mock proves

--- aggregation-mode/src/aggregator/sp1.rs
+++ aggregation-mode/src/aggregator/sp1.rs
@@ -1,5 +1,5 @@
 use sp1_aggregator::SP1CompressedProof;
-use sp1_sdk::{ProverClient, SP1ProofMode, SP1ProofWithPublicValues, SP1Stdin, SP1VerifyingKey};
+use sp1_sdk::{Prover, ProverClient, SP1ProofWithPublicValues, SP1Stdin, SP1VerifyingKey};
 
 use super::interface::{AggregatedProof, AggregatedVerificationError, ProgramOutput};
 
@@ -13,23 +13,15 @@ pub struct SP1AggregatedProof {
 pub(crate) fn aggregate_proofs(
     proofs: Vec<SP1CompressedProof>,
 ) -> Result<ProgramOutput, AggregatedVerificationError> {
-    #[cfg(feature = "prove")]
-    {
-        prove(proofs)
-    }
-    // If not in prove mode, execute the program and create a mock proof
-    #[cfg(not(feature = "prove"))]
-    {
-        mock_prove(proofs)
-    }
-}
-
-#[cfg(feature = "prove")]
-fn prove(proofs: Vec<SP1CompressedProof>) -> Result<ProgramOutput, AggregatedVerificationError> {
     let mut stdin = SP1Stdin::new();
     stdin.write(&proofs);
 
+    #[cfg(feature = "prove")]
     let client = ProverClient::from_env();
+    // If not in prove mode, create a mock proof via mock client
+    #[cfg(not(feature = "prove"))]
+    let client = ProverClient::builder().mock().build();
+
     let (pk, vk) = client.setup(PROGRAM_ELF);
     let proof = client
         .prove(&pk, &stdin)
@@ -43,32 +35,6 @@ fn prove(proofs: Vec<SP1CompressedProof>) -> Result<ProgramOutput, AggregatedVer
         .map_err(AggregatedVerificationError::SP1Verification)?;
 
     let output = ProgramOutput::new(AggregatedProof::SP1(SP1AggregatedProof { proof, vk }));
-
-    Ok(output)
-}
-
-fn mock_prove(
-    proofs: Vec<SP1CompressedProof>,
-) -> Result<ProgramOutput, AggregatedVerificationError> {
-    let mut stdin = SP1Stdin::new();
-    stdin.write(&proofs);
-
-    let client = ProverClient::from_env();
-    let (pk, vk) = client.setup(PROGRAM_ELF);
-    let (public_vales, _) = client
-        .execute(PROGRAM_ELF, &stdin)
-        .run()
-        .map_err(|_| AggregatedVerificationError::SP1Proving)?;
-
-    let output = ProgramOutput::new(AggregatedProof::SP1(SP1AggregatedProof {
-        proof: SP1ProofWithPublicValues::create_mock_proof(
-            &pk,
-            public_vales,
-            SP1ProofMode::Groth16,
-            "",
-        ),
-        vk,
-    }));
 
     Ok(output)
 }
