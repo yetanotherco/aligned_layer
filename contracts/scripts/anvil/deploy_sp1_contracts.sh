168b216efe504a2081a6df4356995027a144b3ae
Lucas Rack <lucas.rack@lambdaclass.com>
2025-04-01 14:45:20 -0300

    Fix devnet instead of testnet

--- contracts/scripts/anvil/deploy_sp1_contracts.sh
+++ contracts/scripts/anvil/deploy_sp1_contracts.sh
@@ -13,20 +13,21 @@ cd ../../
 
 sleep 1
 
-export CHAINS='TESTNET'
-export RPC_TESTNET='http://localhost:8545'
+export CHAINS='DEVNET'
+export RPC_DEVNET='http://localhost:8545'
 export OWNER='0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC'
+export VERIFIER_PRIVATE_KEY="0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"
 
 # Deploy Groth16 SP1 verifier gateway
 forge script script/deploy/SP1VerifierGatewayGroth16Deployer.s.sol:SP1VerifierGatewayScript \
-    --rpc-url "http://localhost:8545" \
-    --private-key "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a" \
+    --rpc-url $RPC_DEVNET \
+    --private-key $VERIFIER_PRIVATE_KEY \
     --broadcast
 
-# # Deploy Groth16 SP1 verifier
+# Deploy Groth16 SP1 verifier
 forge script ./script/deploy/SP1VerifierGroth16Deployer.s.sol:SP1VerifierScript \
-    --rpc-url "http://localhost:8545" \
-    --private-key "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a" \
+    --rpc-url $RPC_DEVNET \
+    --private-key $VERIFIER_PRIVATE_KEY \
     --broadcast 
 
 
