2af21b9b32902d5ededc9c156f6af910b7988e5c
Lucas Rack <lucas.rack@lambdaclass.com>
2025-04-01 11:04:57 -0300

    Fix deployers aux files and keys

--- contracts/scripts/anvil/deploy_sp1_contracts.sh
+++ contracts/scripts/anvil/deploy_sp1_contracts.sh
@@ -3,17 +3,41 @@
 # cd to the directory of this script so that this can be run from anywhere
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 # At this point we are in tests/integration
-echo "$parent_path"
+cd "$parent_path"
 
-# # Deploy Groth16 SP1 verifier gateway
-# forge script script/deploy/SP1VerifierGatewayGroth16Deployer.s.sol:SP1VerifierGatewayScript \
-#     --rpc-url "http://localhost:8545" \
-#     --private-key "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a" \
-#     --broadcast
+# Start an empty anvil chain in the background and dump its state to a json file upon exit
+anvil --dump-state state/sp1-deployed-anvil-state.json &
+
+# cd to /contracts
+cd ../../
+
+sleep 1
+
+export CHAINS='TESTNET'
+export RPC_TESTNET='http://localhost:8545'
+export OWNER='0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC'
+
+# Deploy Groth16 SP1 verifier gateway
+forge script script/deploy/SP1VerifierGatewayGroth16Deployer.s.sol:SP1VerifierGatewayScript \
+    --rpc-url "http://localhost:8545" \
+    --private-key "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a" \
+    --broadcast
 
 # # Deploy Groth16 SP1 verifier
-# forge script ./script/deploy/SP1VerifierGroth16Deployer.s.sol:SP1VerifierScript \
-#     --rpc-url "http://localhost:8545" \
-#     --private-key "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6" \
-#     --broadcast \
-#     --verify --verifier etherscan --multi 
+forge script ./script/deploy/SP1VerifierGroth16Deployer.s.sol:SP1VerifierScript \
+    --rpc-url "http://localhost:8545" \
+    --private-key "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a" \
+    --broadcast 
+
+
+# Kill the anvil process to save state
+pkill anvil
+
+# Anvil adds a block state, making the code to fail. We don't care about this, just the accounts and the deployed code
+cd "$parent_path"
+
+jq 'del(.block)' state/sp1-deployed-anvil-state.json > state/sp1-deployed-anvil-state-tmp.json
+
+cp -f state/sp1-deployed-anvil-state-tmp.json state/sp1-deployed-anvil-state.json
+
+rm state/sp1-deployed-anvil-state-tmp.json
