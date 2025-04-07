cad79125d759aa48cc3bd39de1a0fae2fbdbe43b
JuArce <52429267+JuArce@users.noreply.github.com>
2025-04-07 11:16:15 -0300

    fix: set better error messages

--- aggregation_mode/src/backend/fetcher.rs
+++ aggregation_mode/src/backend/fetcher.rs
@@ -29,11 +29,11 @@ pub struct ProofsFetcher {
 
 impl ProofsFetcher {
     pub fn new(config: &Config) -> Self {
-        let rpc_url = config.eth_rpc_url.parse().expect("correct url");
+        let rpc_url = config.eth_rpc_url.parse().expect("RPC URL should be valid");
         let rpc_provider = ProviderBuilder::new().on_http(rpc_url);
         let aligned_service_manager = AlignedLayerServiceManager::new(
             Address::from_str(&config.aligned_service_manager_address)
-                .expect("Address to be correct"),
+                .expect("AlignedProofAggregationService address should be valid"),
             rpc_provider.clone(),
         );
 
