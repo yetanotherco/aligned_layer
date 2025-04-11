6ebc9f6eda9ccc61c1aea06826dd418a861a1170
JuArce <52429267+JuArce@users.noreply.github.com>
2025-04-11 13:41:00 -0300

    fix: tensordock url

--- infra/aggregation_mode/aggregation_mode_start.sh
+++ infra/aggregation_mode/aggregation_mode_start.sh
@@ -4,7 +4,7 @@ BASE_URL=https://dashboard.tensordock.com/api/v2
 
 echo "Starting GPU Server..."
 
-curl -X GET "$BASE_URL/api/v2/instances" \
+curl -X GET "$BASE_URL/instances" \
     -H "Authorization: Bearer $API_KEY" \
     -H "Content-Type: application/json" \
     -H "Accept: application/json"
