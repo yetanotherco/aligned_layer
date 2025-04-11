a727abc9a29295d1e6a10a64695f9bfdb0772425
Lucas Rack <lucas.rack@lambdaclass.com>
2025-04-11 12:26:43 -0300

    fix risc0 proof

--- batcher/aligned/send_proof_with_random_address.sh
+++ batcher/aligned/send_proof_with_random_address.sh
@@ -68,8 +68,9 @@ elif [[ $PROOF_TYPE == "risc0" ]]; then
     cd batcher/aligned/ 
     cargo run --release -- submit \
 		--proving_system Risc0 \
-		--proof ../../scripts/test_files/risc_zero/no_public_inputs/risc_zero_no_pub_input_2_0.proof \
-        --vm_program ../../scripts/test_files/risc_zero/no_public_inputs/no_pub_input_id_2_0.bin \
+		--proof ../../scripts/test_files/risc_zero/fibonacci_proof_generator/risc_zero_fibonacci_2_0.proof \
+        --vm_program ../../scripts/test_files/risc_zero/fibonacci_proof_generator/fibonacci_id_2_0.bin \
+        --public_input ../../scripts/test_files/risc_zero/fibonacci_proof_generator/risc_zero_fibonacci_2_0.pub \
 		--random_address \
         --repetitions $REPETITIONS \
 		--rpc_url $RPC_URL \
