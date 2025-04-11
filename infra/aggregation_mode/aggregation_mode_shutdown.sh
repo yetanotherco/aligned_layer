ef359e15ed30af10662533747af8f2478c6d3576
JuArce <52429267+JuArce@users.noreply.github.com>
2025-04-11 19:06:00 -0300

    chore: remove token

--- infra/aggregation_mode/aggregation_mode_shutdown.sh
+++ infra/aggregation_mode/aggregation_mode_shutdown.sh
@@ -6,8 +6,8 @@ echo "Stopping GPU Server..."
 
 sleep 120
 
-curl -X POST "https://dashboard.tensordock.com/api/v2/instances/41ef030d-22f2-4e5e-852e-cb21982271c0/stop" \
-    -H "Authorization: Bearer fwf7iWW3PEQXqUTFGPJvlmQ8lny3MIUg" \
+curl -X POST "$BASE_URL/instances/$INSTANCE_ID/stop" \
+    -H "Authorization: Bearer $API_KEY" \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
     --data-urlencode 'disassociate_resources=true' \
