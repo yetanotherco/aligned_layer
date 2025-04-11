c47ac882e8b3ed2ef8483dc5379b15c7bac45617
JuArce <52429267+JuArce@users.noreply.github.com>
2025-04-11 13:50:15 -0300

    feat: update startup script

--- infra/aggregation_mode/aggregation_mode_start.sh
+++ infra/aggregation_mode/aggregation_mode_start.sh
@@ -4,7 +4,7 @@ BASE_URL=https://dashboard.tensordock.com/api/v2
 
 echo "Starting GPU Server..."
 
-curl -X GET "$BASE_URL/instances" \
+curl -X POST "$BASE_URL/instances/$INSTANCE_ID/start" \
     -H "Authorization: Bearer $API_KEY" \
     -H "Content-Type: application/json" \
     -H "Accept: application/json"
