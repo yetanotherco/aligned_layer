4c9856cf49eb8dd0c9ecb325e0bcd3d3104ca485
JuArce <52429267+JuArce@users.noreply.github.com>
2025-04-11 18:59:10 -0300

    chore: disassociate resources

--- infra/aggregation_mode/aggregation_mode_shutdown.sh
+++ infra/aggregation_mode/aggregation_mode_shutdown.sh
@@ -6,10 +6,11 @@ echo "Stopping GPU Server..."
 
 sleep 120
 
-curl -X POST "$BASE_URL/instances/$INSTANCE_ID/stop" \
-    -H "Authorization: Bearer $API_KEY" \
+curl -X POST "https://dashboard.tensordock.com/api/v2/instances/41ef030d-22f2-4e5e-852e-cb21982271c0/stop" \
+    -H "Authorization: Bearer fwf7iWW3PEQXqUTFGPJvlmQ8lny3MIUg" \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
+    --data-urlencode 'disassociate_resources=true' \
     --max-time 60
 
 echo "GPU Server stopped successfully."
