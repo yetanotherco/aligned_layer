eef1a05c13fd3a4507f918c47243e9b6ee0cfd0f
Lucas Rack <lucas.rack@lambdaclass.com>
2025-04-22 16:36:08 -0300

    fix spelling

--- batcher/aligned-sdk/src/communication/messaging.rs
+++ batcher/aligned-sdk/src/communication/messaging.rs
@@ -266,7 +266,7 @@ async fn handle_batcher_response(msg: Message) -> Result<BatchInclusionData, Sub
             Err(SubmitError::GenericError(e))
         }
         Ok(SubmitProofResponseMessage::BatchQueueLimitExceededError) => {
-            error!("Batcher responded with error: queue limit has been exeeded. Funds have not been spent.");
+            error!("Batcher responded with error: queue limit has been exceeded. Funds have not been spent.");
             Err(SubmitError::BatchQueueLimitExceededError)
         }
         Err(e) => {
