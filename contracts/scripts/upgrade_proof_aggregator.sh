fe5350fa5a23bd524eb3afa0787c77444113f579
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-04-23 18:35:34 -0300

    chore: move upgrade files to root
    this is so we can reuse the existing .env files

--- contracts/scripts/upgrade_proof_aggregator.sh
+++ contracts/scripts/upgrade_proof_aggregator.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# ENV VARIABLES
+#
+# EXISTING_DEPLOYMENT_INFO_PATH: Path to the proof aggregator deployment output file
+#   - Holesky Stage: ./script/output/holesky/proof_aggregation_service_deployment_output.stage.json
+#   - Holesky Prod: ./script/output/holesky/roof_aggregation_service_deployment_output.json
+#
+# PROOF_AGGREGATOR_OUTPUT_PATH: Path to the proof aggregator output file
+#   - Holesky Stage: ./script/output/holesky/proof_aggregation_service_deployment_output.stage.json
+#   - Holesky Prod: ./script/output/holesky/proof_aggregation_service_deployment_output.json
+#
+# RPC_URL: The RPC URL to connect to the Ethereum network
+#
+# PRIVATE_KEY: The private key to use for the deployment
+#
+# ETHERSCAN_API_KEY: The Etherscan API key to use for verification
+#
+
 # cd to the directory of this script so that this can be run from anywhere
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
