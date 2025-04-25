5f12886b74a3d8c08d92d5e7584cb9318a6ac81f
JuArce <52429267+JuArce@users.noreply.github.com>
2025-04-25 19:24:57 -0300

    fix: stop docker containers

--- infra/aggregation_mode/run.sh
+++ infra/aggregation_mode/run.sh
@@ -7,6 +7,7 @@ sleep $SLEEP
 
 echo "Starting SP1 Aggregation Mode..."
 AGGREGATOR=sp1 SP1_PROVER=cuda /home/user/.cargo/bin/proof_aggregator /home/user/config/config-proof-aggregator-sp1.yaml
+docker stop $(docker ps -a -q) ## stop all containers
 echo "SP1 Aggregation Mode finished"
 
 echo "Starting Risc0 Aggregation Mode..."
