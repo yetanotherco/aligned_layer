5e39263dbed047ee914adae311789c5547ea6bbc
JuArce <52429267+JuArce@users.noreply.github.com>
2025-04-25 14:09:36 -0300

    infra: add risc0 setup

--- infra/aggregation_mode/config_file.sh
+++ infra/aggregation_mode/config_file.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# This script creates a configuration file for the proof aggregator
+
+# ENV VARIABLES
+#
+# TEMPLATE_FILE: Path to the template file
+#   - SP1: ./infra/aggregation_mode/config-proof-aggregator-sp1.template.yaml
+#   - Risc0: ./infra/aggregation_mode/config-proof-aggregator-risc0.template.yaml
+#
+# OUTPUT_FILE: Path to the output file
+#   - SP1: $HOME/config/config-proof-aggregator-sp1.yaml
+#   - Risc0: $HOME/config/config-proof-aggregator-risc0.yaml
+
 # Check if template file path is provided as argument
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <template_file_path>"
@@ -39,9 +51,9 @@ prompt_and_replace "<private_key_store_password>" "ECDSA Private Key Store Passw
 mkdir -p /home/user/config
 
 # Copy the completed file to destination
-cp "$TEMP_FILE" $HOME/config/config-proof-aggregator.yaml
+cp "$TEMP_FILE" "$OUTPUT_FILE"
 
 # Clean up temporary file
 rm "$TEMP_FILE"
 
-echo "Configuration file has been created and copied to $HOME/config/config-proof-aggregator.yaml"
+echo "Configuration file has been created and copied to $OUTPUT_FILE"
