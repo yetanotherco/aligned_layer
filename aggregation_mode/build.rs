7383e8bd621aa8db36305d9cccbe6484b9bfcc78
JuArce <52429267+JuArce@users.noreply.github.com>
2025-05-05 17:27:48 -0300

    feat: use deterministic build for SP1

--- aggregation_mode/build.rs
+++ aggregation_mode/build.rs
@@ -1,7 +1,11 @@
+// Reference: https://docs.succinct.xyz/docs/sp1/writing-programs/compiling#advanced-build-options-1
 fn main() {
     sp1_build::build_program_with_args("./aggregation_programs/sp1", {
         sp1_build::BuildArgs {
             output_directory: Some("./aggregation_programs/sp1/elf".to_string()),
+            // We use Docker to generate a reproducible ELF that will be identical across all platforms
+            // (https://docs.succinct.xyz/docs/sp1/writing-programs/compiling#production-builds)
+            docker: true,  
             ..Default::default()
         }
     });
