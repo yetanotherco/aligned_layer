5f449f30ba44d435c8ec2657918894c68e9395f4
JuArce <52429267+JuArce@users.noreply.github.com>
2025-05-06 13:21:44 -0300

    feat: use deterministic build for risc0

--- aggregation_mode/build.rs
+++ aggregation_mode/build.rs
@@ -1,3 +1,6 @@
+use risc0_build::{DockerOptionsBuilder, GuestOptionsBuilder};
+use std::collections::HashMap;
+
 fn main() {
     sp1_build::build_program_with_args("./aggregation_programs/sp1", {
         sp1_build::BuildArgs {
@@ -6,5 +9,16 @@ fn main() {
         }
     });
 
-    risc0_build::embed_methods();
+    // With this containerized build process, we ensure that all builds of the guest code,
+    // regardless of the machine or local environment, will produce the same ImageID
+    let docker_options = DockerOptionsBuilder::default().build().unwrap();
+    // Reference: https://github.com/risc0/risc0/blob/main/risc0/build/src/config.rs#L73-L90
+    let guest_options = GuestOptionsBuilder::default()
+        .use_docker(docker_options)
+        .build()
+        .unwrap();
+    risc0_build::embed_methods_with_options(HashMap::from([(
+        "risc0_aggregation_program",
+        guest_options,
+    )]));
 }
