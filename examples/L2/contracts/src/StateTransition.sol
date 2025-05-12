7ed99d7f02f2f7e983cb90fda565920a666c2a18
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-05-12 19:02:19 -0300

    feat: deploy script for contract

--- examples/L2/contracts/src/StateTransition.sol
+++ examples/L2/contracts/src/StateTransition.sol
@@ -3,16 +3,23 @@ pragma solidity ^0.8.13;
 
 contract StateTransition {
     event StateUpdated(bytes32);
+    event ProgramIdUpdated(bytes32);
+
+    error OnlyOwner(address);
 
     bytes32 public PROGRAM_ID = 0x00;
     bytes32 public stateRoot;
     address public alignedProofAggregator;
+    address public owner;
 
-    constructor(address _alignedProofAggregator) {
+    constructor(bytes32 programId, bytes32 initialStateRoot, address _alignedProofAggregator, address _owner) {
         alignedProofAggregator = _alignedProofAggregator;
+        owner = _owner;
+        PROGRAM_ID = programId;
+        stateRoot = initialStateRoot;
     }
 
-    function updateState(bytes calldata publicInputs, bytes32[] calldata merkleProof) public {
+    function updateState(bytes calldata publicInputs, bytes32[] calldata merkleProof) public onlyOwner {
         bytes memory callData = abi.encodeWithSignature(
             "verifyProofInclusion(bytes32[],bytes32,bytes)", merkleProof, PROGRAM_ID, publicInputs
         );
@@ -28,5 +35,18 @@ contract StateTransition {
         stateRoot = newStateRoot;
 
         emit StateUpdated(stateRoot);
+    }
+
+    function setProgramId(bytes32 programId) public onlyOwner {
+        PROGRAM_ID = programId;
+
+        emit ProgramIdUpdated(PROGRAM_ID);
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert OnlyOwner(msg.sender);
+        }
+        _;
     }
 }
