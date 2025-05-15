1bea52db96e0aa0997e555b7d5738786b0e12e95
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-05-15 10:29:08 -0300

    feat: update write_program_image_id_vk_hash with new programs

--- aggregation_mode/bin/write_program_image_id_vk_hash.rs
+++ aggregation_mode/bin/write_program_image_id_vk_hash.rs
@@ -1,36 +1,48 @@
 use alloy::hex::hex;
-use proof_aggregator::aggregators::{
-    risc0_aggregator::RISC0_AGGREGATOR_PROGRAM_ID_BYTES, sp1_aggregator,
-};
+use proof_aggregator::aggregators::{risc0_aggregator, sp1_aggregator};
 use serde_json::json;
 use sp1_sdk::HashableKey;
 use std::{env, fs, path::Path};
 use tracing::info;
 use tracing_subscriber::FmtSubscriber;
 
-const SP1_PROGRAM_ELF: &[u8] =
-    include_bytes!("../aggregation_programs/sp1/elf/sp1_aggregator_program");
+const SP1_CHUNK_AGGREGATOR_PROGRAM_ELF: &[u8] =
+    include_bytes!("../aggregation_programs/sp1/elf/sp1_chunk_aggregator_program");
 
-// include!(concat!(env!("OUT_DIR"), "/methods.rs"));
+const SP1_ROOT_AGGREGATOR_PROGRAM_ELF: &[u8] =
+    include_bytes!("../aggregation_programs/sp1/elf/sp1_root_aggregator_program");
 
 fn main() {
     let subscriber = FmtSubscriber::builder().finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     info!("About to write sp1 programs vk hash bytes + risc0 programs image id bytes");
-    let sp1_vk_hash = sp1_aggregator::vk_from_elf(SP1_PROGRAM_ELF).bytes32_raw();
-    //let risc0_image_id_bytes = RISC0_AGGREGATOR_PROGRAM_ID_BYTES;
-    // Just to test sp1
-    let risc0_image_id_bytes = SP1_PROGRAM_ELF;
+    let sp1_chunk_aggregator_vk_hash =
+        sp1_aggregator::vk_from_elf(SP1_CHUNK_AGGREGATOR_PROGRAM_ELF).bytes32_raw();
+    let sp1_chunk_aggregator_vk_hash_words =
+        sp1_aggregator::vk_from_elf(SP1_CHUNK_AGGREGATOR_PROGRAM_ELF).hash_u32();
+    let sp1_root_aggreagot_vk_hash =
+        sp1_aggregator::vk_from_elf(SP1_ROOT_AGGREGATOR_PROGRAM_ELF).bytes32_raw();
 
-    let sp1_vk_hash_hex = hex::encode(sp1_vk_hash);
-    let risc0_image_id_hex = hex::encode(risc0_image_id_bytes);
+    let risc0_chunk_aggregator_image_id_bytes =
+        risc0_aggregator::RISC0_CHUNK_AGGREGATOR_PROGRAM_ID_BYTES;
+    let risc0_root_aggregator_image_id_bytes =
+        risc0_aggregator::RISC0_ROOT_AGGREGATOR_PROGRAM_ID_BYTES;
+
+    let sp1_chunk_aggregator_vk_hash_hex = hex::encode(sp1_chunk_aggregator_vk_hash);
+    let sp1_root_aggregator_vk_hash_hex = hex::encode(sp1_root_aggreagot_vk_hash);
+    let risc0_chunk_aggregator_image_id_hex = hex::encode(risc0_chunk_aggregator_image_id_bytes);
+    let risc0_root_aggregator_imaged_id_hex = hex::encode(risc0_root_aggregator_image_id_bytes);
 
     let dest_path = Path::new("programs_ids.json");
 
     let json_data = json!({
-        "sp1_vk_hash": format!("0x{}", sp1_vk_hash_hex),
-        "risc0_image_id": format!("0x{}", risc0_image_id_hex),
+        "sp1_chunk_aggregator_vk_hash": format!("0x{}", sp1_chunk_aggregator_vk_hash_hex),
+        "sp1_chunk_aggregator_vk_hash_words": format!("{:?}", sp1_chunk_aggregator_vk_hash_words),
+        "sp1_root_aggregator_vk_hash": format!("0x{}", sp1_root_aggregator_vk_hash_hex),
+        "risc0_chunk_aggregator_image_id": format!("0x{}", risc0_chunk_aggregator_image_id_hex),
+        "risc0_chunk_aggregator_image_id_bytes": format!("{:?}", risc0_chunk_aggregator_image_id_bytes),
+        "risc0_root_aggregator_image_id": format!("0x{}", risc0_root_aggregator_imaged_id_hex),
     });
 
     // Write to the file
