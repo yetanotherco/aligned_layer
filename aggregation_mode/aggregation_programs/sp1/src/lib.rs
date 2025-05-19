c408d44269b3c1e2cf3b1491f5a56a2e2c164b89
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-05-19 16:05:49 -0300

    chore: add docs for merkle tree backend for Hash32 in root programs

--- aggregation_mode/aggregation_programs/sp1/src/lib.rs
+++ aggregation_mode/aggregation_programs/sp1/src/lib.rs
@@ -64,6 +64,16 @@ impl IsMerkleTreeBackend for SP1VkAndPubInputs {
 #[derive(Serialize, Deserialize)]
 pub struct Hash32(pub [u8; 32]);
 
+// Note: this MerkleTreeBackend is defined in three locations
+// - aggregation_mode/src/aggregators/mod.rs
+// - aggregation_mode/src/aggregators/risc0_aggregator.rs and
+// - aggregation_mode/src/aggregators/sp1_aggregator.rs
+// All 3 implementations should match
+// The definition on aggregator/mod.rs supports taking proofs from both Risc0 and SP1,
+// Additionally, a version that takes the leaves as already hashed data is defined on:
+// - batcher/aligned-sdk/src/sdk/aggregation.rs
+// This one is used in the SDK since
+// the user may not have access to the proofs that they didn't submit
 impl IsMerkleTreeBackend for Hash32 {
     type Data = Hash32;
     type Node = [u8; 32];
