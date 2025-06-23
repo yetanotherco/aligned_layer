836be62d8dd61c826fe1279e9aadc0be8505aab9
JuArce <52429267+JuArce@users.noreply.github.com>
2025-06-23 16:29:25 -0300

    test: update risc0 test files paths in bindings

--- operator/risc_zero/risc_zero_test.go
+++ operator/risc_zero/risc_zero_test.go
@@ -8,17 +8,17 @@ import (
 )
 
 func TestFibonacciRiscZeroProofVerifies(t *testing.T) {
-	innerReceiptBytes, err := os.ReadFile("../../scripts/test_files/risc_zero/fibonacci_proof_generator/risc_zero_fibonacci_2_0.proof")
+	innerReceiptBytes, err := os.ReadFile("../../scripts/test_files/risc_zero/fibonacci_proof_generator/risc_zero_fibonacci_2_1_0.proof")
 	if err != nil {
 		t.Errorf("could not open proof file: %s", err)
 	}
 
-	imageIdBytes, err := os.ReadFile("../../scripts/test_files/risc_zero/fibonacci_proof_generator/fibonacci_id_2_0.bin")
+	imageIdBytes, err := os.ReadFile("../../scripts/test_files/risc_zero/fibonacci_proof_generator/fibonacci_id_2_1_0.bin")
 	if err != nil {
 		t.Errorf("could not open image id file: %s", err)
 	}
 
-	publicInputBytes, err := os.ReadFile("../../scripts/test_files/risc_zero/fibonacci_proof_generator/risc_zero_fibonacci_2_0.pub")
+	publicInputBytes, err := os.ReadFile("../../scripts/test_files/risc_zero/fibonacci_proof_generator/risc_zero_fibonacci_2_1_0.pub")
 	if err != nil {
 		t.Errorf("could not open public input file: %s", err)
 	}
