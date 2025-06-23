c5b2f65c5c95a5edc3145bf8c333ff7bcc81298d
maximopalopoli <mpalopoli@fi.uba.ar>
2025-06-23 11:27:44 -0300

    Add arms for each network on the network enum and set devnet as default

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -623,10 +623,12 @@ async fn main() -> Result<(), AlignedError> {
 
             for batch_merkle_root in unique_batch_merkle_roots {
                 let base_url = match submit_args.network.clone().into() {
-                    // Note: in case the explorer address changes for other networks, we should add an arm to this
-                    // match with that network since the default URL used here is the mainnet one
                     Network::Holesky => "https://holesky.explorer.alignedlayer.com/batches/0x",
-                    _ => "https://explorer.alignedlayer.com/batches/0x",
+                    Network::HoleskyStage => "https://holesky.explorer.alignedlayer.com/batches/0x",
+                    Network::Mainnet => "https://explorer.alignedlayer.com/batches/0x",
+                    Network::MainnetStage => "https://explorer.alignedlayer.com/batches/0x",
+                    Network::Devnet => "http://localhost:4000/batches/0x",
+                    _ => "http://localhost:4000/batches/0x",
                 };
 
                 info!(
