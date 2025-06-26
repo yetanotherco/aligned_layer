d494cc6ce095d96c28ace09f05521510a3324629
JuArce <52429267+JuArce@users.noreply.github.com>
2025-06-26 15:08:09 -0300

    fix: cargo fmt

--- crates/batcher/build.rs
+++ crates/batcher/build.rs
@@ -9,8 +9,9 @@ fn main() {
 
     // Fix the missing dependency issue
     let mut get_cmd = Command::new("go");
-    get_cmd.arg("get")
-           .arg("github.com/iden3/go-rapidsnark/verifier@v0.0.5");
+    get_cmd
+        .arg("get")
+        .arg("github.com/iden3/go-rapidsnark/verifier@v0.0.5");
 
     let _ = get_cmd.output(); // Run but don't fail if it has issues
 
