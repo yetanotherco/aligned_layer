21805ceb39f51d2ca082409e3b20baf3398dd68f
JuArce <52429267+JuArce@users.noreply.github.com>
2025-07-15 19:33:29 -0300

    example: update validating-public-input (missing contract update)

--- examples/validating-public-input/aligned-integration/src/main.rs
+++ examples/validating-public-input/aligned-integration/src/main.rs
@@ -20,11 +20,11 @@ use log::info;
 use serde_json::json;
 
 const PROOF_FILE_RISC0_PATH: &str =
-    "../risc_zero/fibonacci_proof_generator/risc_zero_fibonacci_2_0.proof";
+    "../risc_zero/fibonacci_proof_generator/risc_zero_fibonacci.proof";
 const PUB_INPUT_RISC0_FILE_PATH: &str =
-    "../risc_zero/fibonacci_proof_generator/risc_zero_fibonacci_2_0.pub";
+    "../risc_zero/fibonacci_proof_generator/risc_zero_fibonacci.pub";
 const IMAGE_ID_RISC0_PATH: &str =
-    "../risc_zero/fibonacci_proof_generator/risc_zero_fibonacci_id_2_0.bin";
+    "../risc_zero/fibonacci_proof_generator/risc_zero_fibonacci_id.bin";
 const PROOF_SP1_FILE_PATH: &str = "../sp1/fibonacci/sp1_fibonacci.proof";
 const PUB_INPUT_SP1_FILE_PATH: &str = "../sp1/fibonacci/sp1_fibonacci.pub";
 const ELF_FILE_PATH: &str = "../sp1/fibonacci/sp1_fibonacci.elf";
