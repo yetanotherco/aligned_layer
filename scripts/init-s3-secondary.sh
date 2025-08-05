bdd87f412a9201fe980b55cd609be506bed3a8b2
JuArce <52429267+JuArce@users.noreply.github.com>
2025-08-05 10:48:57 -0300

    fix: port

--- scripts/init-s3-secondary.sh
+++ scripts/init-s3-secondary.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 /etc/localstack/init/ready.d/init-s3.py \
-  --endpoint_url http://localhost:4567 \
+  --endpoint_url http://localhost:4566 \
   --access_key test2 \
   --secret_key test2 \
   --bucket_name aligned.storage \
