8c8fb8091f93c41836da9467aeac495602f9ab91
JuArce <52429267+JuArce@users.noreply.github.com>
2025-08-28 16:01:50 -0300

    fix(telemetry): add Hoodi network compatibility

--- telemetry_api/lib/telemetry_api/contract_managers/delegation_manager.ex
+++ telemetry_api/lib/telemetry_api/contract_managers/delegation_manager.ex
@@ -9,6 +9,7 @@ defmodule TelemetryApi.ContractManagers.DelegationManager do
                   "holesky" -> 1_210_000
                   "mainnet" -> 19_000_000
                   "sepolia" -> 9_062_616
+                  "hoodi" -> 1_093_860
                   _ -> raise("Invalid environment")
                 end)
 
