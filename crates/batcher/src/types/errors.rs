d55d6b4b35a11854561cbcc3ea4a893c8992d706
MauroFab <maurotoscano2@gmail.com>
2025-09-15 17:47:06 -0300

    Fix

--- crates/batcher/src/types/errors.rs
+++ crates/batcher/src/types/errors.rs
@@ -66,6 +66,7 @@ pub enum BatcherError {
     WsSinkEmpty,
     AddressNotFoundInUserStates(Address),
     QueueRemoveError(String),
+    StateCorruptedAndFlushed(String),
 }
 
 impl From<tungstenite::Error> for BatcherError {
@@ -146,6 +147,9 @@ impl fmt::Debug for BatcherError {
             }
             BatcherError::QueueRemoveError(e) => {
                 write!(f, "Error while removing entry from queue: {}", e)
+            }
+            BatcherError::StateCorruptedAndFlushed(reason) => {
+                write!(f, "Batcher state was corrupted and flushed: {}", reason)
             }
         }
     }
