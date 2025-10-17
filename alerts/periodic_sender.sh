5317e938ac55fbda46f001ef1cbd259c945695f6
maximopalopoli <mpalopoli@fi.uba.ar>
2025-10-17 11:39:08 -0300

    Improve comments on the periodic sender script

--- alerts/periodic_sender.sh
+++ alerts/periodic_sender.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# This script is responsible for the infinite loop and the sleep between passes.
-# It forwards the path of the .env file as the first argument to action.sh
+# This script is responsible for the handling of periodic sending, and the clauses for sending proofs.
+# It forwards the path of the .env file as the first argument to the sender_with_alert.sh script when
+# the conditions are met.
 
 ENV_FILE="$1"
 
@@ -28,31 +29,31 @@ while true; do
     current_gas_price=$(fetch_gas_price)
     echo "Current gas price: $current_gas_price wei"
 
-    # Conditions for sending proofs:
+    # In case current tic and gas price meet the criteria, send a proof and reset tic counter
     if { [ $tic -ge 10 ] && [ $tic -lt 14 ] && [ $current_gas_price -lt 2000000000 ]; }; then
-        # - Between 10 and 14 tics (5 to 7 hours), if gas price is below 2 gwei, send a proof
+        # Between 10 and 14 tics (5 to 7 hours), if gas price is below 2 gwei, send a proof
         message="Sending proof at tic $tic with gas price $current_gas_price wei"
         echo "$message"
         ./alerts/sender_with_alert.sh "$ENV_FILE"
-        tic=0  # Reset tic counter after sending a proof
+        tic=0
     elif { [ $tic -ge 14 ] && [ $tic -lt 16 ] && [ $current_gas_price -lt 5000000000 ]; }; then
-        # - Between 14 and 16 tics (7 to 8 hours), if gas price is below 5 gwei, send a proof
+        # Between 14 and 16 tics (7 to 8 hours), if gas price is below 5 gwei, send a proof
         message="Sending proof at tic $tic with gas price $current_gas_price wei"
         echo "$message"
         ./alerts/sender_with_alert.sh "$ENV_FILE"
-        tic=0  # Reset tic counter after sending a proof
+        tic=0
     elif { [ $tic -ge 16 ] && [ $tic -lt 24 ] && [ $current_gas_price -lt 15000000000 ]; }; then
-        # - Between 16 and 24 tics (8 to 12 hours), if gas price is below 15 gwei, send a proof
+        # Between 16 and 24 tics (8 to 12 hours), if gas price is below 15 gwei, send a proof
         message="Sending proof at tic $tic with gas price $current_gas_price wei"
         echo "$message"
         ./alerts/sender_with_alert.sh "$ENV_FILE"
-        tic=0  # Reset tic counter after sending a proof
+        tic=0
     elif { [ $tic -ge 50 ]; }; then
-        # - After 50 tics (25 hours), if gas price is below 50 gwei, send a proof
+        # After 50 tics (25 hours), if gas price is below 50 gwei, send a proof
         message="Sending proof at tic $tic with gas price $current_gas_price wei"
         echo "$message"
         ./alerts/sender_with_alert.sh "$ENV_FILE"
-        tic=0  # Reset tic counter after sending a proof
+        tic=0
     fi
 
     tic=$((tic + 1))
