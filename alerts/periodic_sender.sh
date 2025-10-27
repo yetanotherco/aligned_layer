417a63a0ee1fd13edac355f464d3a8f959af29dc
maximopalopoli <mpalopoli@fi.uba.ar>
2025-10-27 10:03:25 -0300

    fix: call send_proof_background without parentheses

--- alerts/periodic_sender.sh
+++ alerts/periodic_sender.sh
@@ -53,43 +53,43 @@ while true; do
         # Between 1 and 3 elapsed intervals (5 to 15 minutes), if gas price is below 1.5 gwei, send a proof
         message="Sending proof at $elapsed_intervals with gas price $current_gas_price wei"
         echo "$message"
-        send_proof_background()
+        send_proof_background
         elapsed_intervals=0
     elif { [ $elapsed_intervals -ge 3 ] && [ $elapsed_intervals -lt 6 ] && [ $current_gas_price -lt 4500000000 ]; }; then
         # Between 3 and 6 elapsed intervals (15 to 30 minutes), if gas price is below 4.5 gwei, send a proof
         message="Sending proof at $elapsed_intervals with gas price $current_gas_price wei"
         echo "$message"
-        send_proof_background()
+        send_proof_background
         elapsed_intervals=0
     elif { [ $elapsed_intervals -ge 6 ] && [ $elapsed_intervals -lt 12 ] && [ $current_gas_price -lt 9000000000 ]; }; then
         # Between 6 and 12 elapsed intervals (30 minutes to 1 hour), if gas price is below 9 gwei, send a proof
         message="Sending proof at $elapsed_intervals with gas price $current_gas_price wei"
         echo "$message"
-        send_proof_background()
+        send_proof_background
         elapsed_intervals=0
     elif { [ $elapsed_intervals -ge 12 ] && [ $elapsed_intervals -lt 24 ] && [ $current_gas_price -lt 24000000000 ]; }; then
         # Between 12 and 24 elapsed intervals (1 to 2 hours), if gas price is below 24 gwei, send a proof
         message="Sending proof at $elapsed_intervals with gas price $current_gas_price wei"
         echo "$message"
-        send_proof_background()
+        send_proof_background
         elapsed_intervals=0
     elif { [ $elapsed_intervals -ge 24 ] && [ $elapsed_intervals -lt 48 ] && [ $current_gas_price -lt 48000000000 ]; }; then
         # Between 24 and 48 elapsed intervals (2 to 4 hours), if gas price is below 48 gwei, send a proof
         message="Sending proof at $elapsed_intervals with gas price $current_gas_price wei"
         echo "$message"
-        send_proof_background()
+        send_proof_background
         elapsed_intervals=0
     elif { [ $elapsed_intervals -ge 48 ] && [ $elapsed_intervals -lt 96 ] && [ $current_gas_price -lt 96000000000 ]; }; then
         # Between 48 and 96 elapsed intervals (4 to 8 hours), if gas price is below 96 gwei, send a proof
         message="Sending proof at $elapsed_intervals with gas price $current_gas_price wei"
         echo "$message"
-        send_proof_background()
+        send_proof_background
         elapsed_intervals=0
     elif { [ $elapsed_intervals -ge 96 ] && [ $current_gas_price -lt 192000000000 ]; }; then
         # After 96 elapsed intervals (8 hours), if gas price is below 192 gwei, send a proof
         message="Sending proof at $elapsed_intervals with gas price $current_gas_price wei"
         echo "$message"
-        send_proof_background()
+        send_proof_background
         elapsed_intervals=0
     fi
 
