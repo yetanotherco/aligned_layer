38fd3b34c87a54f4fbd2b7ed4dd052be1055141a
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-11-13 16:30:00 -0300

    refactor: centralize proving system id constant

--- aggregation_mode/src/aggregators/risc0_aggregator.rs
+++ aggregation_mode/src/aggregators/risc0_aggregator.rs
@@ -1,5 +1,6 @@
 include!(concat!(env!("OUT_DIR"), "/methods.rs"));
 
+use aligned_sdk::aggregation_layer::RISC0_PROVING_SYSTEM_ID;
 use risc0_zkvm::{default_prover, ExecutorEnv, ProverOpts, Receipt};
 use sha3::{Digest, Keccak256};
 
@@ -80,7 +81,7 @@ pub const RISC0_CHUNK_AGGREGATOR_PROGRAM_ID_BYTES: [u8; 32] = {
 impl Risc0ProofReceiptAndImageId {
     pub fn hash_image_id_and_public_inputs(&self) -> [u8; 32] {
         let mut hasher = Keccak256::new();
-        hasher.update(&[1u8]);
+        hasher.update(&[RISC0_PROVING_SYSTEM_ID]);
         hasher.update(self.image_id);
         hasher.update(self.public_inputs());
         hasher.finalize().into()
