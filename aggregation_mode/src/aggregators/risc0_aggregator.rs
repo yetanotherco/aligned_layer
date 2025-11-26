85db268b07344d247e947a85ea376d0b5fef580c
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-11-26 15:10:40 -0300

    chore: address clippy warnings

--- aggregation_mode/src/aggregators/risc0_aggregator.rs
+++ aggregation_mode/src/aggregators/risc0_aggregator.rs
@@ -81,7 +81,7 @@ pub const RISC0_CHUNK_AGGREGATOR_PROGRAM_ID_BYTES: [u8; 32] = {
 impl Risc0ProofReceiptAndImageId {
     pub fn hash_image_id_and_public_inputs(&self) -> [u8; 32] {
         let mut hasher = Keccak256::new();
-        hasher.update(&[AggregationModeProvingSystem::RISC0.id()]);
+        hasher.update([AggregationModeProvingSystem::RISC0.id()]);
         hasher.update(self.image_id);
         hasher.update(self.public_inputs());
         hasher.finalize().into()
