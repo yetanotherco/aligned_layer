da5dce6e5441f374fc05c4bad54bde815e37ef43
maximopalopoli <mpalopoli@fi.uba.ar>
2025-12-04 14:30:55 -0300

    Add nonce to the tasks table

--- aggregation_mode/db/migrations/001_init.sql
+++ aggregation_mode/db/migrations/001_init.sql
@@ -7,7 +7,8 @@ CREATE TABLE tasks (
     proof BYTEA,
     program_commitment BYTEA,
     merkle_path BYTEA,
-    status task_status DEFAULT 'pending'
+    status task_status DEFAULT 'pending',
+    nonce BIGINT
 );
 
 CREATE TABLE payment_events (
