b388f1c7a6dca4628bb364014679dd02ac775853
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-12-22 13:09:01 -0300

    refactor: split migration of task status updated

--- aggregation_mode/db/migrations/001_init.sql
+++ aggregation_mode/db/migrations/001_init.sql
@@ -9,8 +9,7 @@ CREATE TABLE tasks (
     merkle_path BYTEA,
     status task_status DEFAULT 'pending',
     nonce BIGINT NOT NULL,
-    inserted_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    status_updated_at TIMESTAMPTZ DEFAULT now()
+    inserted_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE TABLE payment_events (
