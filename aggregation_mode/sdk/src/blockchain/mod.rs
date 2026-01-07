a97690cb3a4de52732abab37034eb2a6fb808397
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2026-01-07 12:06:38 -0300

    chore: remove old comment

--- aggregation_mode/sdk/src/blockchain/mod.rs
+++ aggregation_mode/sdk/src/blockchain/mod.rs
@@ -3,7 +3,6 @@ mod helpers;
 pub mod provider;
 mod types;
 
-// Makes only the two types on this use public
 pub use types::{
     AggregationModeProvingSystem, AggregationModeVerificationData, ProofStatus,
     ProofVerificationAggModeError,
